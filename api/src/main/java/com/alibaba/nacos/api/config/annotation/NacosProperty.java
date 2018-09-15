07d2f3c0579a11e8ee82a59cc083fae7088a9dc8
mercyblitz <mercyblitz@gmail.com>
2018-09-15 13:50:25 +0800

    Update 0.2.1 and refactor some APIs

--- api/src/main/java/com/alibaba/nacos/api/config/annotation/NacosProperty.java
+++ api/src/main/java/com/alibaba/nacos/api/config/annotation/NacosProperty.java
@@ -26,7 +26,7 @@ import java.lang.annotation.*;
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @see NacosConfigurationProperties
  * @see NacosIgnore
- * @since 0.1.0
+ * @since 0.2.0
  */
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
