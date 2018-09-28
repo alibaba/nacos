5373eb9e698a012ba4bc798ef9185c9fe016ed20
王彦民 <yanmin.wang@ele.me>
2018-09-28 15:41:43 +0800

    feat: 服务列表接口对接

--- console/src/main/resources/static/src/pages/ServiceManagement.js
+++ console/src/main/resources/static/src/pages/ServiceManagement.js
@@ -30,125 +30,128 @@ const I18N_PUBNODEDATA = window.aliwareIntl.get('pubnodedata', '')
 
 /*****************************此行为标记行, 请勿删和修改此行, 文件和组件依赖请写在此行上面, 主体代码请写在此行下面的class中*****************************/
 class ServiceManagement extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: false,
-      total: 0,
-      pageSize: 10,
-      currentPage: 1,
-      dataSource: []
-    };
-    this.field = new Field(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            total: 0,
+            pageSize: 10,
+            currentPage: 1,
+            keyword: '',
+            dataSource: []
+        };
+        this.field = new Field(this);
+    }
 
-  componentDidMount() {
-  }
+    componentDidMount() {
+    }
 
-  openLoading() {
-    this.setState({loading: true})
-  }
+    openLoading() {
+        this.setState({loading: true})
+    }
 
-  closeLoading() {
-    this.setState({
-      loading: false
-    });
-  }
+    closeLoading() {
+        this.setState({loading: false})
+    }
 
-  queryServiceList() {
-    this.openLoading()
-    //TODO: request code...
-    setTimeout(() => {
-      this.setState({dataSource: TEST_DATA_SOURCE}, () => this.closeLoading())
-    }, 1500)
-  }
+    queryServiceList() {
+        const {currentPage, pageSize, keyword} = this.state
+        const parameter = [`startPg=${currentPage}`, `pgSize=${pageSize}`, `keyword=${keyword}`]
+        window.request({
+            url: `/nacos/v1/ns/catalog/serviceList?${parameter.join('&')}`,
+            beforeSend: () => this.openLoading(),
+            success: ({count = 0, serviceList = []} = {}) => this.setState({
+                dataSource: serviceList,
+                total: count
+            }),
+            error: () => this.setState({
+                dataSource: [],
+                total: 0,
+                currentPage: 0
+            }),
+            complete: () => this.closeLoading()
+        });
+    }
 
+    getQueryLater() {
+        setTimeout(() => this.queryServiceList());
+    }
 
-  getQueryLater() {
-    setTimeout(() => this.queryServiceList());
-  }
+    render() {
+        const {keyword} = this.state
+        const {init, getValue} = this.field;
+        this.init = init;
+        this.getValue = getValue;
+        const locale = {empty: I18N_PUBNODEDATA}
 
-  render() {
-    const {init, getValue} = this.field;
-    this.init = init;
-    this.getValue = getValue;
-    const locale = {empty: I18N_PUBNODEDATA}
-
-    return (
-      <div style={{padding: 10}}>
-        <Loading
-          shape="flower"
-          style={{position: 'relative'}}
-          visible={this.state.loading}
-          tip="Loading..."
-          color="#333"
-        >
-          <RegionGroup
-            left={I18N_SERVICE_LIST}
-            namespaceCallBack={this.getQueryLater.bind(this)}
-          />
-          <Row className="demo-row" style={{marginBottom: 10, padding: 0}}>
-            <Col span="24">
-              <Form inline field={this.field}>
-                <FormItem label={I18N_SERVICE_NAME}>
-                  <Input
-                    placeholder={I18N_ENTER_SERVICE_NAME}
-                    style={{width: 200}} {...this.init('dataId')} />
-                </FormItem>
-                <FormItem label="">
-                  <Button
-                    type="primary"
-                    onClick={this.queryServiceList.bind(this)}
-                    style={{marginRight: 10}}
-                  >{I18N_QUERY}</Button>
-                </FormItem>
-              </Form>
-            </Col>
-          </Row>
-          <Row style={{padding: 0}}>
-            <Col span="24" style={{padding: 0}}>
-              <Table
-                dataSource={this.state.dataSource}
-                fixedHeader={true}
-                maxBodyHeight={500}
-                locale={locale}
-                language={window.aliwareIntl.currentLanguageCode}
-              >
-                <Column title="Service Name" dataIndex="serviceName"/>
-                <Column title="Cluster Count" dataIndex="clusterCount"/>
-                <Column title="IP Count" dataIndex="ipCount"/>
-                <Column title="Health Status" dataIndex="healthStatus"/>
-                <Column title="Operation" align="center" cell={(value, index, record) => (
-                  <Button type="normal">Detail</Button>
-                )}/>
-              </Table>
-            </Col>
-          </Row>
-          <div style={{marginTop: 10, textAlign: 'right'}}>
-            <Pagination
-              current={this.state.currentPage}
-              total={this.state.total}
-              pageSize={this.state.pageSize}
-              onChange={currentPage => this.setState({currentPage})}
-              language={window.pageLanguage}
-            />
-          </div>
-        </Loading>
-      </div>
-    );
-  }
+        return (
+            <div style={{padding: 10}}>
+                <Loading
+                    shape="flower"
+                    style={{position: 'relative'}}
+                    visible={this.state.loading}
+                    tip="Loading..."
+                    color="#333"
+                >
+                    <RegionGroup
+                        left={I18N_SERVICE_LIST}
+                        namespaceCallBack={this.getQueryLater.bind(this)}
+                    />
+                    <Row className="demo-row" style={{marginBottom: 10, padding: 0}}>
+                        <Col span="24">
+                            <Form inline field={this.field}>
+                                <FormItem label={I18N_SERVICE_NAME}>
+                                    <Input
+                                        placeholder={I18N_ENTER_SERVICE_NAME}
+                                        style={{width: 200}}
+                                        value={keyword}
+                                        onChange={keyword => this.setState({keyword})}
+                                    />
+                                </FormItem>
+                                <FormItem label="">
+                                    <Button
+                                        type="primary"
+                                        onClick={() => this.setState({currentPage: 1}, () => this.queryServiceList())}
+                                        style={{marginRight: 10}}
+                                    >{I18N_QUERY}</Button>
+                                </FormItem>
+                            </Form>
+                        </Col>
+                    </Row>
+                    <Row style={{padding: 0}}>
+                        <Col span="24" style={{padding: 0}}>
+                            <Table
+                                dataSource={this.state.dataSource}
+                                fixedHeader={true}
+                                maxBodyHeight={530}
+                                locale={locale}
+                                language={window.aliwareIntl.currentLanguageCode}
+                                className={r => this.rowColor[r.status]}
+                            >
+                                <Column title="服务名" dataIndex="name"/>
+                                <Column title="集群数目" dataIndex="clusterCount"/>
+                                <Column title="实例数目" dataIndex="ipCount"/>
+                                <Column title="健康程度" dataIndex="status"/>
+                                <Column title="操作" align="center" cell={(value, index, record) => (
+                                    <Button type="normal" disabled>详情</Button>
+                                )}/>
+                            </Table>
+                        </Col>
+                    </Row>
+                    <div style={{marginTop: 10, textAlign: 'right'}}>
+                        <Pagination
+                            current={this.state.currentPage}
+                            total={this.state.total}
+                            pageSize={this.state.pageSize}
+                            onChange={currentPage => this.setState({currentPage}, () => this.queryServiceList())}
+                            language={window.pageLanguage}
+                        />
+                    </div>
+                </Loading>
+            </div>
+        );
+    }
 }
 
 /*****************************此行为标记行, 请勿删和修改此行, 主体代码请写在此行上面的class中, 组件导出语句及其他信息请写在此行下面*****************************/
 export default ServiceManagement;
-
-const TEST_DATA_SOURCE = []
-
-for (let i = 0; i < 20; i++) {
-  TEST_DATA_SOURCE.push({
-    serviceName: 'test.com',
-    clusterCount: 40,
-    ipCount: 50,
-    healthStatus: ''
-  })
-}
