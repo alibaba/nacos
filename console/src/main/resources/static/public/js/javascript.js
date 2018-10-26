c32fbd9f653132f7ea487fa371280579034172dd
xuechaos <xchaos8@126.com>
2018-10-26 21:02:33 +0800

    update lisence header

--- console/src/main/resources/static/public/js/javascript.js
+++ console/src/main/resources/static/public/js/javascript.js
@@ -1,5 +1,15 @@
-// CodeMirror, copyright (c) by Marijn Haverbeke and others
-// Distributed under an MIT license: http://codemirror.net/LICENSE
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
