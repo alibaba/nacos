6551ff2ecb2852a005e0a19f0f6e06791fd57573
王彦民 <yanmin.wang@ele.me>
2018-12-01 09:05:52 +0800

    Fixed language

--- console/src/main/resources/static/console-fe/src/locales/zh-CN.js
+++ console/src/main/resources/static/console-fe/src/locales/zh-CN.js
@@ -19,5 +19,28 @@ const I18N_CONF = {
     community: '社区',
     languageSwitchButton: 'En',
   },
+  MainLayout: {
+    nacosName: 'NACOS',
+    nacosVersion: '0.5.0',
+    doesNotExist: '您访问的页面不存在',
+    configurationManagementVirtual: '配置管理',
+    configurationManagement: '配置列表',
+    configdetail: '配置详情',
+    configsync: '同步配置',
+    configeditor: '配置编辑',
+    newconfig: '新建配置',
+    historyRollback: '历史版本',
+    configRollback: '配置回滚',
+    historyDetail: '历史详情',
+    listeningToQuery: '监听查询',
+    serviceManagementVirtual: '服务管理',
+    serviceManagement: '服务列表',
+    serviceDetail: '服务详情',
+    namespace: '命名空间',
+  },
+  NameSpace: {
+    prompt: '提示',
+    namespaceDetails: '命名空间详情',
+  },
 };
 export default I18N_CONF;
