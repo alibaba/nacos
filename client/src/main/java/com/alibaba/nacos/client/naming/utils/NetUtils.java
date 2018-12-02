f142044b32889c16325d88ef6aef40bc5d6ba2b2
nkorange <zpf.073@gmail.com>
2018-12-02 17:40:06 +0800

    #377 Clean up code

--- client/src/main/java/com/alibaba/nacos/client/naming/utils/NetUtils.java
+++ client/src/main/java/com/alibaba/nacos/client/naming/utils/NetUtils.java
@@ -30,7 +30,7 @@ public class NetUtils {
                return LOCAL_IP;
             }
 
-            String ip = System.getProperty("com.taobao.vipserver.localIP", InetAddress.getLocalHost().getHostAddress());
+            String ip = System.getProperty("com.alibaba.nacos.client.naming.local.ip", InetAddress.getLocalHost().getHostAddress());
 
             return LOCAL_IP = ip;
         } catch (UnknownHostException e) {
