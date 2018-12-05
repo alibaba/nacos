09d8984e7125fd2023dae08fca9afaebb2f61c58
hxy1991 <cannel_2020@163.com>
2018-12-05 22:50:52 +0800

    Fixes #391

--- console/src/test/java/com/alibaba/nacos/console/controller/HealthControllerTest.java
+++ console/src/test/java/com/alibaba/nacos/console/controller/HealthControllerTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.nacos.console.controller;
 
 import com.alibaba.fastjson.JSONObject;
@@ -24,6 +39,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import static org.mockito.ArgumentMatchers.any;
 
+/**
+ * @author <a href="mailto:huangxiaoyu1018@gmail.com">hxy1991</a>
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = MockServletContext.class)
 @WebAppConfiguration
