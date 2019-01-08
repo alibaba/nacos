39d1eb09a5b0445609d6e3d172880f725174df79
fudali <fudali113@gmail.com>
2019-01-08 17:47:18 +0800

    fix: mac 下 oracle 安装包安装的 java 的地址可能在 /usr/bin/java

--- distribution/bin/startup.sh
+++ distribution/bin/startup.sh
@@ -20,6 +20,7 @@ error_exit ()
 }
 
 [ ! -e "$JAVA_HOME/bin/java" ] && JAVA_HOME=$HOME/jdk/java
+[ ! -e "$JAVA_HOME/bin/java" ] && JAVA_HOME=/usr
 [ ! -e "$JAVA_HOME/bin/java" ] && JAVA_HOME=/usr/java
 [ ! -e "$JAVA_HOME/bin/java" ] && JAVA_HOME=/opt/taobao/java
 [ ! -e "$JAVA_HOME/bin/java" ] && error_exit "Please set the JAVA_HOME variable in your environment, We need java(x64)! jdk8 or later is better!"
