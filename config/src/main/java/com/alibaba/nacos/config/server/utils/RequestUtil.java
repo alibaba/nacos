9ac02e638bc7f623ba3fc3022a2ff7535ae27bb8
fudali <fudali113@gmail.com>
2019-01-09 16:13:31 +0800

    feat: 获取客服端 ip 支持 X-Forwarded-For header

--- config/src/main/java/com/alibaba/nacos/config/server/utils/RequestUtil.java
+++ config/src/main/java/com/alibaba/nacos/config/server/utils/RequestUtil.java
@@ -24,11 +24,29 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class RequestUtil {
 
+    private static final String X_REAL_IP = "X-Real-IP";
+    private static final String X_FORWARDED_FOR = "X-Forwarded-For";
+    private static final String X_FORWARDED_FOR_SPLIT_SYMBOL = ",";
+
     public static final String CLIENT_APPNAME_HEADER = "Client-AppName";
 
+    /**
+     * get real client ip
+     * <p>
+     * first use X-Forwarded-For header    https://zh.wikipedia.org/wiki/X-Forwarded-For
+     * next nginx X-Real-IP
+     * last {@link HttpServletRequest#getRemoteAddr()}
+     *
+     * @param request {@link HttpServletRequest}
+     * @return
+     */
     public static String getRemoteIp(HttpServletRequest request) {
-        String nginxHeader = request.getHeader("X-Real-IP");
-        return (nginxHeader == null) ? request.getRemoteAddr() : nginxHeader;
+        String xForwardedFor = request.getHeader(X_FORWARDED_FOR);
+        if (!StringUtils.isBlank(xForwardedFor)) {
+            return xForwardedFor.split(X_FORWARDED_FOR_SPLIT_SYMBOL)[0].trim();
+        }
+        String nginxHeader = request.getHeader(X_REAL_IP);
+        return StringUtils.isBlank(nginxHeader) ? request.getRemoteAddr() : nginxHeader;
     }
 
     public static String getAppName(HttpServletRequest request) {
