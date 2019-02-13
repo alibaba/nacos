97f33fe4e668b1f72dc812cacfb01ff7e6076f0e
pbting <314226532@qq.com>
2019-02-13 16:12:16 +0800

    code format with idea

--- client/src/main/java/com/alibaba/nacos/client/identify/Constants.java
+++ client/src/main/java/com/alibaba/nacos/client/identify/Constants.java
@@ -21,32 +21,32 @@ package com.alibaba.nacos.client.identify;
  * @author Nacos
  */
 public class Constants {
-	public static final String ACCESS_KEY = "accessKey";
+    public static final String ACCESS_KEY = "accessKey";
 
-	public static final String SECRET_KEY = "secretKey";
+    public static final String SECRET_KEY = "secretKey";
 
-	public static final String TENANT_ID = "tenantId";
+    public static final String TENANT_ID = "tenantId";
 
-	public static final String PROPERTIES_FILENAME = "spas.properties";
+    public static final String PROPERTIES_FILENAME = "spas.properties";
 
-	public static final String CREDENTIAL_PATH = "/home/admin/.spas_key/";
+    public static final String CREDENTIAL_PATH = "/home/admin/.spas_key/";
 
-	public static final String CREDENTIAL_DEFAULT = "default";
+    public static final String CREDENTIAL_DEFAULT = "default";
 
-	public static final String DOCKER_CREDENTIAL_PATH = "/etc/instanceInfo";
+    public static final String DOCKER_CREDENTIAL_PATH = "/etc/instanceInfo";
 
-	public static final String DOCKER_ACCESS_KEY = "env_spas_accessKey";
+    public static final String DOCKER_ACCESS_KEY = "env_spas_accessKey";
 
-	public static final String DOCKER_SECRET_KEY = "env_spas_secretKey";
+    public static final String DOCKER_SECRET_KEY = "env_spas_secretKey";
 
-	public static final String DOCKER_TENANT_ID = "ebv_spas_tenantId";
+    public static final String DOCKER_TENANT_ID = "ebv_spas_tenantId";
 
-	public static final String ENV_ACCESS_KEY = "spas_accessKey";
+    public static final String ENV_ACCESS_KEY = "spas_accessKey";
 
-	public static final String ENV_SECRET_KEY = "spas_secretKey";
+    public static final String ENV_SECRET_KEY = "spas_secretKey";
 
-	public static final String ENV_TENANT_ID = "tenant.id";
+    public static final String ENV_TENANT_ID = "tenant.id";
 
-	public static final String NO_APP_NAME = "";
+    public static final String NO_APP_NAME = "";
 
 }
