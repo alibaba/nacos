1a6fa9e69cfbf35769d793f187aed124736bb97a
pbting <314226532@qq.com>
2019-02-13 15:42:41 +0800

    support ans

--- client/src/main/java/com/alibaba/nacos/client/naming/net/HttpClient.java
+++ client/src/main/java/com/alibaba/nacos/client/naming/net/HttpClient.java
@@ -35,159 +35,187 @@ import java.util.zip.GZIPInputStream;
  */
 public class HttpClient {
 
-    public static final int TIME_OUT_MILLIS = Integer.getInteger("com.alibaba.nacos.client.naming.ctimeout", 50000);
-    public static final int CON_TIME_OUT_MILLIS = Integer.getInteger("com.alibaba.nacos.client.naming.ctimeout", 3000);
-    private static final boolean ENABLE_HTTPS = Boolean.getBoolean("com.alibaba.nacos.client.naming.tls.enable");
+	public static final int TIME_OUT_MILLIS = Integer
+			.getInteger("com.alibaba.nacos.client.naming.ctimeout", 50000);
+	public static final int CON_TIME_OUT_MILLIS = Integer
+			.getInteger("com.alibaba.nacos.client.naming.ctimeout", 3000);
+	private static final boolean ENABLE_HTTPS = Boolean
+			.getBoolean("com.alibaba.nacos.client.naming.tls.enable");
 
-    static {
-        // limit max redirection
-        System.setProperty("http.maxRedirects", "5");
-    }
+	private static final String POST = "POST";
+	private static final String PUT = "PUT";
 
-    public static String getPrefix() {
-        if (ENABLE_HTTPS) {
-            return "https://";
-        }
+	static {
+		// limit max redirection
+		System.setProperty("http.maxRedirects", "5");
+	}
 
-        return "http://";
+	public static String getPrefix() {
+		if (ENABLE_HTTPS) {
+			return "https://";
+		}
 
-    }
+		return "http://";
 
-    public static HttpResult httpGet(String url, List<String> headers, Map<String, String> paramValues, String encoding) {
-        return request(url, headers, paramValues, encoding, "GET");
-    }
+	}
 
-    public static HttpResult request(String url, List<String> headers, Map<String, String> paramValues, String encoding, String method) {
-        HttpURLConnection conn = null;
-        try {
-            String encodedContent = encodingParams(paramValues, encoding);
-            url += (null == encodedContent) ? "" : ("?" + encodedContent);
+	public static HttpResult httpGet(String url, List<String> headers,
+			Map<String, String> paramValues, String encoding) {
+		return request(url, headers, paramValues, encoding, "GET");
+	}
 
-            conn = (HttpURLConnection) new URL(url).openConnection();
+	public static HttpResult request(String url, List<String> headers,
+			Map<String, String> paramValues, String encoding, String method) {
+		HttpURLConnection conn = null;
+		try {
+			String encodedContent = encodingParams(paramValues, encoding);
+			url += (null == encodedContent) ? "" : ("?" + encodedContent);
 
-            conn.setConnectTimeout(CON_TIME_OUT_MILLIS);
-            conn.setReadTimeout(TIME_OUT_MILLIS);
-            conn.setRequestMethod(method);
-            setHeaders(conn, headers, encoding);
-            conn.connect();
-            LogUtils.LOG.debug("Request from server: " + url);
-            return getResult(conn);
-        } catch (Exception e) {
-            try {
-                if (conn != null) {
-                    LogUtils.LOG.warn("failed to request " + conn.getURL() + " from "
-                            + InetAddress.getByName(conn.getURL().getHost()).getHostAddress());
-                }
-            } catch (Exception e1) {
-                LogUtils.LOG.error("NA", "failed to request ", e1);
-                //ignore
-            }
+			conn = (HttpURLConnection) new URL(url).openConnection();
 
-            LogUtils.LOG.error("NA", "failed to request ", e);
+			setHeaders(conn, headers, encoding);
+			conn.setConnectTimeout(CON_TIME_OUT_MILLIS);
+			conn.setReadTimeout(TIME_OUT_MILLIS);
+			conn.setRequestMethod(method);
+			conn.setDoOutput(true);
+			if (POST.equals(method) || PUT.equals(method)) {
+				// fix: apache http nio framework must set some content to request body
+				byte[] b = encodedContent.getBytes();
+				conn.setRequestProperty("Content-Length", String.valueOf(b.length));
+				conn.getOutputStream().write(b, 0, b.length);
+				conn.getOutputStream().flush();
+				conn.getOutputStream().close();
+			}
+			conn.connect();
+			LogUtils.LOG.debug("Request from server: " + url);
+			return getResult(conn);
+		}
+		catch (Exception e) {
+			try {
+				if (conn != null) {
+					LogUtils.LOG.warn(
+							"failed to request " + conn.getURL() + " from " + InetAddress
+									.getByName(conn.getURL().getHost()).getHostAddress());
+				}
+			}
+			catch (Exception e1) {
+				LogUtils.LOG.error("NA", "failed to request ", e1);
+				// ignore
+			}
 
-            return new HttpResult(500, e.toString(), Collections.<String, String>emptyMap());
-        } finally {
-            if (conn != null) {
-                conn.disconnect();
-            }
-        }
-    }
+			LogUtils.LOG.error("NA", "failed to request ", e);
 
-    private static HttpResult getResult(HttpURLConnection conn) throws IOException {
-        int respCode = conn.getResponseCode();
+			return new HttpResult(500, e.toString(),
+					Collections.<String, String>emptyMap());
+		}
+		finally {
+			if (conn != null) {
+				conn.disconnect();
+			}
+		}
+	}
 
-        InputStream inputStream;
-        if (HttpURLConnection.HTTP_OK == respCode
-                || HttpURLConnection.HTTP_NOT_MODIFIED == respCode) {
-            inputStream = conn.getInputStream();
-        } else {
-            inputStream = conn.getErrorStream();
-        }
+	private static HttpResult getResult(HttpURLConnection conn) throws IOException {
+		int respCode = conn.getResponseCode();
 
-        Map<String, String> respHeaders = new HashMap<String, String>(conn.getHeaderFields().size());
-        for (Map.Entry<String, List<String>> entry : conn.getHeaderFields().entrySet()) {
-            respHeaders.put(entry.getKey(), entry.getValue().get(0));
-        }
+		InputStream inputStream;
+		if (HttpURLConnection.HTTP_OK == respCode
+				|| HttpURLConnection.HTTP_NOT_MODIFIED == respCode) {
+			inputStream = conn.getInputStream();
+		}
+		else {
+			inputStream = conn.getErrorStream();
+		}
 
-        String encodingGzip = "gzip";
+		Map<String, String> respHeaders = new HashMap<String, String>(
+				conn.getHeaderFields().size());
+		for (Map.Entry<String, List<String>> entry : conn.getHeaderFields().entrySet()) {
+			respHeaders.put(entry.getKey(), entry.getValue().get(0));
+		}
 
-        if (encodingGzip.equals(respHeaders.get(HttpHeaders.CONTENT_ENCODING))) {
-            inputStream = new GZIPInputStream(inputStream);
-        }
+		String encodingGzip = "gzip";
 
-        return new HttpResult(respCode, IoUtils.toString(inputStream, getCharset(conn)), respHeaders);
-    }
+		if (encodingGzip.equals(respHeaders.get(HttpHeaders.CONTENT_ENCODING))) {
+			inputStream = new GZIPInputStream(inputStream);
+		}
 
-    private static String getCharset(HttpURLConnection conn) {
-        String contentType = conn.getContentType();
-        if (StringUtils.isEmpty(contentType)) {
-            return "UTF-8";
-        }
+		return new HttpResult(respCode, IoUtils.toString(inputStream, getCharset(conn)),
+				respHeaders);
+	}
 
-        String[] values = contentType.split(";");
-        if (values.length == 0) {
-            return "UTF-8";
-        }
+	private static String getCharset(HttpURLConnection conn) {
+		String contentType = conn.getContentType();
+		if (StringUtils.isEmpty(contentType)) {
+			return "UTF-8";
+		}
 
-        String charset = "UTF-8";
-        for (String value : values) {
-            value = value.trim();
+		String[] values = contentType.split(";");
+		if (values.length == 0) {
+			return "UTF-8";
+		}
 
-            if (value.toLowerCase().startsWith("charset=")) {
-                charset = value.substring("charset=".length());
-            }
-        }
+		String charset = "UTF-8";
+		for (String value : values) {
+			value = value.trim();
 
-        return charset;
-    }
+			if (value.toLowerCase().startsWith("charset=")) {
+				charset = value.substring("charset=".length());
+			}
+		}
 
-    private static void setHeaders(HttpURLConnection conn, List<String> headers, String encoding) {
-        if (null != headers) {
-            for (Iterator<String> iter = headers.iterator(); iter.hasNext(); ) {
-                conn.addRequestProperty(iter.next(), iter.next());
-            }
-        }
+		return charset;
+	}
 
-        conn.addRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset="
-                + encoding);
-        conn.addRequestProperty("Accept-Charset", encoding);
-    }
+	private static void setHeaders(HttpURLConnection conn, List<String> headers,
+			String encoding) {
+		if (null != headers) {
+			for (Iterator<String> iter = headers.iterator(); iter.hasNext();) {
+				conn.addRequestProperty(iter.next(), iter.next());
+			}
+		}
 
-    private static String encodingParams(Map<String, String> params, String encoding)
-            throws UnsupportedEncodingException {
-        StringBuilder sb = new StringBuilder();
-        if (null == params || params.isEmpty()) {
-            return null;
-        }
+		conn.addRequestProperty("Content-Type",
+				"application/x-www-form-urlencoded;charset=" + encoding);
+		conn.addRequestProperty("Accept-Charset", encoding);
+	}
 
-        params.put("encoding", encoding);
+	private static String encodingParams(Map<String, String> params, String encoding)
+			throws UnsupportedEncodingException {
+		if (null == params) {
+			return null;
+		}
+		params.put("encoding", encoding);
+		StringBuilder sb = new StringBuilder();
 
-        for (Map.Entry<String, String> entry : params.entrySet()) {
-            if (StringUtils.isEmpty(entry.getValue())) {
-                continue;
-            }
+		for (Map.Entry<String, String> entry : params.entrySet()) {
+			if (StringUtils.isEmpty(entry.getValue())) {
+				continue;
+			}
 
-            sb.append(entry.getKey()).append("=");
-            sb.append(URLEncoder.encode(entry.getValue(), encoding));
-            sb.append("&");
-        }
+			sb.append(entry.getKey()).append("=");
+			sb.append(URLEncoder.encode(entry.getValue(), encoding));
+			sb.append("&");
+		}
 
-        return sb.toString();
-    }
+		if (sb.length() > 0) {
+			sb = sb.deleteCharAt(sb.length() - 1);
+		}
+		return sb.toString();
+	}
 
-    public static class HttpResult {
-        final public int code;
-        final public String content;
-        final private Map<String, String> respHeaders;
+	public static class HttpResult {
+		final public int code;
+		final public String content;
+		final private Map<String, String> respHeaders;
 
-        public HttpResult(int code, String content, Map<String, String> respHeaders) {
-            this.code = code;
-            this.content = content;
-            this.respHeaders = respHeaders;
-        }
+		public HttpResult(int code, String content, Map<String, String> respHeaders) {
+			this.code = code;
+			this.content = content;
+			this.respHeaders = respHeaders;
+		}
 
-        public String getHeader(String name) {
-            return respHeaders.get(name);
-        }
-    }
+		public String getHeader(String name) {
+			return respHeaders.get(name);
+		}
+	}
 }
