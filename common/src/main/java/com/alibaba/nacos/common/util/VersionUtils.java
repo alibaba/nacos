099410e0ead40b4ae12fba2646a9f3d7dfaef84b
xuechaos <xchaos8@126.com>
2019-02-27 20:00:05 +0800

    function mode server code completed

---
+++ common/src/main/java/com/alibaba/nacos/common/util/VersionUtils.java
@@ -0,0 +1,31 @@
+package com.alibaba.nacos.common.util;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @author xingxuechao
+ * on:2019/2/27 12:32 PM
+ */
+public class VersionUtils {
+
+    public static String VERSION;
+    /**获取当前version*/
+    public static final String VERSION_DEFAULT = "${pom.version}";
+
+
+    static{
+        try{
+            InputStream in = VersionUtils.class.getClassLoader()
+                .getResourceAsStream("nacos-version.txt");
+            Properties props = new Properties();
+            props.load(in);
+            String val = props.getProperty("version");
+            if (val != null && !VERSION_DEFAULT.equals(val)) {
+                VERSION = val;
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
