35d61c55524dcdec788b4cc1371f7e1293ee4d67
kaitai.zj <nanamikon@gmail.com>
2019-02-28 10:53:51 +0800

    #821 Registering instance should update valid (health)

--- naming/src/main/java/com/alibaba/nacos/naming/web/ApiCommands.java
+++ naming/src/main/java/com/alibaba/nacos/naming/web/ApiCommands.java
@@ -526,7 +526,7 @@ public class ApiCommands {
             cluster = WebUtils.optional(request, "clusterName", UtilsAndCommons.DEFAULT_CLUSTER_NAME);
         }
         boolean enabled = BooleanUtils.toBoolean(WebUtils.optional(request, "enable", "true"));
-        boolean valid = BooleanUtils.toBoolean(WebUtils.optional(request, "valid", "true"));
+        boolean healthy = BooleanUtils.toBoolean(WebUtils.optional(request, "healthy", "true"));
 
         IpAddress ipAddress = new IpAddress();
         ipAddress.setPort(Integer.parseInt(port));
@@ -534,7 +534,7 @@ public class ApiCommands {
         ipAddress.setWeight(Double.parseDouble(weight));
         ipAddress.setClusterName(cluster);
         ipAddress.setEnabled(enabled);
-        ipAddress.setValid(valid);
+        ipAddress.setValid(healthy);
 
         if (!ipAddress.validate()) {
             throw new IllegalArgumentException("malfomed ip config: " + ipAddress);
