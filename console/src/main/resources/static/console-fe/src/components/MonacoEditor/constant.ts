5251b1206380d84ac24cd0d13bb0b0a4ed0cec99
zhichen <zhichen.lc@alibaba-inc.com>
2019-03-13 17:34:01 +0800

    update: standardize the editor of metadata #479

---
+++ console/src/main/resources/static/console-fe/src/components/MonacoEditor/constant.ts
@@ -0,0 +1,14 @@
+export const MONACO_OPTIONS: Object = {
+  codeLens: true,
+  selectOnLineNumbers: true,
+  roundedSelection: false,
+  readOnly: false,
+  lineNumbersMinChars: true,
+  theme: 'vs-dark',
+  wordWrapColumn: 120,
+  folding: true,
+  showFoldingControls: 'always',
+  wordWrap: 'wordWrapColumn',
+  cursorStyle: 'line',
+  automaticLayout: true,
+};
