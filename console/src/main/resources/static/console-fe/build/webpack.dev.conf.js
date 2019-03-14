8329f5296854abb202becd42ee5672d234aa2451
zhichen <zhichen.lc@alibaba-inc.com>
2019-03-14 14:03:47 +0800

    roll back webpack.dev.config.js

--- console/src/main/resources/static/console-fe/build/webpack.dev.conf.js
+++ console/src/main/resources/static/console-fe/build/webpack.dev.conf.js
@@ -26,7 +26,7 @@ module.exports = Object.assign({}, base, {
       context: ['/'],
       changeOrigin: true,
       secure: false,
-      target: 'http://localhost:8848/',
+      target: 'http://localhost:8848',
       pathRewrite: {'^/v1' : '/nacos/v1'}
     }],
     disableHostCheck: true,
