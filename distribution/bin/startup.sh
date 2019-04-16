456d2743da8c3ddc3513599206293215a7e05c09
nkorange <zpf.073@gmail.com>
2019-04-16 09:49:42 +0800

    #1069 Increase the URL length limit for temporary solution

--- distribution/bin/startup.sh
+++ distribution/bin/startup.sh
@@ -106,6 +106,7 @@ JAVA_OPT="${JAVA_OPT} -jar ${BASE_DIR}/target/nacos-server.jar"
 JAVA_OPT="${JAVA_OPT} ${JAVA_OPT_EXT}"
 JAVA_OPT="${JAVA_OPT} --spring.config.location=${CUSTOM_SEARCH_LOCATIONS}"
 JAVA_OPT="${JAVA_OPT} --logging.config=${BASE_DIR}/conf/nacos-logback.xml"
+JAVA_OPT="${JAVA_OPT} --server.max-http-header-size=524288"
 
 if [ ! -d "${BASE_DIR}/logs" ]; then
   mkdir ${BASE_DIR}/logs
