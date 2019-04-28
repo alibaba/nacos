800e8346197e18fdc5e4e31cc1ca2505f088c935
张泽江 <zejiang.zhang@zatech.com>
2019-04-28 15:27:04 +0800

    去掉多余引用

--- config/src/main/java/com/alibaba/nacos/config/server/service/dump/DumpService.java
+++ config/src/main/java/com/alibaba/nacos/config/server/service/dump/DumpService.java
@@ -30,7 +30,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
