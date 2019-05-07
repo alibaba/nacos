7ae757ac02fd11676ceb7090485ce06b4e7470c2
chuntaojun <liaochuntao@live.com>
2019-05-07 11:24:11 +0800

    refactor(config): correct the mistake to Constants.HEALTH_CONTROLLER_PATH

--- config/src/main/java/com/alibaba/nacos/config/server/constant/Constants.java
+++ config/src/main/java/com/alibaba/nacos/config/server/constant/Constants.java
@@ -111,7 +111,7 @@ public class Constants {
 
     public static final String CONFIG_CONTROLLER_PATH = BASE_PATH + "/configs";
 
-    public static final String HEALTH_CONTROLLER_PATH = BASE_PATH + "//health";
+    public static final String HEALTH_CONTROLLER_PATH = BASE_PATH + "/health";
 
     public static final String HISTORY_CONTROLLER_PATH = BASE_PATH + "/history";
 
