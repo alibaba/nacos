bd312da34e9f423880fbea54081843ba7ec375a5
chuntaojun <liaochuntao@live.com>
2019-06-14 21:18:45 +0800

    feat(nacos-api): add config type setting

---
+++ api/src/main/java/com/alibaba/nacos/api/config/ConfigType.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0  = the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.api.config;
+
+/**
+ * @author liaochuntao
+ * @date 2019-06-14 21:12
+ **/
+public class ConfigType {
+
+    /**
+     * config type is "properties"
+     */
+    public static final String PROPERTIES = "properties";
+
+    /**
+     * config type is "xml"
+     */
+    public static final String XML = "xml";
+
+    /**
+     * config type is "json"
+     */
+    public static final String JSON = "json";
+
+    /**
+     * config type is "text"
+     */
+    public static final String TEXT = "text";
+
+    /**
+     * config type is "html"
+     */
+    public static final String HTML = "html";
+
+    /**
+     * config type is "yaml"
+     */
+    public static final String YAML = "yaml";
+
+}
