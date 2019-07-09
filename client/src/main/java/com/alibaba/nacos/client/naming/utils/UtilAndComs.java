65c2f55617e9fe632ef78b7e7af5c22c4e44ed41
nkorange <zpf.073@gmail.com>
2019-07-09 11:04:00 +0800

    Fix standalone use derby failed bug

--- client/src/main/java/com/alibaba/nacos/client/naming/utils/UtilAndComs.java
+++ client/src/main/java/com/alibaba/nacos/client/naming/utils/UtilAndComs.java
@@ -20,7 +20,7 @@ package com.alibaba.nacos.client.naming.utils;
  */
 public class UtilAndComs {
 
-    public static final String VERSION = "Nacos-Java-Client:v1.0.1";
+    public static final String VERSION = "Nacos-Java-Client:v1.1.1";
 
     public static String WEB_CONTEXT = "/nacos";
 
