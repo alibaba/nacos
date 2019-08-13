037a3d1741ae84e68fec01595a75e38e77bf8aa1
chuntaojun <liaochuntao@live.com>
2019-08-13 21:41:26 +0800

    feat:

---
+++ core/src/main/java/com/alibaba/nacos/core/TestScan.java
@@ -0,0 +1,11 @@
+package com.alibaba.nacos.core;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author <a href="mailto:liaochunyhm@live.com">liaochuntao</a>
+ * @since
+ */
+@Component
+public class TestScan {
+}
