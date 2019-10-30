0b68a580f456e63d15b1086ae87224e04422ceb2
yshen <2106678195@qq.com>
2019-10-30 15:37:06 +0800

    add cors support

---
+++ console/src/main/java/com/alibaba/nacos/console/config/CqrsConfig.java
@@ -0,0 +1,31 @@
+package com.alibaba.nacos.console.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Spring cors config
+ *
+ * @author yshen
+ */
+@Configuration
+public class CqrsConfig {
+
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setMaxAge(18000L);
+        config.addAllowedMethod("*");
+        return new CorsFilter(source);
+    }
+
+}
