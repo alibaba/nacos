1082539d89352333e4c8eecc81bf577a7b7aa83a
shenggangshu <gangshu.sgs@gongdao.com>
2019-12-03 18:45:10 +0800

    refactor #2134 optimization code

--- config/src/main/java/com/alibaba/nacos/config/server/service/PersistService.java
+++ config/src/main/java/com/alibaba/nacos/config/server/service/PersistService.java
@@ -865,7 +865,7 @@ public class PersistService {
                     ps.setString(index++, dataId);
                     ps.setString(index++, group);
                     ps.setString(index++, tenantTmp);
-                    ps.setString(index++, datumId);
+                    ps.setString(index, datumId);
                 }
             });
         } catch (CannotGetJdbcConnectionException e) {
@@ -888,7 +888,7 @@ public class PersistService {
                     int index = 1;
                     ps.setString(index++, dataId);
                     ps.setString(index++, group);
-                    ps.setString(index++, tenantTmp);
+                    ps.setString(index, tenantTmp);
                 }
             });
         } catch (CannotGetJdbcConnectionException e) {
@@ -947,7 +947,7 @@ public class PersistService {
         if (result == null) {
             throw new IllegalArgumentException("configInfoBetaCount error");
         }
-        return result.intValue();
+        return result;
     }
 
     /**
@@ -993,7 +993,7 @@ public class PersistService {
             if (isPublishOk == null) {
                 return false;
             }
-            return isPublishOk.booleanValue();
+            return isPublishOk;
         } catch (TransactionException e) {
             fatalLog.error("[db-error] " + e.toString(), e);
             return false;
@@ -1035,7 +1035,7 @@ public class PersistService {
             if (isReplaceOk == null) {
                 return false;
             }
-            return isReplaceOk.booleanValue();
+            return isReplaceOk;
         } catch (TransactionException e) {
             fatalLog.error("[db-error] " + e.toString(), e);
             return false;
@@ -1632,7 +1632,7 @@ public class PersistService {
         if (result == null) {
             throw new IllegalArgumentException("configInfoCount error");
         }
-        return result.intValue();
+        return result;
     }
 
     /**
@@ -1644,7 +1644,7 @@ public class PersistService {
         if (result == null) {
             throw new IllegalArgumentException("configInfoCount error");
         }
-        return result.intValue();
+        return result;
     }
 
     /**
@@ -1656,7 +1656,7 @@ public class PersistService {
         if (result == null) {
             throw new IllegalArgumentException("configInfoBetaCount error");
         }
-        return result.intValue();
+        return result;
     }
 
     /**
@@ -1668,7 +1668,7 @@ public class PersistService {
         if (result == null) {
             throw new IllegalArgumentException("configInfoBetaCount error");
         }
-        return result.intValue();
+        return result;
     }
 
     public List<String> getTenantIdList(int page, int pageSize) {
@@ -1690,7 +1690,7 @@ public class PersistService {
         if (result == null) {
             throw new IllegalArgumentException("aggrConfigInfoCount error");
         }
-        return result.intValue();
+        return result;
     }
 
     public int aggrConfigInfoCountIn(String dataId, String group, String tenant, List<String> datumIds) {
@@ -2848,7 +2848,7 @@ public class PersistService {
                 sql.append(", ");
             }
             sql.append("?");
-            paramList.add(Long.valueOf(tagArr[i]));
+            paramList.add(Long.parseLong(tagArr[i]));
         }
         sql.append(") ");
         try {
@@ -2957,7 +2957,7 @@ public class PersistService {
                 sql.append(", ");
             }
             sql.append("?");
-            paramList.add(Long.valueOf(tagArr[i]));
+            paramList.add(Long.parseLong(tagArr[i]));
         }
         sql.append(") ");
         try {
@@ -3562,7 +3562,7 @@ public class PersistService {
         if (result == null) {
             return 0;
         }
-        return result.intValue();
+        return result;
     }
 
 
--- naming/src/main/java/com/alibaba/nacos/naming/core/Instance.java
+++ naming/src/main/java/com/alibaba/nacos/naming/core/Instance.java
@@ -322,8 +322,9 @@ public class Instance extends com.alibaba.nacos.api.naming.pojo.Instance impleme
         while (currentInstanceIds.contains(String.valueOf(id))) {
             id++;
         }
-        currentInstanceIds.add(String.valueOf(id));
-        return String.valueOf(id);
+        String idStr = String.valueOf(id);
+        currentInstanceIds.add(idStr);
+        return idStr;
     }
 
     public void validate() throws NacosException {
