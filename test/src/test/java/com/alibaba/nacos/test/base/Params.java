3e312e7537977d3522941a6467d2d09037b3ea5e
nkorange <zpf.073@gmail.com>
2020-01-06 16:02:32 +0800

    #1105 Add test cases

--- test/src/test/java/com/alibaba/nacos/test/base/Params.java
+++ test/src/test/java/com/alibaba/nacos/test/base/Params.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba.nacos.test.naming;
+package com.alibaba.nacos.test.base;
 
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
---
+++ test/src/test/java/com/alibaba/nacos/test/core/auth/Permission_ITCase.java
@@ -0,0 +1,4 @@
+package com.alibaba.nacos.test.core.auth;
+
+public class Permission_ITCase {
+}
