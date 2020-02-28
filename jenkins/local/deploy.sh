c7af3b3837827265b61126ffffac63a67198c8d2
梁广隆 <499508387@qq.com>
2020-02-28 18:20:24 +0800

    ci：添加local环境的jenkins脚本

---
+++ jenkins/local/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+let_time=$(date "+%Y%m%d")
+
+imageTag="msmp"
+projectName="msmp-nacos"
+envName="local"
+
+cd /root/dockerfile/${envName}/${projectName}/
+
+containerName=$(docker ps -a|grep ${projectName}-${envName}|awk '{print $1}')
+if [ "$containerName" != "" ] ; then
+echo 删除容器 $containerName ...
+docker rm -f $containerName
+fi
+echo 删除镜像
+docker rmi -f ${imageTag}/${projectName}:${envName}
+echo 运行新的容器 ...
+
+docker build -t ${imageTag}/${projectName}:${envName} .
+
+# 后端
+docker run -d --net=host -p 8849:8848 --name ${projectName}-${envName} ${imageTag}/${projectName}:${envName}
