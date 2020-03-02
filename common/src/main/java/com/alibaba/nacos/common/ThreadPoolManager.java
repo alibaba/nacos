536c37dfa9e25855b0b9ca6de5ea8fa6f60131d9
chuntaojun <liaochuntao@live.com>
2020-03-02 14:15:28 +0800

    fix(common): Fixing concurrency issues

--- common/src/main/java/com/alibaba/nacos/common/ThreadPoolManager.java
+++ common/src/main/java/com/alibaba/nacos/common/ThreadPoolManager.java
@@ -17,13 +17,18 @@
 package com.alibaba.nacos.common;
 
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * // TODO Access Metric
+ *
  * For unified management of thread pool resources, the consumer can simply call
  * the register method to {@link ThreadPoolManager#register(String, String, ExecutorService)}
  * the thread pool that needs to be included in the life cycle management of the resource
@@ -58,65 +63,102 @@ public final class ThreadPoolManager {
         resourcesManager = new ConcurrentHashMap<String, Map<String, Set<ExecutorService>>>(8);
     }
 
-	/**
-	 * Register the thread pool resources with the resource manager
-	 *
-     * @param biz business name
-	 * @param resourceName resource name
-	 * @param executor {@link ExecutorService}
-	 */
-	public synchronized void register(String biz, String resourceName, ExecutorService executor) {
-        if (!resourcesManager.containsKey(biz)) {
-            resourcesManager.put(biz, new ConcurrentHashMap<String, Set<ExecutorService>>(8));
-            lockers.put(biz, new Object());
+    public Map<String, Map<String, Map<String, Object>>> getThreadPoolsInfo() {
+        Map<String, Map<String, Map<String, Object>>> info = new HashMap<String, Map<String, Map<String, Object>>>(4);
+        for (Map.Entry<String, Map<String, Set<ExecutorService>>> entry : resourcesManager.entrySet()) {
+            Map<String, Map<String, Object>> subInfo = new HashMap<String, Map<String, Object>>(8);
+            info.put(entry.getKey(), subInfo);
+            for (Map.Entry<String, Set<ExecutorService>> mapEntry : entry.getValue().entrySet()) {
+                Map<String, Object> details = new HashMap<String, Object>(8);
+                subInfo.put(mapEntry.getKey(), details);
+                for (ExecutorService service : mapEntry.getValue()) {
+                    if (service instanceof ThreadPoolExecutor) {
+                        ThreadPoolExecutor executor = (ThreadPoolExecutor) service;
+                        details.put("activeCount", executor.getActiveCount());
+                        details.put("corePoolSize", executor.getCorePoolSize());
+                        details.put("maximumPoolSiz", executor.getMaximumPoolSize());
+                        details.put("workCount", executor.getQueue().size());
+                    }
+                }
+            }
         }
-        resourcesManager.get(biz).get(resourceName).add(executor);
+        return info;
     }
 
-	/**
-	 * Cancel the uniform lifecycle management for all threads under this resource
-	 *
+    /**
+     * Register the thread pool resources with the resource manager
+     *
      * @param biz business name
-	 * @param resourceName resource name
-	 */
-	public synchronized void deregister(String biz, String resourceName) {
-        if (resourcesManager.containsKey(biz)) {
-            resourcesManager.get(biz).remove(resourceName);
+     * @param resourceName resource name
+     * @param executor {@link ExecutorService}
+     */
+    public void register(String biz, String resourceName, ExecutorService executor) {
+        synchronized(this) {
+            if (!resourcesManager.containsKey(biz)) {
+                resourcesManager.put(biz, new HashMap<String, Set<ExecutorService>>(8));
+                lockers.put(biz, new Object());
+            }
+        }
+        final Object monitor = lockers.get(biz);
+        synchronized (monitor) {
+            Map<String, Set<ExecutorService>> map = resourcesManager.get(biz);
+            if (!map.containsKey(resourceName)) {
+                map.put(resourceName, new HashSet<ExecutorService>());
+            }
+            map.get(resourceName).add(executor);
         }
     }
 
-	/**
-	 * Undoing the uniform lifecycle management of {@link ExecutorService} under this resource
-	 *
+    /**
+     * Cancel the uniform lifecycle management for all threads under this resource
+     *
      * @param biz business name
-	 * @param resourceName resource name
-	 * @param executor {@link ExecutorService}
-	 */
-	public synchronized void deregister(String biz, String resourceName, ExecutorService executor) {
+     * @param resourceName resource name
+     */
+    public void deRegister(String biz, String resourceName) {
         if (resourcesManager.containsKey(biz)) {
-            final Map<String, Set<ExecutorService>> subResourceMap = resourcesManager.get(biz);
-            if (subResourceMap.containsKey(resourceName)) {
-                subResourceMap.get(resourceName).remove(executor);
+            final Object monitor = lockers.get(biz);
+            synchronized (monitor) {
+                resourcesManager.get(biz).remove(resourceName);
             }
         }
     }
 
-    public synchronized void destroy(String biz) {
-	    final Object monitor = lockers.get(biz);
-	    if (monitor == null) {
-	        return;
+    /**
+     * Undoing the uniform lifecycle management of {@link ExecutorService} under this resource
+     *
+     * @param biz business name
+     * @param resourceName resource name
+     * @param executor {@link ExecutorService}
+     */
+    public void deRegister(String biz, String resourceName, ExecutorService executor) {
+        if (resourcesManager.containsKey(biz)) {
+            final Object monitor = lockers.get(biz);
+            synchronized (monitor) {
+                final Map<String, Set<ExecutorService>> subResourceMap = resourcesManager.get(biz);
+                if (subResourceMap.containsKey(resourceName)) {
+                    subResourceMap.get(resourceName).remove(executor);
+                }
+            }
         }
-	    synchronized (monitor) {
-	        Map<String, Set<ExecutorService>> subResource = resourcesManager.get(biz);
-	        for (Map.Entry<String, Set<ExecutorService>> entry : subResource.entrySet()) {
-	            for (ExecutorService executor : entry.getValue()) {
-	                executor.shutdown();
-	                int retry = 3;
-	                while (retry > 0) {
-	                    retry --;
-	                    try {
-	                        if (!executor.awaitTermination(30, TimeUnit.SECONDS)) {
-	                            executor.shutdownNow();
+    }
+
+    public void destroy(String biz) {
+        final Object monitor = lockers.get(biz);
+        if (monitor == null) {
+            return;
+        }
+        synchronized (monitor) {
+            Map<String, Set<ExecutorService>> subResource = resourcesManager.get(biz);
+            for (Map.Entry<String, Set<ExecutorService>> entry : subResource.entrySet()) {
+                for (ExecutorService executor : entry.getValue()) {
+                    executor.shutdown();
+                    int retry = 3;
+                    while (retry > 0) {
+                        retry --;
+                        try {
+                            if (!executor.awaitTermination(1, TimeUnit.SECONDS)) {
+                                executor.shutdownNow();
                             }
                         } catch (Exception e) {
                             executor.shutdownNow();
@@ -124,13 +166,16 @@ public final class ThreadPoolManager {
                     }
                 }
             }
+
+            resourcesManager.get(biz).clear();
+
         }
     }
 
     private void destroyAll() {
-	    Set<String> bizs = resourcesManager.keySet();
-	    for (String biz : bizs) {
-	        destroy(biz);
+        Set<String> bizs = resourcesManager.keySet();
+        for (String biz : bizs) {
+            destroy(biz);
         }
     }
 }
---
+++ common/src/test/java/com/alibaba/nacos/common/ThreadPoolManagerTest.java
@@ -0,0 +1,22 @@
+package com.alibaba.nacos.common;
+
+import java.util.Map;
+import java.util.concurrent.Executors;
+import org.junit.Test;
+
+public class ThreadPoolManagerTest {
+
+    private static final String biz = "nacos";
+    private static final String resourceName = "naming";
+
+    @Test
+    public void test_thread_pool_manager() {
+        ThreadPoolManager manager = ThreadPoolManager.getInstance();
+        manager.register(biz, resourceName, Executors.newFixedThreadPool(1));
+
+        Map<String, Map<String, Map<String, Object>>> stringMapMap = manager.getThreadPoolsInfo();
+        System.out.println(stringMapMap);
+
+    }
+
+}
