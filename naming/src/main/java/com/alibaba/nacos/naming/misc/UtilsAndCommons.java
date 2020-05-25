4607bb7c009450a19b7d4357ebc88196d238cecd
KomachiSion <263976490@qq.com>
2020-05-25 15:48:52 +0800

    Replace fastjson with jackson for Selector

--- naming/src/main/java/com/alibaba/nacos/naming/misc/UtilsAndCommons.java
+++ naming/src/main/java/com/alibaba/nacos/naming/misc/UtilsAndCommons.java
@@ -17,14 +17,10 @@ package com.alibaba.nacos.naming.misc;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
-import com.alibaba.fastjson.parser.ParserConfig;
-import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.common.utils.VersionUtils;
 import com.alibaba.nacos.core.utils.ApplicationUtils;
-import com.alibaba.nacos.naming.selector.Selector;
-import com.alibaba.nacos.naming.selector.SelectorJsonAdapter;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
@@ -127,12 +123,6 @@ public class UtilsAndCommons {
     public static final Executor RAFT_PUBLISH_EXECUTOR;
 
     static {
-
-        // custom serializer and deserializer for fast-json
-        SerializeConfig.getGlobalInstance()
-            .put(Selector.class, SelectorJsonAdapter.getInstance());
-        ParserConfig.getGlobalInstance()
-            .putDeserializer(Selector.class, SelectorJsonAdapter.getInstance());
 
         // write null values, otherwise will cause compatibility issues
         JSON.DEFAULT_GENERATE_FEATURE |= SerializerFeature.WriteNullStringAsEmpty.getMask();
--- naming/src/main/java/com/alibaba/nacos/naming/selector/LabelSelector.java
+++ naming/src/main/java/com/alibaba/nacos/naming/selector/LabelSelector.java
@@ -21,8 +21,12 @@ import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.selector.ExpressionSelector;
 import com.alibaba.nacos.api.selector.SelectorType;
 import com.alibaba.nacos.cmdb.service.CmdbReader;
+import com.alibaba.nacos.common.utils.JacksonUtils;
 import com.alibaba.nacos.core.utils.ApplicationUtils;
 import com.alibaba.nacos.naming.core.Instance;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
@@ -55,6 +59,7 @@ import java.util.Set;
  * @see CmdbReader
  * @since 0.7.0
  */
+@JsonTypeInfo(use = Id.NAME, property = "type")
 public class LabelSelector extends ExpressionSelector implements Selector {
 
     /**
@@ -79,6 +84,7 @@ public class LabelSelector extends ExpressionSelector implements Selector {
     static {
         SUPPORTED_INNER_CONNCETORS.add(String.valueOf(CEQUAL));
         SUPPORTED_OUTER_CONNCETORS.add(String.valueOf(CAND));
+        JacksonUtils.registerSubtype(LabelSelector.class, SelectorType.label.name());
     }
 
     public Set<String> getLabels() {
@@ -90,7 +96,7 @@ public class LabelSelector extends ExpressionSelector implements Selector {
     }
 
     public LabelSelector() {
-        setType(SelectorType.label.name());
+        super();
     }
 
     private CmdbReader getCmdbReader() {
