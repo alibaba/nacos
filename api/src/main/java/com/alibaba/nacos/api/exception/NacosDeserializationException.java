070e6ae146ff29ba3ce3787355dd20d73150dc31
KomachiSion <263976490@qq.com>
2020-05-26 11:51:47 +0800

    Handle serialization exception in JacksonUtils

---
+++ api/src/main/java/com/alibaba/nacos/api/exception/NacosDeserializationException.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.api.exception;
+
+/**
+ * Nacos deserialization exception.
+ *
+ * @author yangyi
+ */
+public class NacosDeserializationException extends RuntimeException {
+
+    private static final long serialVersionUID = -2742350751684273728L;
+
+    private static final String DEFAULT_MSG = "Nacos deserialize failed. ";
+
+    private static final String MSG_FOR_SPECIFIED_CLASS = "Nacos deserialize for class [%s] failed. ";
+
+    private Class<?> targetClass;
+
+    public NacosDeserializationException() {
+    }
+
+    public NacosDeserializationException(Class<?> targetClass) {
+        super(String.format(MSG_FOR_SPECIFIED_CLASS, targetClass.getName()));
+        this.targetClass = targetClass;
+    }
+
+    public NacosDeserializationException(Throwable throwable) {
+        super(DEFAULT_MSG, throwable);
+    }
+
+    public NacosDeserializationException(Class<?> targetClass, Throwable throwable) {
+        super(String.format(MSG_FOR_SPECIFIED_CLASS, targetClass.getName()), throwable);
+        this.targetClass = targetClass;
+    }
+}
