d86c6aa940900876830f29472772431cb3f62963
ZShUn <616394599@qq.com>
2020-06-08 19:55:49 +0800

    1.添加License

--- config/src/main/java/com/alibaba/nacos/config/server/configuration/NacosMultipleDataSourceProperties.java
+++ config/src/main/java/com/alibaba/nacos/config/server/configuration/NacosMultipleDataSourceProperties.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.nacos.config.server.configuration;
 
 import com.alibaba.nacos.config.server.configuration.datasource.DataSourceType;
@@ -8,8 +23,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
- * @author zhangshun
- * @date: 2020/1/18 17:32
+ * @author Nacos
  */
 @Component
 @Data
--- config/src/main/java/com/alibaba/nacos/config/server/service/capacity/GroupCapacityPersistServiceTmp.java
+++ config/src/main/java/com/alibaba/nacos/config/server/service/capacity/GroupCapacityPersistServiceTmp.java
@@ -1,9 +1,23 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.nacos.config.server.service.capacity;
 
 import com.alibaba.nacos.config.server.modules.entity.*;
 import com.alibaba.nacos.config.server.modules.repository.ConfigInfoRepository;
 import com.alibaba.nacos.config.server.modules.repository.GroupCapacityRepository;
-import com.alibaba.nacos.config.server.modules.repository.TenantCapacityRepository;
 import com.querydsl.core.BooleanBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,9 +29,9 @@ import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
-import static com.alibaba.nacos.config.server.service.capacity.GroupCapacityPersistService.CLUSTER;
-
-
+/**
+ * @author Nacos
+ */
 @Service
 public class GroupCapacityPersistServiceTmp {
 
