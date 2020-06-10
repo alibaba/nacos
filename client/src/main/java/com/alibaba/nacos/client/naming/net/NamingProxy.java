25eeb525ee00d3dd4b0532cff358f643ea47bde4
huqingfeng <1397649957@qq.com>
2020-06-10 17:42:17 +0800

    a power of two size for hashmap

--- client/src/main/java/com/alibaba/nacos/client/naming/net/NamingProxy.java
+++ client/src/main/java/com/alibaba/nacos/client/naming/net/NamingProxy.java
@@ -197,7 +197,7 @@ public class NamingProxy {
         NAMING_LOGGER.info("[REGISTER-SERVICE] {} registering service {} with instance: {}",
             namespaceId, serviceName, instance);
 
-        final Map<String, String> params = new HashMap<String, String>(9);
+        final Map<String, String> params = new HashMap<String, String>(16);
         params.put(CommonParams.NAMESPACE_ID, namespaceId);
         params.put(CommonParams.SERVICE_NAME, serviceName);
         params.put(CommonParams.GROUP_NAME, groupName);
