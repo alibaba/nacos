f8648801364c937715c16ab99f59670cc780ddfc
杨翊 SionYang <37170243+KomachiSion@users.noreply.github.com>
2020-06-18 17:05:15 +0800

    [ISSUE #2992] use new code style for nacos-common (#3106)
    * use new code style for nacos-common
    
    * Fix code style file can't set java import problem.

--- common/src/main/java/com/alibaba/nacos/common/http/HttpClientConfig.java
+++ common/src/main/java/com/alibaba/nacos/common/http/HttpClientConfig.java
@@ -17,61 +17,63 @@
 package com.alibaba.nacos.common.http;
 
 /**
- * http client config build
+ * http client config build.
  *
  * @author mai.jh
- * @date 2020/6/14
  */
 public class HttpClientConfig {
-
-    private int conTimeOutMillis;
-
-    private int readTimeOutMillis;
-
-    private int maxRedirects;
-
+    
+    private final int conTimeOutMillis;
+    
+    private final int readTimeOutMillis;
+    
+    private final int maxRedirects;
+    
     public HttpClientConfig(int conTimeOutMillis, int readTimeOutMillis, int maxRedirects) {
         this.conTimeOutMillis = conTimeOutMillis;
         this.readTimeOutMillis = readTimeOutMillis;
         this.maxRedirects = maxRedirects;
     }
-
+    
     public int getConTimeOutMillis() {
         return conTimeOutMillis;
     }
-
+    
     public int getReadTimeOutMillis() {
         return readTimeOutMillis;
     }
-
+    
     public int getMaxRedirects() {
         return maxRedirects;
     }
-
+    
     public static HttpClientConfigBuilder builder() {
         return new HttpClientConfigBuilder();
     }
-
+    
     public static final class HttpClientConfigBuilder {
+        
         private int conTimeOutMillis = -1;
+        
         private int readTimeOutMillis = -1;
+        
         private int maxRedirects = 50;
-
+        
         public HttpClientConfigBuilder setConTimeOutMillis(int conTimeOutMillis) {
             this.conTimeOutMillis = conTimeOutMillis;
             return this;
         }
-
+        
         public HttpClientConfigBuilder setReadTimeOutMillis(int readTimeOutMillis) {
             this.readTimeOutMillis = readTimeOutMillis;
             return this;
         }
-
+        
         public HttpClientConfigBuilder setMaxRedirects(int maxRedirects) {
             this.maxRedirects = maxRedirects;
             return this;
         }
-
+        
         public HttpClientConfig build() {
             return new HttpClientConfig(conTimeOutMillis, readTimeOutMillis, maxRedirects);
         }
