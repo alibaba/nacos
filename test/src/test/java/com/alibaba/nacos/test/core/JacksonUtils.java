d06de26ed21bdf84d73c4faccb106f89467127fa
杨翊 SionYang <37170243+KomachiSion@users.noreply.github.com>
2020-06-19 10:55:14 +0800

    upgrade version of Jackson and Guava (#3116)

--- test/src/test/java/com/alibaba/nacos/test/core/JacksonUtils.java
+++ test/src/test/java/com/alibaba/nacos/test/core/JacksonUtils.java
@@ -14,22 +14,7 @@
  * limitations under the License.
  */
 
-package com.alibaba.nacos.test.core;/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package com.alibaba.nacos.test.core;
 
 import com.alibaba.nacos.consistency.ProtocolMetaData;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -67,7 +52,7 @@ public class JacksonUtils {
 		return mapper.readValue(s, typeReference);
 	}
 
-	public static <T> T deserializeObject(InputStream src, TypeReference<?> typeReference)
+	public static <T> T deserializeObject(InputStream src, TypeReference<T> typeReference)
 			throws IOException {
 		return mapper.readValue(src, typeReference);
 	}
