a2088ec7e663ba9c5a8a037c43aeb312b7d82247
杨翊 SionYang <37170243+KomachiSion@users.noreply.github.com>
2020-06-29 13:55:42 +0800

    Use new code style for nacos-console module. (#3195)

--- console/src/main/java/com/alibaba/nacos/console/model/Namespace.java
+++ console/src/main/java/com/alibaba/nacos/console/model/Namespace.java
@@ -13,51 +13,53 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.alibaba.nacos.console.model;
 
 /**
- * Namespace
+ * Namespace.
  *
  * @author diamond
  */
 public class Namespace {
-
+    
     private String namespace;
-
+    
     private String namespaceShowName;
-
+    
     private int quota;
-
+    
     private int configCount;
+    
     /**
-     * 0 : Global configuration， 1 : Default private namespace ，2 : Custom namespace
+     * 0 : Global configuration， 1 : Default private namespace ，2 : Custom namespace.
      */
     private int type;
-
+    
     public String getNamespaceShowName() {
         return namespaceShowName;
     }
-
+    
     public void setNamespaceShowName(String namespaceShowName) {
         this.namespaceShowName = namespaceShowName;
     }
-
+    
     public String getNamespace() {
         return namespace;
     }
-
+    
     public void setNamespace(String namespace) {
         this.namespace = namespace;
     }
-
+    
     public Namespace() {
     }
-
+    
     public Namespace(String namespace, String namespaceShowName) {
         this.namespace = namespace;
         this.namespaceShowName = namespaceShowName;
     }
-
+    
     public Namespace(String namespace, String namespaceShowName, int quota, int configCount, int type) {
         this.namespace = namespace;
         this.namespaceShowName = namespaceShowName;
@@ -65,29 +67,29 @@ public class Namespace {
         this.configCount = configCount;
         this.type = type;
     }
-
+    
     public int getQuota() {
         return quota;
     }
-
+    
     public void setQuota(int quota) {
         this.quota = quota;
     }
-
+    
     public int getConfigCount() {
         return configCount;
     }
-
+    
     public void setConfigCount(int configCount) {
         this.configCount = configCount;
     }
-
+    
     public int getType() {
         return type;
     }
-
+    
     public void setType(int type) {
         this.type = type;
     }
-
+    
 }
