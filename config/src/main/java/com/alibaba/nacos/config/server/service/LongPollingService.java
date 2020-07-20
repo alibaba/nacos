590e704a317443caa3077e10549dffee7c1e6ca2
shiyiyue1102 <liuzunfei@gmail.com>
2020-07-20 15:41:32 +0800

    Add gprc support->merge problem fix

--- config/src/main/java/com/alibaba/nacos/config/server/service/LongPollingService.java
+++ config/src/main/java/com/alibaba/nacos/config/server/service/LongPollingService.java
@@ -31,10 +31,6 @@ import com.alibaba.nacos.config.server.utils.GroupKey;
 import com.alibaba.nacos.config.server.utils.LogUtil;
 import com.alibaba.nacos.config.server.utils.MD5Util;
 import com.alibaba.nacos.config.server.utils.RequestUtil;
-import com.alibaba.nacos.config.server.utils.event.EventDispatcher.AbstractEventListener;
-import com.alibaba.nacos.config.server.utils.event.EventDispatcher.Event;
-
-import com.alibaba.nacos.core.remote.DataChangeListenerNotifier;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
