7c5d94c4209518588f8a7be6b27cdf9589296657
KomachiSion <263976490@qq.com>
2020-07-29 17:14:28 +0800

    Refactor naming client redo when reconnect

--- client/src/main/java/com/alibaba/nacos/client/naming/remote/gprc/NamingGrpcConnectionEventListener.java
+++ client/src/main/java/com/alibaba/nacos/client/naming/remote/gprc/NamingGrpcConnectionEventListener.java
@@ -17,51 +17,116 @@
 package com.alibaba.nacos.client.naming.remote.gprc;
 
 import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.pojo.Instance;
 import com.alibaba.nacos.api.naming.pojo.ServiceInfo;
 import com.alibaba.nacos.api.naming.utils.NamingUtils;
-import com.alibaba.nacos.client.naming.cache.ServiceInfoHolder;
 import com.alibaba.nacos.client.remote.ConnectionEventListener;
 import com.alibaba.nacos.client.utils.LogUtils;
+import com.alibaba.nacos.common.utils.ConcurrentHashSet;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Naming client gprc connection event listener.
+ *
+ * <p>
+ * When connection reconnect to server, redo the register and subscribe.
+ * </p>
  *
  * @author xiweng.yy
  */
 public class NamingGrpcConnectionEventListener implements ConnectionEventListener {
     
-    private final ServiceInfoHolder serviceInfoHolder;
-    
     private final NamingGrpcClientProxy clientProxy;
     
-    public NamingGrpcConnectionEventListener(ServiceInfoHolder serviceInfoHolder, NamingGrpcClientProxy clientProxy) {
-        this.serviceInfoHolder = serviceInfoHolder;
+    private final ConcurrentMap<String, Set<Instance>> registeredInstanceCached = new ConcurrentHashMap<String, Set<Instance>>();
+    
+    private final Set<String> subscribes = new ConcurrentHashSet<String>();
+    
+    public NamingGrpcConnectionEventListener(NamingGrpcClientProxy clientProxy) {
         this.clientProxy = clientProxy;
     }
     
     @Override
     public void onConnected() {
-    
     }
     
     @Override
     public void onReconnected() {
-        for (Map.Entry<String, ServiceInfo> each : serviceInfoHolder.getServiceInfoMap().entrySet()) {
-            ServiceInfo serviceInfo = each.getValue();
-            String serviceName = NamingUtils.getServiceName(serviceInfo.getName());
-            String groupName = NamingUtils.getGroupName(serviceInfo.getName());
+        redoSubscribe();
+        redoRegisterEachService();
+    }
+    
+    private void redoSubscribe() {
+        for (String each : subscribes) {
+            ServiceInfo serviceInfo = ServiceInfo.fromKey(each);
             try {
-                clientProxy.subscribe(serviceName, groupName, serviceInfo.getClusters());
+                clientProxy.subscribe(serviceInfo.getName(), serviceInfo.getGroupName(), serviceInfo.getClusters());
             } catch (NacosException e) {
                 LogUtils.NAMING_LOGGER.warn(String.format("re subscribe service %s failed", serviceInfo.getName()), e);
             }
         }
     }
     
+    private void redoRegisterEachService() {
+        for (Map.Entry<String, Set<Instance>> each : registeredInstanceCached.entrySet()) {
+            String serviceName = NamingUtils.getServiceName(each.getKey());
+            String groupName = NamingUtils.getGroupName(each.getKey());
+            redoRegisterEachInstance(serviceName, groupName, each.getValue());
+        }
+    }
+    
+    private void redoRegisterEachInstance(String serviceName, String groupName, Set<Instance> instances) {
+        for (Instance each : instances) {
+            try {
+                clientProxy.registerService(serviceName, groupName, each);
+            } catch (NacosException e) {
+                LogUtils.NAMING_LOGGER
+                        .warn(String.format("redo register for service %s@@%s failed", groupName, serviceName), e);
+            }
+        }
+    }
+    
     @Override
     public void onDisConnect() {
+    }
     
+    /**
+     * Cache registered instance for redo.
+     *
+     * @param serviceName service name
+     * @param groupName   group name
+     * @param instance    registered instance
+     */
+    public void cacheInstanceForRedo(String serviceName, String groupName, Instance instance) {
+        String key = NamingUtils.getGroupedName(serviceName, groupName);
+        registeredInstanceCached.putIfAbsent(key, new ConcurrentHashSet<Instance>());
+        registeredInstanceCached.get(key).add(instance);
+    }
+    
+    /**
+     * Remove registered instance for redo.
+     *
+     * @param serviceName service name
+     * @param groupName   group name
+     * @param instance    registered instance
+     */
+    public void removeInstanceForRedo(String serviceName, String groupName, Instance instance) {
+        String key = NamingUtils.getGroupedName(serviceName, groupName);
+        Set<Instance> instances = registeredInstanceCached.get(key);
+        if (null != instances) {
+            instances.remove(instance);
+        }
+    }
+    
+    public void cacheSubscriberForRedo(String fullServiceName, String cluster) {
+        subscribes.add(ServiceInfo.getKey(fullServiceName, cluster));
+    }
+    
+    public void removeSubscriberForRedo(String fullServiceName, String cluster) {
+        subscribes.remove(ServiceInfo.getKey(fullServiceName, cluster));
     }
 }
