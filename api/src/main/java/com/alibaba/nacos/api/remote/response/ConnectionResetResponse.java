cc05afb71e86466c907c1238a85b9b588d6ed16d
shiyiyue1102 <liuzunfei@gmail.com>
2020-08-07 10:15:42 +0800

    Add gprc support-> rpc Rsocket submit

---
+++ api/src/main/java/com/alibaba/nacos/api/remote/response/ConnectionResetResponse.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 1999-2020 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.api.remote.response;
+
+/**
+ * response for connection reset.
+ *
+ * @author liuzunfei
+ * @version $Id: ConnectionResetResponse.java, v 0.1 2020年08月06日 1:40 PM liuzunfei Exp $
+ */
+public class ConnectionResetResponse extends Response {
+    
+    @Override
+    public String getType() {
+        return ResponseTypeConstants.CONNECTION_RESET;
+    }
+}
