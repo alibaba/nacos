a864f29d633fe7cc01c975f2e59901a5ddd2dc00
Gagharv <wwfortunate@gmail.com>
2020-08-24 18:35:38 +0800

    [ISSUE#3315]Nacos client support https (#3654)
    * [ISSUE #3315] nacos client support https
    * common module add tls related classes
    * JdkHttpClientRequest support https
    * unified IpUtils
    
    * [ISSUE #3315] nacos client support https
    * common module add tls related classes
    * JdkHttpClientRequest support https
    * unified IpUtils
    
    * [ISSUE #3315] nacos client support https
    
    * format code

--- common/src/main/java/com/alibaba/nacos/common/http/AbstractHttpClientFactory.java
+++ common/src/main/java/com/alibaba/nacos/common/http/AbstractHttpClientFactory.java
@@ -20,9 +20,21 @@ import com.alibaba.nacos.common.http.client.NacosAsyncRestTemplate;
 import com.alibaba.nacos.common.http.client.NacosRestTemplate;
 import com.alibaba.nacos.common.http.client.request.DefaultAsyncHttpClientRequest;
 import com.alibaba.nacos.common.http.client.request.JdkHttpClientRequest;
+import com.alibaba.nacos.common.tls.SelfHostnameVerifier;
+import com.alibaba.nacos.common.tls.TlsFileWatcher;
+import com.alibaba.nacos.common.tls.TlsHelper;
+import com.alibaba.nacos.common.tls.TlsSystemConfig;
+import com.alibaba.nacos.common.utils.BiConsumer;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.slf4j.Logger;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * AbstractHttpClientFactory Let the creator only specify the http client config.
@@ -34,7 +46,23 @@ public abstract class AbstractHttpClientFactory implements HttpClientFactory {
     @Override
     public final NacosRestTemplate createNacosRestTemplate() {
         HttpClientConfig httpClientConfig = buildHttpClientConfig();
-        return new NacosRestTemplate(assignLogger(), new JdkHttpClientRequest(httpClientConfig));
+        final JdkHttpClientRequest clientRequest = new JdkHttpClientRequest(httpClientConfig);
+        
+        // enable ssl
+        initTls(new BiConsumer<SSLContext, HostnameVerifier>() {
+            @Override
+            public void accept(SSLContext sslContext, HostnameVerifier hostnameVerifier) {
+                clientRequest.setSSLContext(loadSSLContext());
+                clientRequest.replaceSSLHostnameVerifier(hostnameVerifier);
+            }
+        }, new TlsFileWatcher.FileChangeListener() {
+            @Override
+            public void onChanged(String filePath) {
+                clientRequest.setSSLContext(loadSSLContext());
+            }
+        });
+        
+        return new NacosRestTemplate(assignLogger(), clientRequest);
     }
     
     @Override
@@ -49,6 +77,43 @@ public abstract class AbstractHttpClientFactory implements HttpClientFactory {
         return RequestConfig.custom().setConnectTimeout(httpClientConfig.getConTimeOutMillis())
                 .setSocketTimeout(httpClientConfig.getReadTimeOutMillis())
                 .setMaxRedirects(httpClientConfig.getMaxRedirects()).build();
+    }
+    
+    protected void initTls(BiConsumer<SSLContext, HostnameVerifier> initTlsBiFunc,
+            TlsFileWatcher.FileChangeListener tlsChangeListener) {
+        if (!TlsSystemConfig.tlsEnable) {
+            return;
+        }
+        
+        final HostnameVerifier hv = HttpsURLConnection.getDefaultHostnameVerifier();
+        final SelfHostnameVerifier selfHostnameVerifier = new SelfHostnameVerifier(hv);
+        
+        initTlsBiFunc.accept(loadSSLContext(), selfHostnameVerifier);
+        
+        if (tlsChangeListener != null) {
+            try {
+                TlsFileWatcher.getInstance()
+                        .addFileChangeListener(tlsChangeListener, TlsSystemConfig.tlsClientTrustCertPath,
+                                TlsSystemConfig.tlsClientKeyPath);
+            } catch (IOException e) {
+                assignLogger().error("add tls file listener fail", e);
+            }
+        }
+    }
+    
+    @SuppressWarnings("checkstyle:abbreviationaswordinname")
+    protected synchronized SSLContext loadSSLContext() {
+        if (!TlsSystemConfig.tlsEnable) {
+            return null;
+        }
+        try {
+            return TlsHelper.buildSslContext(true);
+        } catch (NoSuchAlgorithmException e) {
+            assignLogger().error("Failed to create SSLContext", e);
+        } catch (KeyManagementException e) {
+            assignLogger().error("Failed to create SSLContext", e);
+        }
+        return null;
     }
     
     /**
