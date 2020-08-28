3306c18ab30a38e2f3a53e7540a058a8a6b9fbb9
KomachiSion <263976490@qq.com>
2020-08-28 17:59:59 +0800

    For pmd

--- naming/src/main/java/com/alibaba/nacos/naming/consistency/ephemeral/distro/newimpl/component/DistroTransportAgent.java
+++ naming/src/main/java/com/alibaba/nacos/naming/consistency/ephemeral/distro/newimpl/component/DistroTransportAgent.java
@@ -28,17 +28,65 @@ import java.util.List;
  */
 public interface DistroTransportAgent {
     
+    /**
+     * Sync data.
+     *
+     * @param data         data
+     * @param targetServer target server
+     * @return true is sync successfully, otherwise false
+     */
     boolean syncData(DistroData data, String targetServer);
     
+    /**
+     * Sync data with callback.
+     *
+     * @param data         data
+     * @param targetServer target server
+     * @param callback     callback
+     */
     void syncData(DistroData data, String targetServer, DistroCallback callback);
     
+    /**
+     * Sync verify data.
+     *
+     * @param verifyData   verify data
+     * @param targetServer target server
+     * @return true is verify successfully, otherwise false
+     */
     boolean syncVerifyData(DistroData verifyData, String targetServer);
     
+    /**
+     * Sync verify data.
+     *
+     * @param verifyData   verify data
+     * @param targetServer target server
+     * @param callback     callback
+     */
     void syncVerifyData(DistroData verifyData, String targetServer, DistroCallback callback);
     
+    /**
+     * get Data from target server.
+     *
+     * @param key          key of data
+     * @param targetServer target server
+     * @return distro data
+     */
     DistroData getData(DistroKey key, String targetServer);
     
+    /**
+     * Get datum.
+     *
+     * @param keys         keys of datum
+     * @param targetServer target server
+     * @return list of distro data
+     */
     List<DistroData> getDatum(List<DistroKey> keys, String targetServer);
     
+    /**
+     * Get all datum from target server.
+     *
+     * @param targetServer target server.
+     * @return list of distro data
+     */
     List<DistroData> getAllDatum(String targetServer);
 }
