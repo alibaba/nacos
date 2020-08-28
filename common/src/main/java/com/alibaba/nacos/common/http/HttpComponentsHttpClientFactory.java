16d1695f3ac4df7ec944743ac20e397ad6b0d931
mai.jh <maijh97@gmail.com>
2020-08-28 20:00:46 +0800

    refactor: Add apache http client Factory.

--- common/src/main/java/com/alibaba/nacos/common/http/HttpComponentsHttpClientFactory.java
+++ common/src/main/java/com/alibaba/nacos/common/http/HttpComponentsHttpClientFactory.java
@@ -4,39 +4,19 @@ import com.alibaba.nacos.common.http.client.NacosRestTemplate;
 import com.alibaba.nacos.common.http.client.request.DefaultHttpClientRequest;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.HttpClients;
-import org.slf4j.Logger;
 
 /**
- * apache http client implements.
+ * apache http client factory implements.
  *
  * @author mai.jh
  */
-public abstract class HttpComponentsHttpClientFactory implements HttpClientFactory {
+public abstract class HttpComponentsHttpClientFactory extends AbstractHttpClientFactory {
     
     @Override
-    public NacosRestTemplate createNacosRestTemplate() {
-        final HttpClientConfig httpClientConfig = buildHttpClientConfig();
-        
-        final RequestConfig requestConfig = RequestConfig.custom()
-                .setConnectTimeout(httpClientConfig.getConTimeOutMillis())
-                .setSocketTimeout(httpClientConfig.getReadTimeOutMillis())
-                .setMaxRedirects(httpClientConfig.getMaxRedirects()).build();
-        
+    public final NacosRestTemplate createNacosRestTemplate() {
+        final RequestConfig requestConfig = getRequestConfig();
         return new NacosRestTemplate(assignLogger(),
                 new DefaultHttpClientRequest(HttpClients.custom().setDefaultRequestConfig(requestConfig).build()));
     }
     
-    /**
-     * build http client config.
-     *
-     * @return HttpClientConfig
-     */
-    protected abstract HttpClientConfig buildHttpClientConfig();
-    
-    /**
-     * assign Logger.
-     *
-     * @return Logger
-     */
-    protected abstract Logger assignLogger();
 }
