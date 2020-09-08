cb20ad60cb8e0067ca957b0d70a7e9a9a7f203b1
KomachiSion <263976490@qq.com>
2020-09-08 10:31:49 +0800

    Add client manager

---
+++ naming/src/main/java/com/alibaba/nacos/naming/core/v2/client/AbstractClient.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.naming.core.v2.client;
+
+import com.alibaba.nacos.naming.core.v2.pojo.InstancePublishInfo;
+import com.alibaba.nacos.naming.core.v2.pojo.Service;
+import com.alibaba.nacos.naming.pojo.Subscriber;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Abstract implementation of {@code Client}.
+ *
+ * @author xiweng.yy
+ */
+public abstract class AbstractClient implements Client {
+    
+    private final ConcurrentHashMap<Service, InstancePublishInfo> publishers = new ConcurrentHashMap<>(16, 0.75f, 1);
+    
+    private final ConcurrentHashMap<Service, Subscriber> subscribers = new ConcurrentHashMap<>(16, 0.75f, 1);
+    
+    @Override
+    public boolean addServiceInstance(Service service, InstancePublishInfo instancePublishInfo) {
+        publishers.put(service, instancePublishInfo);
+        return true;
+    }
+    
+    @Override
+    public boolean removeServiceInstance(Service service) {
+        publishers.remove(service);
+        return true;
+    }
+    
+    @Override
+    public boolean addServiceSubscriber(Service service, Subscriber subscriber) {
+        subscribers.put(service, subscriber);
+        return true;
+    }
+    
+    @Override
+    public boolean removeServiceSubscriber(Service service) {
+        subscribers.remove(service);
+        return true;
+    }
+}
---
+++ naming/src/main/java/com/alibaba/nacos/naming/core/v2/client/manager/impl/ConnectionBasedClientManager.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.naming.core.v2.client.manager.impl;
+
+import com.alibaba.nacos.api.remote.RemoteConstants;
+import com.alibaba.nacos.core.remote.ClientConnectionEventListener;
+import com.alibaba.nacos.core.remote.Connection;
+import com.alibaba.nacos.naming.core.v2.client.Client;
+import com.alibaba.nacos.naming.core.v2.client.impl.ConnectionBasedClient;
+import com.alibaba.nacos.naming.core.v2.client.manager.ClientManager;
+import com.alibaba.nacos.naming.misc.Loggers;
+
+import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * The manager of {@code ConnectionBasedClient}.
+ *
+ * @author xiweng.yy
+ */
+public class ConnectionBasedClientManager extends ClientConnectionEventListener implements ClientManager {
+    
+    private final ConcurrentMap<String, ConnectionBasedClient> clients = new ConcurrentHashMap<>();
+    
+    @Override
+    public void clientConnected(Connection connect) {
+        if (!RemoteConstants.LABEL_MODULE_NAMING.equals(connect.getMetaInfo().getLabel(RemoteConstants.LABEL_MODULE))) {
+            return;
+        }
+        Loggers.SRV_LOG.info("Client connection {} connect", connect.getConnectionId());
+        clientConnected(new ConnectionBasedClient(connect.getConnectionId(), true));
+    }
+    
+    @Override
+    public boolean clientConnected(Client client) {
+        if (!clients.containsKey(client.getClientId())) {
+            clients.put(client.getClientId(), (ConnectionBasedClient) client);
+        }
+        return true;
+    }
+    
+    @Override
+    public void clientDisConnected(Connection connect) {
+        clientDisconnected(connect.getConnectionId());
+    }
+    
+    @Override
+    public boolean clientDisconnected(String clientId) {
+        Loggers.SRV_LOG.info("Client connection {} disconnect, remove instances and subscribers", clientId);
+        ConnectionBasedClient client = clients.remove(clientId);
+        if (null == client) {
+            return true;
+        }
+        // TODO remove all subscribers
+        // TODO remove all instances
+        return true;
+    }
+    
+    @Override
+    public Client getClient(String clientId) {
+        return clients.get(clientId);
+    }
+    
+    @Override
+    public Collection<String> allClientId() {
+        return clients.keySet();
+    }
+}
