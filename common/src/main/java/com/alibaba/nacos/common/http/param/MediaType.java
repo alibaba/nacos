473090ba13ee40c27924faaafaf1b1ec91b3481d
mai.jh <maijh97@gmail.com>
2020-09-17 17:49:11 +0800

    Fix http client contentType charset problem (#3848)

--- common/src/main/java/com/alibaba/nacos/common/http/param/MediaType.java
+++ common/src/main/java/com/alibaba/nacos/common/http/param/MediaType.java
@@ -16,15 +16,15 @@
 
 package com.alibaba.nacos.common.http.param;
 
+import com.alibaba.nacos.api.common.Constants;
+import com.alibaba.nacos.common.utils.StringUtils;
+
 /**
  * Http Media type.
  *
  * @author <a href="mailto:liaochuntao@live.com">liaochuntao</a>
  */
 public final class MediaType {
-    
-    private MediaType() {
-    }
     
     public static final String APPLICATION_ATOM_XML = "application/atom+xml";
     
@@ -46,4 +46,66 @@ public final class MediaType {
     
     public static final String TEXT_PLAIN = "text/plain;charset=UTF-8";
     
+    private MediaType(String type, String charset) {
+        this.type = type;
+        this.charset = charset;
+    }
+    
+    /**
+     * content type.
+     */
+    private final String type;
+    
+    /**
+     * content type charset.
+     */
+    private final String charset;
+    
+    /**
+     * Parse the given String contentType into a {@code MediaType} object.
+     *
+     * @param contentType mediaType
+     * @return MediaType
+     */
+    public static MediaType valueOf(String contentType) {
+        if (StringUtils.isEmpty(contentType)) {
+            throw new IllegalArgumentException("MediaType must not be empty");
+        }
+        String[] values = contentType.split(";");
+        String charset = Constants.ENCODE;
+        for (String value : values) {
+            if (value.startsWith("charset=")) {
+                charset = value.substring("charset=".length());
+            }
+        }
+        return new MediaType(values[0], charset);
+    }
+    
+    /**
+     * Use the given contentType and charset to assemble into a {@code MediaType} object.
+     *
+     * @param contentType contentType
+     * @param charset charset
+     * @return MediaType
+     */
+    public static MediaType valueOf(String contentType, String charset) {
+        if (StringUtils.isEmpty(contentType)) {
+            throw new IllegalArgumentException("MediaType must not be empty");
+        }
+        String[] values = contentType.split(";");
+        return new MediaType(values[0], StringUtils.isEmpty(charset) ? Constants.ENCODE : charset);
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public String getCharset() {
+        return charset;
+    }
+    
+    @Override
+    public String toString() {
+        return type + ";charset=" + charset;
+    }
 }
