ddf2dc02470ce661c99474d521176c145a0ad809
KomachiSion <263976490@qq.com>
2020-09-18 16:31:09 +0800

    Solve conflict

--- core/src/main/java/com/alibaba/nacos/core/remote/BaseRpcServer.java
+++ core/src/main/java/com/alibaba/nacos/core/remote/BaseRpcServer.java
@@ -18,8 +18,8 @@ package com.alibaba.nacos.core.remote;
 
 import com.alibaba.nacos.api.remote.PayloadRegistry;
 import com.alibaba.nacos.common.remote.ConnectionType;
-import com.alibaba.nacos.core.utils.ApplicationUtils;
 import com.alibaba.nacos.core.utils.Loggers;
+import com.alibaba.nacos.sys.utils.ApplicationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
