7ee053ee64047803894e8b29bca390890016af4a
qq213539 <213539@qq.com>
2020-10-12 18:09:02 +0800

    修改注释

--- config/src/main/java/com/alibaba/nacos/config/server/utils/TenantUtil.java
+++ config/src/main/java/com/alibaba/nacos/config/server/utils/TenantUtil.java
@@ -6,8 +6,6 @@ import org.apache.commons.lang3.StringUtils;
  * Tenant(namespace) Util.
  * Because config and naming treat tenant(namespace) differently,
  * this tool class can only be used by the config module.
- * @author klw(213539 @ qq.com)
- * @ClassName: TenantUtil
  * @date 2020/10/12 17:56
  */
 public class TenantUtil {
@@ -18,10 +16,8 @@ public class TenantUtil {
     
     /**
      * Treat the tenant parameters with values of "public" and "null" as an empty string.
-     * @author klw(213539@qq.com)
-     * 2020/10/12 17:59
      * @param tenant tenant(namespace) id
-     * @return java.lang.String
+     * @return java.lang.String A tenant(namespace) string processed
      */
     public static String processTenantParameter(String tenant) {
         if (StringUtils.isBlank(tenant) || NAMESPACE_PUBLIC_KEY.equalsIgnoreCase(tenant) || NAMESPACE_NULL_KEY
