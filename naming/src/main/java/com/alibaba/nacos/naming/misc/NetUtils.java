ce519390ecb26ea03cf0b383568aa72e2cca144f
qq213539 <213539@qq.com>
2020-10-19 12:07:21 +0800

    移除无用代码

--- naming/src/main/java/com/alibaba/nacos/naming/misc/NetUtils.java
+++ naming/src/main/java/com/alibaba/nacos/naming/misc/NetUtils.java
@@ -36,20 +36,4 @@ public class NetUtils {
         return InetUtils.getSelfIP() + IPUtil.IP_PORT_SPLITER + ApplicationUtils.getPort();
     }
     
-    /**
-     * Transfer int to IP.
-     *
-     * @param ip ip int
-     * @return IP string
-     */
-    public static String num2ip(int ip) {
-        int[] b = new int[4];
-        
-        b[0] = (ip >> 24) & 0xff;
-        b[1] = (ip >> 16) & 0xff;
-        b[2] = (ip >> 8) & 0xff;
-        b[3] = ip & 0xff;
-        return b[0] + "." + b[1] + "." + b[2] + "." + b[3];
-    }
-    
 }
