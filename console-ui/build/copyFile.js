a59cdabc39d3068b6828902258830855f77242e9
杨翊 SionYang <263976490@qq.com>
2020-11-11 13:59:50 +0800

    Revert "解决前端目录调整后前端访问不了问题 (#4195)" (#4196)
    This reverts commit a0bbd562b0cd71eb16b4a5e275ebb245f265e698.

--- console-ui/build/copyFile.js
+++ console-ui/build/copyFile.js
@@ -19,38 +19,27 @@ const path = require('path');
 // 默认打包存放地址
 const srcDir = path.join(__dirname, '../dist');
 // 打包后文件存放地址
-const destDir = path.join(__dirname,
-    '../../console/src/main/resources/static/');
+const destDir = path.join(__dirname, '../../console/src/main/resources/static/');
 
-const copy = function (src, dst) {
-  let paths = fs.readdirSync(src); //同步读取当前目录
-  paths.forEach(function (path) {
-    const _src = src + '/' + path;
-    const _dst = dst + '/' + path;
-    fs.stat(_src, function (err, stats) {  //stats  该对象 包含文件属性
-      if (err) {
-        throw err;
-      }
-      if (stats.isFile()) { //如果是个文件则拷贝
-        let readable = fs.createReadStream(_src);//创建读取流
-        let writable = fs.createWriteStream(_dst);//创建写入流
-        readable.pipe(writable);
-      } else if (stats.isDirectory()) { //是目录则 递归
-        checkDirectory(_src, _dst, copy);
-      }
-    });
-  });
-}
-const checkDirectory = function (src, dst, callback) {
-  fs.access(dst, fs.constants.F_OK, (err) => {
-    if (err) {
-      fs.mkdirSync(dst);
-      callback(src, dst);
-    } else {
-      callback(src, dst);
+const mkdir = dir => {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
     }
-  });
-};
+  };
 
-checkDirectory(srcDir,destDir,copy);
+const copyList = ['js/main.js', 'css/main.css'];
 
+copyList.forEach(_fileName => {
+    const srcFileName = path.join(srcDir, _fileName);
+    const destFileName = path.join(destDir, _fileName);
+
+    if (!fs.existsSync(srcFileName)) {
+        return;
+    }
+
+    mkdir(path.dirname(destFileName));
+
+    const readStream = fs.createReadStream(srcFileName);
+    const writeStream = fs.createWriteStream(destFileName);
+    readStream.pipe(writeStream);
+});
