622b63430a4461ed3fefc9dcadecde66dd37e0c3
mai.jh <maijh97@gmail.com>
2020-11-17 17:03:28 +0800

    fix Code style.

--- common/src/main/java/com/alibaba/nacos/common/http/AbstractApacheHttpClientFactory.java
+++ common/src/main/java/com/alibaba/nacos/common/http/AbstractApacheHttpClientFactory.java
@@ -18,7 +18,6 @@ package com.alibaba.nacos.common.http;
 
 import com.alibaba.nacos.common.http.client.NacosRestTemplate;
 import com.alibaba.nacos.common.http.client.request.DefaultHttpClientRequest;
-import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.protocol.RequestContent;
 
