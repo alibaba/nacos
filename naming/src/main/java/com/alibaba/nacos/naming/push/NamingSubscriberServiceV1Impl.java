10a5e450c682c4497f23e3e63f3fcf2ab8f4cc0c
KomachiSion <263976490@qq.com>
2020-12-10 20:02:24 +0800

    Add Subscriber Service

---
+++ naming/src/main/java/com/alibaba/nacos/naming/push/NamingSubscriberServiceV1Impl.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 1999-2020 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.naming.push;
+
+import com.alibaba.nacos.naming.core.v2.pojo.Service;
+import com.alibaba.nacos.naming.pojo.Subscriber;
+
+import java.util.Collection;
+
+/**
+ * Naming subscriber service for v1.x.
+ *
+ * @author xiweng.yy
+ */
+public class NamingSubscriberServiceV1Impl implements NamingSubscriberService {
+    
+    @Override
+    public Collection<Subscriber> getSubscribers(String namespaceId, String serviceName) {
+        return null;
+    }
+    
+    @Override
+    public Collection<Subscriber> getSubscribers(Service service) {
+        return null;
+    }
+}
