5b2d76fb345703c9783bacab22afc4feb5a2af70
liaochuntao <liaochuntao@live.com>
2020-12-16 18:30:41 +0800

    [ISSUE #4364] Fix Cluster member state isn't updated to 'DOWN' after the node becomes down (#4371)
    * fix: fix issue #4364
    
    * refactor: fixed node changes that could not trigger event publishing
    
    * fix: fixed a problem with frequent releases of events

--- core/src/main/java/com/alibaba/nacos/core/cluster/lookup/StandaloneMemberLookup.java
+++ core/src/main/java/com/alibaba/nacos/core/cluster/lookup/StandaloneMemberLookup.java
@@ -17,7 +17,7 @@
 package com.alibaba.nacos.core.cluster.lookup;
 
 import com.alibaba.nacos.core.cluster.AbstractMemberLookup;
-import com.alibaba.nacos.core.cluster.MemberUtils;
+import com.alibaba.nacos.core.cluster.MemberUtil;
 import com.alibaba.nacos.sys.env.EnvUtil;
 import com.alibaba.nacos.sys.utils.InetUtils;
 
@@ -34,7 +34,7 @@ public class StandaloneMemberLookup extends AbstractMemberLookup {
     public void start() {
         if (start.compareAndSet(false, true)) {
             String url = InetUtils.getSelfIP() + ":" + EnvUtil.getPort();
-            afterLookup(MemberUtils.readServerConf(Collections.singletonList(url)));
+            afterLookup(MemberUtil.readServerConf(Collections.singletonList(url)));
         }
     }
 }
