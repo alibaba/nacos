a0fc1f1236d0d88bb3bc6655fe690825e1246e62
mai.jh <maijh97@gmail.com>
2020-12-26 13:18:30 +0800

    [ISSUE-#4567] Refactor nacos-client Load Logging Configuration code. (#4572)
    * for #4567, refactor nacos-client logging load Configuration code.
    
    * Modify exception handling.
    
    * When customizing AbstractNacosLogging Load Configuration Fail, use the default Logging Load Configuration.
    
    * NacosLogging has been modified to a single-column mode.

--- client/src/main/java/com/alibaba/nacos/client/utils/LogUtils.java
+++ client/src/main/java/com/alibaba/nacos/client/utils/LogUtils.java
@@ -16,9 +16,7 @@
 
 package com.alibaba.nacos.client.utils;
 
-import com.alibaba.nacos.client.logging.AbstractNacosLogging;
-import com.alibaba.nacos.client.logging.log4j2.Log4J2NacosLogging;
-import com.alibaba.nacos.client.logging.logback.LogbackNacosLogging;
+import com.alibaba.nacos.client.logging.NacosLogging;
 import org.slf4j.Logger;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -34,33 +32,7 @@ public class LogUtils {
     public static final Logger NAMING_LOGGER;
     
     static {
-        try {
-            boolean isLogback = false;
-            AbstractNacosLogging nacosLogging;
-            
-            try {
-                Class.forName("ch.qos.logback.classic.Logger");
-                nacosLogging = new LogbackNacosLogging();
-                isLogback = true;
-            } catch (ClassNotFoundException e) {
-                nacosLogging = new Log4J2NacosLogging();
-            }
-            
-            try {
-                nacosLogging.loadConfiguration();
-            } catch (Throwable t) {
-                if (isLogback) {
-                    getLogger(LogUtils.class)
-                            .warn("Load Logback Configuration of Nacos fail, message: {}", t.getMessage());
-                } else {
-                    getLogger(LogUtils.class)
-                            .warn("Load Log4j Configuration of Nacos fail, message: {}", t.getMessage());
-                }
-            }
-        } catch (Throwable ex) {
-            getLogger(LogUtils.class).warn("Init Nacos Logging fail, message: {}", ex.getMessage());
-        }
-        
+        NacosLogging.getInstance().loadConfiguration();
         NAMING_LOGGER = getLogger("com.alibaba.nacos.client.naming");
     }
     
