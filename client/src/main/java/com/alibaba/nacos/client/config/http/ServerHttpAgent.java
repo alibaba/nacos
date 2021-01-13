ca5751052b4f270ac22740f35bc79734a8aaff2d
KomachiSion <263976490@qq.com>
2021-01-13 19:31:54 +0800

    Solve conflict from develop

--- client/src/main/java/com/alibaba/nacos/client/config/http/ServerHttpAgent.java
+++ client/src/main/java/com/alibaba/nacos/client/config/http/ServerHttpAgent.java
@@ -19,6 +19,7 @@ package com.alibaba.nacos.client.config.http;
 import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.client.config.impl.ConfigHttpClientManager;
 import com.alibaba.nacos.client.config.impl.ServerListManager;
+import com.alibaba.nacos.client.config.impl.SpasAdapter;
 import com.alibaba.nacos.client.utils.ContextPathUtil;
 import com.alibaba.nacos.client.utils.LogUtils;
 import com.alibaba.nacos.client.utils.ParamUtil;
