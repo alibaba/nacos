4236f78b42f4649321ddf5eb6b42313875c5778b
shalk <xshalk@163.com>
2021-04-01 00:12:15 +0800

    wip

---
+++ client/src/test/java/com/alibaba/nacos/client/naming/remote/http/NamingHttpClientProxyTest.java
@@ -0,0 +1,91 @@
+/*
+ *
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.alibaba.nacos.client.naming.remote.http;
+
+import org.junit.Test;
+
+public class NamingHttpClientProxyTest {
+    
+    @Test
+    public void testRegisterService() {
+    }
+    
+    public void testDeregisterService() {
+    }
+    
+    public void testUpdateInstance() {
+    }
+    
+    public void testQueryInstancesOfService() {
+    }
+    
+    public void testQueryService() {
+    }
+    
+    public void testCreateService() {
+    }
+    
+    public void testDeleteService() {
+    }
+    
+    public void testUpdateService() {
+    }
+    
+    public void testSendBeat() {
+    }
+    
+    public void testServerHealthy() {
+    }
+    
+    public void testGetServiceList() {
+    }
+    
+    public void testSubscribe() {
+    }
+    
+    public void testUnsubscribe() {
+    }
+    
+    public void testUpdateBeatInfo() {
+    }
+    
+    public void testReqApi() {
+    }
+    
+    public void testTestReqApi() {
+    }
+    
+    public void testTestReqApi1() {
+    }
+    
+    public void testCallServer() {
+    }
+    
+    public void testGetNamespaceId() {
+    }
+    
+    public void testSetServerPort() {
+    }
+    
+    public void testGetBeatReactor() {
+    }
+    
+    public void testShutdown() {
+    }
+}
