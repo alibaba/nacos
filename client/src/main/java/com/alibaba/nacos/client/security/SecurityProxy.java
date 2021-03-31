fe9e5f21ef9f7b4329fc2c2637d300ead3134a7d
ShawnSiao <xiaoalenxx@qq.com>
2021-03-31 23:32:13 +0800

    rename ignore to e.

--- client/src/main/java/com/alibaba/nacos/client/security/SecurityProxy.java
+++ client/src/main/java/com/alibaba/nacos/client/security/SecurityProxy.java
@@ -42,45 +42,45 @@ import java.util.concurrent.TimeUnit;
  * @since 1.2.0
  */
 public class SecurityProxy {
-    
+
     private static final Logger SECURITY_LOGGER = LoggerFactory.getLogger(SecurityProxy.class);
-    
+
     private static final String LOGIN_URL = "/v1/auth/users/login";
-    
+
     private final NacosRestTemplate nacosRestTemplate;
-    
+
     private final String contextPath;
-    
+
     /**
      * User's name.
      */
     private final String username;
-    
+
     /**
      * User's password.
      */
     private final String password;
-    
+
     /**
      * A token to take with when sending request to Nacos server.
      */
     private volatile String accessToken;
-    
+
     /**
      * TTL of token in seconds.
      */
     private long tokenTtl;
-    
+
     /**
      * Last timestamp refresh security info from server.
      */
     private long lastRefreshTime;
-    
+
     /**
      * time window to refresh security info in seconds.
      */
     private long tokenRefreshWindow;
-    
+
     /**
      * Construct from properties, keeping flexibility.
      *
@@ -93,7 +93,7 @@ public class SecurityProxy {
                 .normalizeContextPath(properties.getProperty(PropertyKeyConst.CONTEXT_PATH, "/nacos"));
         this.nacosRestTemplate = nacosRestTemplate;
     }
-    
+
     /**
      * Login to servers.
      *
@@ -101,26 +101,26 @@ public class SecurityProxy {
      * @return true if login successfully
      */
     public boolean login(List<String> servers) {
-        
+
         try {
             if ((System.currentTimeMillis() - lastRefreshTime) < TimeUnit.SECONDS
                     .toMillis(tokenTtl - tokenRefreshWindow)) {
                 return true;
             }
-            
+
             for (String server : servers) {
                 if (login(server)) {
                     lastRefreshTime = System.currentTimeMillis();
                     return true;
                 }
             }
-        } catch (Throwable ignore) {
-            SECURITY_LOGGER.error("[SecurityProxy] login  failed, servers: {}, errorMsg: {}", servers,  ignore.getMessage());
+        } catch (Throwable e) {
+            SECURITY_LOGGER.error("[SecurityProxy] login  failed, servers: {}, errorMsg: {}", servers,  e.getMessage());
         }
-        
+
         return false;
     }
-    
+
     /**
      * Login to server.
      *
@@ -128,14 +128,14 @@ public class SecurityProxy {
      * @return true if login successfully
      */
     public boolean login(String server) {
-        
+
         if (StringUtils.isNotBlank(username)) {
             Map<String, String> params = new HashMap<String, String>(2);
             Map<String, String> bodyMap = new HashMap<String, String>(2);
             params.put("username", username);
             bodyMap.put("password", password);
             String url = "http://" + server + contextPath + LOGIN_URL;
-            
+
             if (server.contains(Constants.HTTP_PREFIX)) {
                 url = server + contextPath + LOGIN_URL;
             }
@@ -160,7 +160,7 @@ public class SecurityProxy {
         }
         return true;
     }
-    
+
     public String getAccessToken() {
         return accessToken;
     }
