876791da1343f9a46c4d13bcf2bd3b20c1059439
Lisheng Zheng <zls1129@gmail.com>
2021-04-13 09:30:22 +0800

    fix(istio): should respond all kinds of collections for istio's request (#5312)

--- istio/src/main/java/com/alibaba/nacos/istio/mcp/NacosMcpService.java
+++ istio/src/main/java/com/alibaba/nacos/istio/mcp/NacosMcpService.java
@@ -223,14 +223,12 @@ public class NacosMcpService extends ResourceSourceGrpc.ResourceSourceImplBase {
                     Loggers.MAIN.info("ACK nonce: {}, type: {}", value.getResponseNonce(), value.getCollection());
                     return;
                 }
-                
-                if (!CollectionTypes.SERVICE_ENTRY.equals(value.getCollection())) {
-                    // Return empty resources for other types:
-                    Resources resources = Resources.newBuilder().setCollection(value.getCollection())
-                            .setNonce(String.valueOf(System.currentTimeMillis())).build();
-                    
-                    responseObserver.onNext(resources);
-                }
+
+                // Istio will send request to establish the stream, should respond all kinds of collections.
+                Resources resources = Resources.newBuilder().setCollection(value.getCollection())
+                        .setNonce(String.valueOf(System.currentTimeMillis())).build();
+
+                responseObserver.onNext(resources);
             }
             
             @Override
