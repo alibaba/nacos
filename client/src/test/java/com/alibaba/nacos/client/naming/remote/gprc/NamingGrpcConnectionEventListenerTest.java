4560c6763a806a239545a5db07b13a714a33f2ac
shalk <xshalk@163.com>
2021-04-14 22:52:56 +0800

    wip

--- client/src/test/java/com/alibaba/nacos/client/naming/remote/gprc/NamingGrpcConnectionEventListenerTest.java
+++ client/src/test/java/com/alibaba/nacos/client/naming/remote/gprc/NamingGrpcConnectionEventListenerTest.java
@@ -18,38 +18,76 @@
 
 package com.alibaba.nacos.client.naming.remote.gprc;
 
+import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.naming.pojo.Instance;
+import com.alibaba.nacos.api.naming.pojo.ServiceInfo;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class NamingGrpcConnectionEventListenerTest {
     
     @Test
-    public void testOnConnected() {
-        NamingGrpcClientProxy proxy = mock(NamingGrpcClientProxy.class);
-        NamingGrpcConnectionEventListener listener = new NamingGrpcConnectionEventListener(proxy);
-        listener.onConnected();
-        //TODO
-    }
-    
-    @Test
-    public void testCacheInstanceForRedo() {
+    public void testCacheInstanceForRedo() throws NacosException {
+        //given
         NamingGrpcClientProxy proxy = mock(NamingGrpcClientProxy.class);
         NamingGrpcConnectionEventListener listener = new NamingGrpcConnectionEventListener(proxy);
         String serviceName = "service1";
         String groupName = "group1";
         Instance instance = new Instance();
-        
         listener.cacheInstanceForRedo(serviceName, groupName, instance);
+        //when
+        listener.onConnected();
+        //then
+        verify(proxy, times(1)).registerService(serviceName, groupName, instance);
     }
     
-    public void testRemoveInstanceForRedo() {
+    @Test
+    public void testRemoveInstanceForRedo() throws NacosException {
+        //given
+        NamingGrpcClientProxy proxy = mock(NamingGrpcClientProxy.class);
+        NamingGrpcConnectionEventListener listener = new NamingGrpcConnectionEventListener(proxy);
+        String serviceName = "service1";
+        String groupName = "group1";
+        Instance instance = new Instance();
+        listener.cacheInstanceForRedo(serviceName, groupName, instance);
+        listener.removeInstanceForRedo(serviceName, groupName, instance);
+        //when
+        listener.onConnected();
+        //then
+        verify(proxy, times(0)).registerService(serviceName, groupName, instance);
     }
     
-    public void testCacheSubscriberForRedo() {
+    @Test
+    public void testCacheSubscriberForRedo() throws NacosException {
+        //given
+        NamingGrpcClientProxy proxy = mock(NamingGrpcClientProxy.class);
+        NamingGrpcConnectionEventListener listener = new NamingGrpcConnectionEventListener(proxy);
+        String fullServiceName = "group1@@service1";
+        String cluster = "cluster1";
+        listener.cacheSubscriberForRedo(fullServiceName, cluster);
+        //when
+        listener.onConnected();
+        //then
+        ServiceInfo info = ServiceInfo.fromKey(fullServiceName);
+        verify(proxy, times(1)).subscribe(info.getName(), info.getGroupName(), cluster);
     }
     
-    public void testRemoveSubscriberForRedo() {
+    @Test
+    public void testRemoveSubscriberForRedo() throws NacosException {
+        //given
+        NamingGrpcClientProxy proxy = mock(NamingGrpcClientProxy.class);
+        NamingGrpcConnectionEventListener listener = new NamingGrpcConnectionEventListener(proxy);
+        String fullServiceName = "group1@@service1";
+        String cluster = "cluster1";
+        listener.cacheSubscriberForRedo(fullServiceName, cluster);
+        listener.removeSubscriberForRedo(fullServiceName, cluster);
+        //when
+        listener.onConnected();
+        //then
+        ServiceInfo info = ServiceInfo.fromKey(fullServiceName);
+        verify(proxy, times(0)).subscribe(info.getName(), info.getGroupName(), cluster);
     }
 }
