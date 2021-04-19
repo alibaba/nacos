9b956fab8419f5d9b6d99e746615b236223aba08
Varian <fengmingyang04@aliyun.com>
2021-04-19 14:05:38 +0800

    One aspect of code quality is to have better expressions.

--- naming/src/main/java/com/alibaba/nacos/naming/healthcheck/ClientBeatProcessor.java
+++ naming/src/main/java/com/alibaba/nacos/naming/healthcheck/ClientBeatProcessor.java
@@ -34,62 +34,60 @@ import java.util.concurrent.TimeUnit;
  * @author nkorange
  */
 public class ClientBeatProcessor implements Runnable {
-    
+
     public static final long CLIENT_BEAT_TIMEOUT = TimeUnit.SECONDS.toMillis(15);
-    
+
     private RsInfo rsInfo;
-    
+
     private Service service;
-    
+
     @JsonIgnore
     public PushService getPushService() {
         return ApplicationUtils.getBean(PushService.class);
     }
-    
+
     public RsInfo getRsInfo() {
         return rsInfo;
     }
-    
+
     public void setRsInfo(RsInfo rsInfo) {
         this.rsInfo = rsInfo;
     }
-    
+
     public Service getService() {
         return service;
     }
-    
+
     public void setService(Service service) {
         this.service = service;
     }
-    
+
     @Override
     public void run() {
         Service service = this.service;
         if (Loggers.EVT_LOG.isDebugEnabled()) {
             Loggers.EVT_LOG.debug("[CLIENT-BEAT] processing beat: {}", rsInfo.toString());
         }
-        
+
         String ip = rsInfo.getIp();
         String clusterName = rsInfo.getCluster();
         int port = rsInfo.getPort();
         Cluster cluster = service.getClusterMap().get(clusterName);
         List<Instance> instances = cluster.allIPs(true);
-        
+
         for (Instance instance : instances) {
             if (instance.getIp().equals(ip) && instance.getPort() == port) {
                 if (Loggers.EVT_LOG.isDebugEnabled()) {
                     Loggers.EVT_LOG.debug("[CLIENT-BEAT] refresh beat: {}", rsInfo.toString());
                 }
                 instance.setLastBeat(System.currentTimeMillis());
-                if (!instance.isMarked()) {
-                    if (!instance.isHealthy()) {
-                        instance.setHealthy(true);
-                        Loggers.EVT_LOG
-                                .info("service: {} {POS} {IP-ENABLED} valid: {}:{}@{}, region: {}, msg: client beat ok",
-                                        cluster.getService().getName(), ip, port, cluster.getName(),
-                                        UtilsAndCommons.LOCALHOST_SITE);
-                        getPushService().serviceChanged(service);
-                    }
+                if (!instance.isMarked() && !instance.isHealthy()) {
+                    instance.setHealthy(true);
+                    Loggers.EVT_LOG
+                        .info("service: {} {POS} {IP-ENABLED} valid: {}:{}@{}, region: {}, msg: client beat ok",
+                            cluster.getService().getName(), ip, port, cluster.getName(),
+                            UtilsAndCommons.LOCALHOST_SITE);
+                    getPushService().serviceChanged(service);
                 }
             }
         }
