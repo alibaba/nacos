8483ae97635e171a3c1a9ed7c4a0894c06b3c999
MajorHe1 <hefan1007@gmail.com>
2021-04-21 15:48:53 +0800

    [ISSUE #5361] add a judgement whether request instance of AbstractNamingRequest

--- naming/src/main/java/com/alibaba/nacos/naming/remote/rpc/filter/GrpcRequestFilter.java
+++ naming/src/main/java/com/alibaba/nacos/naming/remote/rpc/filter/GrpcRequestFilter.java
@@ -1,6 +1,7 @@
 package com.alibaba.nacos.naming.remote.rpc.filter;
 
 import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.remote.request.AbstractNamingRequest;
 import com.alibaba.nacos.api.remote.request.Request;
 import com.alibaba.nacos.api.remote.request.RequestMeta;
 import com.alibaba.nacos.api.remote.response.Response;
@@ -17,7 +18,7 @@ public class GrpcRequestFilter extends AbstractRequestFilter {
     
     @Override
     protected Response filter(Request request, RequestMeta meta, Class handlerClazz) throws NacosException {
-        if (!upgradeJudgement.isUseGrpcFeatures()) {
+        if (request instanceof AbstractNamingRequest && !upgradeJudgement.isUseGrpcFeatures()) {
             Response response = getDefaultResponseInstance(handlerClazz);
             response.setErrorInfo(NacosException.SERVER_ERROR, "Nacos cluster is running with 1.X mode, can't accept gRPC request temporarily. Please check the server status or close Double write to force open 2.0 mode. Detail https://nacos.io/en-us/docs/2.0.0-upgrading.html.");
             return response;
