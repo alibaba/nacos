5b401c746eeffc4ba10156b0c8c86849847dd7cd
goodjava <goodjava@163.com>
2021-04-22 10:44:12 +0800

    change code style

--- client/src/main/java/com/alibaba/nacos/client/naming/core/PushReceiver.java
+++ client/src/main/java/com/alibaba/nacos/client/naming/core/PushReceiver.java
@@ -40,23 +40,23 @@ import static com.alibaba.nacos.client.utils.LogUtils.NAMING_LOGGER;
  * @author xuanyin
  */
 public class PushReceiver implements Runnable, Closeable {
-
+    
     private static final Charset UTF_8 = Charset.forName("UTF-8");
-
+    
     private static final int UDP_MSS = 64 * 1024;
-
+    
     private ScheduledExecutorService executorService;
-
+    
     private DatagramSocket udpSocket;
-
+    
     private HostReactor hostReactor;
-
+    
     private volatile boolean closed = false;
-
+    
     public static String getPushReceiverUdpPort() {
         return System.getenv(PropertyKeyConst.PUSH_RECEIVER_UDP_PORT);
     }
-
+    
     public PushReceiver(HostReactor hostReactor) {
         try {
             this.hostReactor = hostReactor;
@@ -75,32 +75,32 @@ public class PushReceiver implements Runnable, Closeable {
                     return thread;
                 }
             });
-
+            
             this.executorService.execute(this);
         } catch (Exception e) {
             NAMING_LOGGER.error("[NA] init udp socket failed", e);
         }
     }
-
+    
     @Override
     public void run() {
         while (!closed) {
             try {
-
+                
                 // byte[] is initialized with 0 full filled by default
                 byte[] buffer = new byte[UDP_MSS];
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-
+                
                 udpSocket.receive(packet);
-
+                
                 String json = new String(IoUtils.tryDecompress(packet.getData()), UTF_8).trim();
                 NAMING_LOGGER.info("received push data: " + json + " from " + packet.getAddress().toString());
-
+                
                 PushPacket pushPacket = JacksonUtils.toObj(json, PushPacket.class);
                 String ack;
                 if ("dom".equals(pushPacket.type) || "service".equals(pushPacket.type)) {
                     hostReactor.processServiceJson(pushPacket.data);
-
+                    
                     // send ack to server
                     ack = "{\"type\": \"push-ack\"" + ", \"lastRefTime\":\"" + pushPacket.lastRefTime + "\", \"data\":"
                             + "\"\"}";
@@ -114,7 +114,7 @@ public class PushReceiver implements Runnable, Closeable {
                     ack = "{\"type\": \"unknown-ack\"" + ", \"lastRefTime\":\"" + pushPacket.lastRefTime
                             + "\", \"data\":" + "\"\"}";
                 }
-
+                
                 udpSocket.send(new DatagramPacket(ack.getBytes(UTF_8), ack.getBytes(UTF_8).length,
                         packet.getSocketAddress()));
             } catch (Exception e) {
@@ -125,7 +125,7 @@ public class PushReceiver implements Runnable, Closeable {
             }
         }
     }
-
+    
     @Override
     public void shutdown() throws NacosException {
         String className = this.getClass().getName();
@@ -135,16 +135,16 @@ public class PushReceiver implements Runnable, Closeable {
         udpSocket.close();
         NAMING_LOGGER.info("{} do shutdown stop", className);
     }
-
+    
     public static class PushPacket {
-
+        
         public String type;
-
+        
         public long lastRefTime;
-
+        
         public String data;
     }
-
+    
     public int getUdpPort() {
         return this.udpSocket.getLocalPort();
     }
