38bfe9597dc119598b7600a9f299475f8fcf953f
shalk <xshalk@163.com>
2021-04-28 00:16:20 +0800

    wip

--- client/src/test/java/com/alibaba/nacos/client/naming/remote/http/NamingHttpClientManagerTest.java
+++ client/src/test/java/com/alibaba/nacos/client/naming/remote/http/NamingHttpClientManagerTest.java
@@ -18,24 +18,50 @@
 
 package com.alibaba.nacos.client.naming.remote.http;
 
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.common.http.client.NacosRestTemplate;
+import com.alibaba.nacos.common.http.client.request.HttpClientRequest;
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class NamingHttpClientManagerTest {
     
     @Test
     public void testGetInstance() {
+        Assert.assertNotNull(NamingHttpClientManager.getInstance());
     }
     
     @Test
     public void testGetPrefix() {
+        Assert.assertEquals("http://", NamingHttpClientManager.getInstance().getPrefix());
     }
     
     @Test
     public void testGetNacosRestTemplate() {
+        Assert.assertNotNull(NamingHttpClientManager.getInstance().getNacosRestTemplate());
     }
     
     @Test
-    public void testShutdown() {
+    public void testShutdown() throws NoSuchFieldException, IllegalAccessException, NacosException, IOException {
+        //given
+        NamingHttpClientManager instance = NamingHttpClientManager.getInstance();
+        
+        HttpClientRequest mockHttpClientRequest = Mockito.mock(HttpClientRequest.class);
+        Field requestClient = NacosRestTemplate.class.getDeclaredField("requestClient");
+        requestClient.setAccessible(true);
+        requestClient.set(instance.getNacosRestTemplate(), mockHttpClientRequest);
+        // when
+        NamingHttpClientManager.getInstance().shutdown();
+        // then
+        verify(mockHttpClientRequest, times(1)).close();
+        
     }
     
 }
