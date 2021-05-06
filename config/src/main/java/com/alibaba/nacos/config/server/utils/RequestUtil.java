4e80ee6d0c91decc2f28cc7f7202226b40c99924
plusmancn <plusmancn@gmail.com>
2021-05-06 16:50:37 +0800

    fix: code style of the remaining files

--- config/src/main/java/com/alibaba/nacos/config/server/utils/RequestUtil.java
+++ config/src/main/java/com/alibaba/nacos/config/server/utils/RequestUtil.java
@@ -28,17 +28,17 @@ import javax.servlet.http.HttpServletRequest;
  * @author Nacos
  */
 public class RequestUtil {
-
+    
     private static final String X_REAL_IP = "X-Real-IP";
-
+    
     private static final String X_FORWARDED_FOR = "X-Forwarded-For";
-
+    
     private static final String X_FORWARDED_FOR_SPLIT_SYMBOL = ",";
-
+    
     public static final String CLIENT_APPNAME_HEADER = "Client-AppName";
-
+    
     public static final String NACOS_USER_KEY = "nacosuser";
-
+    
     /**
      * get real client ip
      *
@@ -56,7 +56,7 @@ public class RequestUtil {
         String nginxHeader = request.getHeader(X_REAL_IP);
         return StringUtils.isBlank(nginxHeader) ? request.getRemoteAddr() : nginxHeader;
     }
-
+    
     /**
      * Gets the name of the client application in the header.
      *
@@ -66,7 +66,7 @@ public class RequestUtil {
     public static String getAppName(HttpServletRequest request) {
         return request.getHeader(CLIENT_APPNAME_HEADER);
     }
-
+    
     /**
      * Gets the user of the client application in the Attribute.
      *
@@ -78,11 +78,11 @@ public class RequestUtil {
         if (userObj == null) {
             return null;
         }
-
+        
         User user = (User) userObj;
         return user;
     }
-
+    
     /**
      * Gets the username of the client application in the Attribute.
      *
@@ -94,4 +94,5 @@ public class RequestUtil {
         // If auth is disabled, get username from parameters by agreed key
         return user == null ? request.getParameter(Constants.USERNAME) : user.getUserName();
     }
+    
 }
