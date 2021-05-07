413685e075637753b7a0e725f559b0f4ac7c62c3
xiaoheng1 <2018154970@qq.com>
2021-05-07 17:23:51 +0800

    fix #5592 Optimize the AbstractMemberLookup class, abstract out the template method

--- core/src/main/java/com/alibaba/nacos/core/cluster/AbstractMemberLookup.java
+++ core/src/main/java/com/alibaba/nacos/core/cluster/AbstractMemberLookup.java
@@ -46,4 +46,16 @@ public abstract class AbstractMemberLookup implements MemberLookup {
     public void destroy() throws NacosException {
     
     }
+    
+    @Override
+    public void start() throws NacosException {
+        if (start.compareAndSet(false, true)) {
+            doStart();
+        }
+    }
+    
+    /**
+     * subclass can override this method if need.
+     */
+    protected abstract void doStart() throws NacosException;
 }
