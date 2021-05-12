38556c24f775eca12758218a2c33417c7e9008f2
mingyang feng <39239059+bird-man04@users.noreply.github.com>
2021-05-12 15:04:05 +0800

    Update ClientBeatProcessor.java

--- naming/src/main/java/com/alibaba/nacos/naming/healthcheck/ClientBeatProcessor.java
+++ naming/src/main/java/com/alibaba/nacos/naming/healthcheck/ClientBeatProcessor.java
@@ -34,47 +34,47 @@ import java.util.concurrent.TimeUnit;
  * @author nkorange
  */
 public class ClientBeatProcessor implements Runnable {
-
+    
     public static final long CLIENT_BEAT_TIMEOUT = TimeUnit.SECONDS.toMillis(15);
-
+    
     private RsInfo rsInfo;
-
+    
     private Service service;
-
+    
     @JsonIgnore
     public PushService getPushService() {
         return ApplicationUtils.getBean(PushService.class);
     }
-
+    
     public RsInfo getRsInfo() {
         return rsInfo;
     }
-
+    
     public void setRsInfo(RsInfo rsInfo) {
         this.rsInfo = rsInfo;
     }
-
+    
     public Service getService() {
         return service;
     }
-
+    
     public void setService(Service service) {
         this.service = service;
     }
-
+    
     @Override
     public void run() {
         Service service = this.service;
         if (Loggers.EVT_LOG.isDebugEnabled()) {
             Loggers.EVT_LOG.debug("[CLIENT-BEAT] processing beat: {}", rsInfo.toString());
         }
-
+        
         String ip = rsInfo.getIp();
         String clusterName = rsInfo.getCluster();
         int port = rsInfo.getPort();
         Cluster cluster = service.getClusterMap().get(clusterName);
         List<Instance> instances = cluster.allIPs(true);
-
+        
         for (Instance instance : instances) {
             if (instance.getIp().equals(ip) && instance.getPort() == port) {
                 if (Loggers.EVT_LOG.isDebugEnabled()) {
