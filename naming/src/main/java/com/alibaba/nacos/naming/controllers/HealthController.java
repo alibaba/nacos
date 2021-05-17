b33134cf50054b4cac71e66cee73c825217e6d7c
xiaoheng1 <2018154970@qq.com>
2021-05-17 09:44:23 +0800

    Extract constants in HealthController (#5717)
    * fix #5715 Extract constants in HealthController
    
    * fix #5715 Extract constants in HealthController
    
    * add license

--- naming/src/main/java/com/alibaba/nacos/naming/controllers/HealthController.java
+++ naming/src/main/java/com/alibaba/nacos/naming/controllers/HealthController.java
@@ -50,6 +50,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.alibaba.nacos.naming.constants.RequestConstant.HEALTHY_KEY;
+import static com.alibaba.nacos.naming.constants.RequestConstant.IP_KEY;
+import static com.alibaba.nacos.naming.constants.RequestConstant.PORT_KEY;
+import static com.alibaba.nacos.naming.constants.RequestConstant.VALID_KEY;
+
 /**
  * Health status related operation controller.
  *
@@ -93,9 +98,9 @@ public class HealthController {
     @PutMapping(value = {"", "/instance"})
     @Secured(action = ActionTypes.WRITE, parser = NamingResourceParser.class)
     public ResponseEntity update(HttpServletRequest request) throws NacosException {
-        String healthyString = WebUtils.optional(request, "healthy", StringUtils.EMPTY);
+        String healthyString = WebUtils.optional(request, HEALTHY_KEY, StringUtils.EMPTY);
         if (StringUtils.isBlank(healthyString)) {
-            healthyString = WebUtils.optional(request, "valid", StringUtils.EMPTY);
+            healthyString = WebUtils.optional(request, VALID_KEY, StringUtils.EMPTY);
         }
         if (StringUtils.isBlank(healthyString)) {
             throw new IllegalArgumentException("Param 'healthy' is required.");
@@ -105,8 +110,8 @@ public class HealthController {
         String namespaceId = WebUtils.optional(request, CommonParams.NAMESPACE_ID, Constants.DEFAULT_NAMESPACE_ID);
         String clusterName = WebUtils
                 .optional(request, CommonParams.CLUSTER_NAME, UtilsAndCommons.DEFAULT_CLUSTER_NAME);
-        String ip = WebUtils.required(request, "ip");
-        int port = Integer.parseInt(WebUtils.required(request, "port"));
+        String ip = WebUtils.required(request, IP_KEY);
+        int port = Integer.parseInt(WebUtils.required(request, PORT_KEY));
         getHealthOperator()
                 .updateHealthStatusForPersistentInstance(namespaceId, serviceName, clusterName, ip, port, health);
         return ResponseEntity.ok("ok");
