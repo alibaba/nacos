dc289fe294d8ab7d972a963eda13e4f795a07372
KomachiSion <263976490@qq.com>
2021-06-17 20:21:31 +0800

    Fix #5985, Fix unit test problem for nacos-core.

--- core/src/test/java/com/alibaba/nacos/core/remote/TpsMonitorManagerTest.java
+++ core/src/test/java/com/alibaba/nacos/core/remote/TpsMonitorManagerTest.java
@@ -21,43 +21,75 @@ import com.alibaba.nacos.core.remote.control.TpsControlRule;
 import com.alibaba.nacos.core.remote.control.TpsMonitorManager;
 import com.alibaba.nacos.core.remote.control.TpsMonitorPoint;
 import org.assertj.core.util.Lists;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-public class TpsMonitorManagerTester {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TpsMonitorManagerTest {
     
     static TpsMonitorManager tpsMonitorManager = new TpsMonitorManager();
     
-    static {
+    @BeforeClass
+    public static void setUpBeforeClass() {
         TpsMonitorPoint publish = new TpsMonitorPoint("configPublish");
         tpsMonitorManager.registerTpsControlPoint(publish);
         TpsControlRule rule = new TpsControlRule();
         rule.setPointRule(new TpsControlRule.Rule(5000, TimeUnit.SECONDS, "SUM", "intercept"));
         rule.getMonitorKeyRule()
-                .putIfAbsent("testKey:a*b", new TpsControlRule.Rule(5000, TimeUnit.MINUTES, "EACH", "intercept"));
+                .putIfAbsent("testKey:a*b", new TpsControlRule.Rule(500, TimeUnit.SECONDS, "EACH", "intercept"));
         rule.getMonitorKeyRule()
-                .putIfAbsent("testKey:*", new TpsControlRule.Rule(2000000, TimeUnit.MINUTES, "SUM", "intercept"));
+                .putIfAbsent("testKey:*", new TpsControlRule.Rule(2000000, TimeUnit.SECONDS, "SUM", "intercept"));
         
         publish.applyRule(rule);
     }
     
+    @Before
+    public void setUp() {
+        // make sure different case will not effect each other.
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException ignored) {
+        }
+    }
+    
     @Test
-    public void test() {
-        for (int i = 0; i < 10000000; i++) {
+    public void testApplyTps() {
+        for (int i = 0; i < 100; i++) {
             String value = "atg" + (new Random().nextInt(100) + 2) + "efb";
             boolean pass = tpsMonitorManager
                     .applyTps("configPublish", "testconnectionId", Lists.list(new TestKey(value)));
+            assertTrue(pass);
             try {
                 Thread.sleep(1L);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+    }
+    
+    @Test
+    public void testApplyTpsWithOverFlow() {
+        for (int i = 0; i < 1000; i++) {
+            String value = "atg" + (new Random().nextInt(100) + 2) + "efb";
+            boolean pass = tpsMonitorManager
+                    .applyTps("configPublish", "testconnectionId", Lists.list(new TestKey(value)));
             if (!pass) {
-                System.out.println("Apply tps fail.");
+                return;
+            }
+            try {
+                Thread.sleep(1L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
+        Assert.fail("fail to limit.");
     }
     
     class TestKey extends MonitorKey {
