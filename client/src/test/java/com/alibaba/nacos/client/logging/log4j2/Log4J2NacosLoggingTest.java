eabf755e03d8a5ed24d9095618539529519348ac
shalk <xshalk@163.com>
2021-06-23 23:22:08 +0800

    fix Log4J2NacosLoggingTest

--- client/src/test/java/com/alibaba/nacos/client/logging/log4j2/Log4J2NacosLoggingTest.java
+++ client/src/test/java/com/alibaba/nacos/client/logging/log4j2/Log4J2NacosLoggingTest.java
@@ -18,13 +18,36 @@
 
 package com.alibaba.nacos.client.logging.log4j2;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Map;
 
 public class Log4J2NacosLoggingTest {
     
+    private static final String NACOS_LOGGER_PREFIX = "com.alibaba.nacos";
+    
     @Test
     public void testLoadConfiguration() {
+        LoggerContext loggerContext = (LoggerContext) LogManager.getContext(false);
+        Configuration contextConfiguration = loggerContext.getConfiguration();
+        Assert.assertEquals(0, contextConfiguration.getLoggers().size());
         Log4J2NacosLogging log4J2NacosLogging = new Log4J2NacosLogging();
+        //when
         log4J2NacosLogging.loadConfiguration();
+        //then
+        loggerContext = (LoggerContext) LogManager.getContext(false);
+        contextConfiguration = loggerContext.getConfiguration();
+        Map<String, LoggerConfig> nacosClientLoggers = contextConfiguration.getLoggers();
+        Assert.assertEquals(4, nacosClientLoggers.size());
+        for (Map.Entry<String, LoggerConfig> loggerEntry : nacosClientLoggers.entrySet()) {
+            String loggerName = loggerEntry.getKey();
+            Assert.assertTrue(loggerName.startsWith(NACOS_LOGGER_PREFIX));
+        }
+        
     }
 }
