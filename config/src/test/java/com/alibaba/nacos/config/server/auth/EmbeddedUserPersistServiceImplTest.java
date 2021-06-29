73ff7cd56210eef317a7a07c33dac6721a85b5c3
lixiaoshuang <644968328@qq.com>
2021-06-29 12:57:30 +0800

    add unit test for EmbeddedUserPersistServiceImpl

--- config/src/test/java/com/alibaba/nacos/config/server/auth/EmbeddedUserPersistServiceImplTest.java
+++ config/src/test/java/com/alibaba/nacos/config/server/auth/EmbeddedUserPersistServiceImplTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.alibaba.nacos.config.server.auth;
 
 import com.alibaba.nacos.config.server.model.Page;
