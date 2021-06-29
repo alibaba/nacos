b8bc6bfa16b4ecf0b31e387d9709c54612be50ea
MajorHe1 <hefan1007@gmail.com>
2021-06-29 22:59:57 +0800

    [ISSUE #6142] add unit test for NamingGrpcConnectionEventListener. remove cache missed judgement of user and role.

--- console/src/main/java/com/alibaba/nacos/console/security/nacos/users/NacosUserDetailsServiceImpl.java
+++ console/src/main/java/com/alibaba/nacos/console/security/nacos/users/NacosUserDetailsServiceImpl.java
@@ -91,7 +91,7 @@ public class NacosUserDetailsServiceImpl implements UserDetailsService {
     
     public User getUser(String username) {
         User user = userMap.get(username);
-        if (!authConfigs.isCachingEnabled() || user == null) {
+        if (!authConfigs.isCachingEnabled()) {
             user = getUserFromDatabase(username);
         }
         return user;
