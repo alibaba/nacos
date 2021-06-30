dea2f54369771fe73c932fae04e8e3d5b658695f
zzq1314zll <zzqlovezll@gmail.com>
2021-06-30 12:43:12 +0800

    delete Useless method, add contains method and test method

--- common/src/test/java/com/alibaba/nacos/common/utils/ArrayUtilsTest.java
+++ common/src/test/java/com/alibaba/nacos/common/utils/ArrayUtilsTest.java
@@ -25,10 +25,12 @@ import org.junit.Test;
  */
 public class ArrayUtilsTest {
     
+    Integer[] nullArr = null;
+    
+    Integer[] nothingArr = new Integer[]{};
+    
     @Test
     public void testisEmpty() {
-        Integer[] nullArr = null;
-        Integer[] nothingArr = new Integer[]{};
         Integer[] arr = new Integer[]{1, 2};
         Assert.assertTrue(ArrayUtils.isEmpty(nullArr));
         Assert.assertTrue(ArrayUtils.isEmpty(nothingArr));
@@ -36,60 +38,15 @@ public class ArrayUtilsTest {
     }
     
     @Test
-    public void testtoPrimitive() {
-        Boolean[] nullArr = null;
-        Boolean[] nothingArr = new Boolean[]{};
-        Boolean[] arr = new Boolean[]{Boolean.TRUE, Boolean.FALSE, Boolean.TRUE};
-        Assert.assertNull(ArrayUtils.toPrimitive(nullArr));
-        Assert.assertArrayEquals(new boolean[0], ArrayUtils.toPrimitive(nothingArr));
-        Assert.assertArrayEquals(new boolean[]{true, false, true}, ArrayUtils.toPrimitive(arr));
+    public void contains() {
+        Integer[] arr = new Integer[]{1, 2, 3};
+        Integer[] arr1 = new Integer[]{1, 2, 3, null};
+        Assert.assertFalse(ArrayUtils.contains(nullArr, "a"));
+        Assert.assertFalse(ArrayUtils.contains(nullArr, null));
+        Assert.assertFalse(ArrayUtils.contains(nothingArr, "b"));
+        Assert.assertFalse(ArrayUtils.contains(arr, null));
+        Assert.assertTrue(ArrayUtils.contains(arr1, null));
+        Assert.assertTrue(ArrayUtils.contains(arr, 1));
+        Assert.assertFalse(ArrayUtils.contains(arr, "1"));
     }
-    
-    @Test
-    public void testadd() {
-        testadd1();
-        Assert.assertArrayEquals(new String[]{"a"}, ArrayUtils.add(null, "a"));;
-        Assert.assertArrayEquals(new String[]{"a", null}, ArrayUtils.add(new String[]{"a"}, null));
-        Assert.assertArrayEquals(new String[]{"a", "b"}, ArrayUtils.add(new String[]{"a"}, "b"));
-        Assert.assertArrayEquals(new String[]{"a", "b", "c"}, ArrayUtils.add(new String[]{"a", "b"}, "c"));
-    }
-    
-    private void testadd1() {
-        try {
-            ArrayUtils.add(null, null);
-        } catch (Exception ex) {
-            if (!(ex instanceof  IllegalArgumentException)) {
-                Assert.fail("unknown mistake");
-            }
-        }
-    }
-    
-    @Test
-    public void testaddAll() {
-        testaddAll1();
-        Assert.assertArrayEquals(new String[] {"a", "b"}, ArrayUtils.addAll(new String[] {"a", "b"}, null));
-        Assert.assertArrayEquals(new String[] {"a", "b"}, ArrayUtils.addAll(null, new String[] {"a", "b"}));
-        Assert.assertArrayEquals(new String[0], ArrayUtils.addAll(new String[0], new String[0]));
-        Assert.assertArrayEquals(new String[]{null, null}, ArrayUtils.addAll(new String[]{null}, new String[]{null}));
-        Assert.assertArrayEquals(new String[] {"a", "b", "c", "1", "2", "3"},
-                ArrayUtils.addAll(new String[]{"a", "b", "c"}, new String[]{"1", "2", "3"}));
-    }
-    
-    private void testaddAll1() {
-        try {
-            ArrayUtils.addAll(null, null);
-        } catch (Exception ex) {
-            if (!(ex instanceof  IllegalArgumentException)) {
-                Assert.fail("unknown mistake");
-            }
-        }
-    }
-    
-    @Test
-    public void testclone() {
-        Assert.assertArrayEquals(null, ArrayUtils.clone(null));
-        Assert.assertArrayEquals(new String[] {"a", "b"}, ArrayUtils.clone(new String[] {"a", "b"}));
-        Assert.assertArrayEquals(new String[0], ArrayUtils.clone(new String[0]));
-    }
-    
 }
