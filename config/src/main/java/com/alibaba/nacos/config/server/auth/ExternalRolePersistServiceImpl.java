7c96c055c15b6063277241ce73c4852bf293ff4a
mask616 <mask616@163.com>
2021-07-01 22:19:48 +0800

    [ISSUE #6216] Unify SQL naming style.

--- config/src/main/java/com/alibaba/nacos/config/server/auth/ExternalRolePersistServiceImpl.java
+++ config/src/main/java/com/alibaba/nacos/config/server/auth/ExternalRolePersistServiceImpl.java
@@ -62,7 +62,7 @@ public class ExternalRolePersistServiceImpl implements RolePersistService {
         
         PaginationHelper<RoleInfo> helper = persistService.createPaginationHelper();
         
-        String sqlCountRows = "SELECT count(*) FROM (SELECT distinct role from roles) roles WHERE ";
+        String sqlCountRows = "SELECT count(*) FROM (SELECT DISTINCT role from roles) roles WHERE ";
         String sqlFetchRows = "SELECT role,username FROM roles WHERE ";
         
         String where = " 1=1 ";
