393fb4f613d77a3b109f3fa37b3162773b98ba91
zzq1314zll <zzqlovezll@gmail.com>
2021-07-01 12:23:17 +0800

    Optimize the code.

--- common/src/main/java/com/alibaba/nacos/common/utils/StringUtils.java
+++ common/src/main/java/com/alibaba/nacos/common/utils/StringUtils.java
@@ -64,7 +64,7 @@ public class StringUtils {
             return true;
         }
         for (int i = 0; i < strLen; i++) {
-            if (Character.isWhitespace(cs.charAt(i)) == false) {
+            if (!Character.isWhitespace(cs.charAt(i))) {
                 return false;
             }
         }
