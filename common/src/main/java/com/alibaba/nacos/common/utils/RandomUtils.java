5374c7262fa55547fc0ae2ff465ea2f146575e91
zzq1314zll <zzqlovezll@gmail.com>
2021-07-02 10:35:37 +0800

    [ISSUE #6228] Canonical code

--- common/src/main/java/com/alibaba/nacos/common/utils/RandomUtils.java
+++ common/src/main/java/com/alibaba/nacos/common/utils/RandomUtils.java
@@ -26,49 +26,50 @@ import java.util.Random;
 public class RandomUtils {
     
     /**
-     * Random object used by random method. This has to be not local to the random method so as to not return the same
-     * value in the same millisecond.
+     * Random Object for random method.
      */
     private static final Random RANDOM = new Random();
     
     /**
-     * <p>
      * Returns a random long within the specified range.
-     * </p>
      *
      * @param startInclusive the smallest value that can be returned, must be non-negative
      * @param endExclusive   the upper bound (not included)
      * @return the random long
-     * @throws IllegalArgumentException if {@code startInclusive > endExclusive} or if {@code startInclusive} is
-     *                                  negative
+     * @throws IllegalArgumentException if startInclusive or endExclusive illegal
      */
     public static long nextLong(final long startInclusive, final long endExclusive) {
         checkParameters(startInclusive, endExclusive);
-        if (startInclusive == endExclusive) {
+        long diff = endExclusive - startInclusive;
+        if (diff == 0) {
             return startInclusive;
         }
-        return (long) (startInclusive + ((endExclusive - startInclusive) * RANDOM.nextDouble()));
+        return (long) (startInclusive + (diff * RANDOM.nextDouble()));
     }
     
     /**
-     * <p>
      * Returns a random integer within the specified range.
-     * </p>
      *
-     * @param startInclusive the smallest value that can be returned, must be non-negative
+     * @param startInclusive lower limit, must be non-negative
      * @param endExclusive   the upper bound (not included)
      * @return the random integer
-     * @throws IllegalArgumentException if {@code startInclusive > endExclusive} or if {@code startInclusive} is
-     *                                  negative
+     * @throws IllegalArgumentException if startInclusive or endExclusive illegal
      */
     public static int nextInt(final int startInclusive, final int endExclusive) {
         checkParameters(startInclusive, endExclusive);
-        if (startInclusive == endExclusive) {
+        int diff = endExclusive - startInclusive;
+        if (diff == 0) {
             return startInclusive;
         }
-        return startInclusive + RANDOM.nextInt(endExclusive - startInclusive);
+        return startInclusive + RANDOM.nextInt(diff);
     }
     
+    /**
+     * Check input parameters.
+     *
+     * @param startInclusive lower limit, must be non-negative
+     * @param endExclusive   the upper bound (not included)
+     */
     private static void checkParameters(final long startInclusive, final long endExclusive) {
         if (endExclusive < startInclusive) {
             throw new IllegalArgumentException("startInclusive must be less than or equal to the endExclusive.");
