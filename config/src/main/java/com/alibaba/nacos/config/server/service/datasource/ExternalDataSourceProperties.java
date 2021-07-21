1495d79ed0419c65166ba0a34f03d064a43d95c4
jian huang <jian.huang@cmexpro.com>
2021-07-21 21:47:33 +0800

    支持通过配置文件指定数据库驱动类

--- config/src/main/java/com/alibaba/nacos/config/server/service/datasource/ExternalDataSourceProperties.java
+++ config/src/main/java/com/alibaba/nacos/config/server/service/datasource/ExternalDataSourceProperties.java
@@ -13,6 +13,7 @@
 
 package com.alibaba.nacos.config.server.service.datasource;
 
+import com.alibaba.nacos.common.utils.StringUtils;
 import com.google.common.base.Preconditions;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.commons.collections.CollectionUtils;
@@ -34,12 +35,14 @@ import static com.alibaba.nacos.common.utils.CollectionUtils.getOrDefault;
  */
 public class ExternalDataSourceProperties {
     
-    private static final String JDBC_DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
-    
+    private static final String MYSQL_JDBC_DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+
     private static final String TEST_QUERY = "SELECT 1";
     
     private Integer num;
-    
+
+    private String driverClassName;
+
     private List<String> url = new ArrayList<>();
     
     private List<String> user = new ArrayList<>();
@@ -49,7 +52,11 @@ public class ExternalDataSourceProperties {
     public void setNum(Integer num) {
         this.num = num;
     }
-    
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
     public void setUrl(List<String> url) {
         this.url = url;
     }
@@ -79,7 +86,7 @@ public class ExternalDataSourceProperties {
             int currentSize = index + 1;
             Preconditions.checkArgument(url.size() >= currentSize, "db.url.%s is null", index);
             DataSourcePoolProperties poolProperties = DataSourcePoolProperties.build(environment);
-            poolProperties.setDriverClassName(JDBC_DRIVER_NAME);
+            poolProperties.setDriverClassName(StringUtils.isEmpty(driverClassName) ? MYSQL_JDBC_DRIVER_NAME : driverClassName);
             poolProperties.setJdbcUrl(url.get(index).trim());
             poolProperties.setUsername(getOrDefault(user, index, user.get(0)).trim());
             poolProperties.setPassword(getOrDefault(password, index, password.get(0)).trim());
