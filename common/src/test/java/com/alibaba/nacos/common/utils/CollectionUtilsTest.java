b5f9dd55d8e8dba72a92aeae98c9dc0ea2106629
zzq1314zll <zzqlovezll@gmail.com>
2021-07-24 12:07:52 +0800

    update unit test method name

--- common/src/test/java/com/alibaba/nacos/common/utils/CollectionUtilsTest.java
+++ common/src/test/java/com/alibaba/nacos/common/utils/CollectionUtilsTest.java
@@ -282,7 +282,7 @@ public class CollectionUtilsTest {
     }
     
     @Test(expected = IllegalArgumentException.class)
-    public void testNullPointerException() {
+    public void testListNullPointerException() {
         CollectionUtils.list(null);
     }
     
