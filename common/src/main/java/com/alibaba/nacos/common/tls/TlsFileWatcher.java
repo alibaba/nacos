9609d429075f7be98972f5434069ff27b5c4451c
SunJiFeng <sunjifeng@sfmail.sf-express.com>
2021-07-25 12:35:13 +0800

    replace anonymous Runnable with lambda

--- common/src/main/java/com/alibaba/nacos/common/tls/TlsFileWatcher.java
+++ common/src/main/java/com/alibaba/nacos/common/tls/TlsFileWatcher.java
@@ -97,28 +97,25 @@ public final class TlsFileWatcher {
      */
     public void start() {
         if (started.compareAndSet(false, true)) {
-            service.scheduleAtFixedRate(new Runnable() {
-                @Override
-                public void run() {
-                    for (Map.Entry<String, FileChangeListener> item : watchFilesMap.entrySet()) {
-                        String filePath = item.getKey();
-                        String newHash;
-                        InputStream in = null;
-                        try {
-                            in = new FileInputStream(filePath);
-                            newHash = MD5Utils.md5Hex(IoUtils.toString(in, Constants.ENCODE), Constants.ENCODE);
-                        } catch (Exception ignored) {
-                            LOGGER.warn(" service has exception when calculate the file MD5. " + ignored);
-                            continue;
-                        } finally {
-                            IoUtils.closeQuietly(in);
-                        }
-                        if (!newHash.equals(fileMd5Map.get(filePath))) {
-                            LOGGER.info(filePath + " file hash changed,need reload sslcontext");
-                            fileMd5Map.put(filePath, newHash);
-                            item.getValue().onChanged(filePath);
-                            LOGGER.info(filePath + " onChanged success!");
-                        }
+            service.scheduleAtFixedRate(() -> {
+                for (Map.Entry<String, FileChangeListener> item : watchFilesMap.entrySet()) {
+                    String filePath = item.getKey();
+                    String newHash;
+                    InputStream in = null;
+                    try {
+                        in = new FileInputStream(filePath);
+                        newHash = MD5Utils.md5Hex(IoUtils.toString(in, Constants.ENCODE), Constants.ENCODE);
+                    } catch (Exception ignored) {
+                        LOGGER.warn(" service has exception when calculate the file MD5. " + ignored);
+                        continue;
+                    } finally {
+                        IoUtils.closeQuietly(in);
+                    }
+                    if (!newHash.equals(fileMd5Map.get(filePath))) {
+                        LOGGER.info(filePath + " file hash changed,need reload sslcontext");
+                        fileMd5Map.put(filePath, newHash);
+                        item.getValue().onChanged(filePath);
+                        LOGGER.info(filePath + " onChanged success!");
                     }
                 }
             }, 1, checkInterval, TimeUnit.MINUTES);
