a729001ec57ca861079a8a6a8f017c53ec953d86
ZZQ的 <33930711+zzq1314zll@users.noreply.github.com>
2021-07-30 16:32:38 +0800

    [Reduce dependence]>[Sets] Replace "google.common.collect.*" API with… (#6445)
    * [Reduce dependence]>[Sets] Replace "google.common.collect.*" API with jdk API
    
    * delete empty line separator

--- common/src/main/java/com/alibaba/nacos/common/utils/CollectionUtils.java
+++ common/src/main/java/com/alibaba/nacos/common/utils/CollectionUtils.java
@@ -17,13 +17,16 @@
 package com.alibaba.nacos.common.utils;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Copy from {@link org.apache.commons.collections}.
@@ -263,4 +266,16 @@ public final class CollectionUtils {
         return list;
     }
     
+    /**
+     * Return an set containing all input parameters.
+     * @param elements elements element array
+     * @return set containing all input parameters
+     */
+    public static <T> Set<T> set(T... elements) {
+        if (elements == null) {
+            throw new IllegalArgumentException("Expected an array of elements (or empty array) but received a null.");
+        } else {
+            return new LinkedHashSet(Arrays.asList(elements));
+        }
+    }
 }
