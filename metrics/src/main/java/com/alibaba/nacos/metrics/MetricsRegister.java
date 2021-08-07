a8c13880dfc260cb2fe94a3d8fae8b3168342618
Holdonbei <283255249@qq.com>
2021-08-07 20:10:38 +0800

    add an interface named MetricsRegister and a class named PrometheusMetricsRegister with unit-test.

---
+++ metrics/src/main/java/com/alibaba/nacos/metrics/MetricsRegister.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.metrics;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Timer;
+
+import java.util.function.Supplier;
+
+/**
+ * Metrics com.alibaba.nacos.test.register.
+ *
+ * @author holdonbei
+ */
+public interface MetricsRegister {
+    
+    /**
+     * Register Gauge.
+     *
+     * @param name name
+     * @param tags Key/value pairs
+     * @param description description for gauge
+     * @param number number for gauge
+     */
+    void registerGauge(String name, Iterable<Tag> tags, String description, Supplier<Number> number);
+    
+    /**
+     * Register Gauge.
+     *
+     * @param name name
+     * @param tags Key/value pairs
+     * @param description description for gauge
+     * @return Gauge
+     */
+    Gauge registerGauge(String name, Iterable<Tag> tags, String description);
+    
+    /**
+     * Register Counter.
+     *
+     * @param name name
+     * @param tags Key/value pairs
+     * @param description description for gauge
+     * @return Counter
+     */
+    Counter registerCounter(String name, Iterable<Tag> tags, String description);
+    
+    /**
+     * RegisterTimer.
+     *
+     * @param name name
+     * @param tags Key/value pairs
+     * @param description description for gauge
+     * @return Timer
+     */
+    Timer registerTimer(String name, Iterable<Tag> tags, String description);
+    
+    /**
+     * Summary.
+     *
+     * @param name name
+     * @param tags Key/value pairs
+     * @param description description for gauge
+     * @return DistributionSummary
+     */
+    DistributionSummary summary(String name, Iterable<Tag> tags, String description);
+    
+    /**
+     * Counter Increment.
+     *
+     * @param name name
+     * @param tags Key/value pairs
+     */
+    void counterIncrement(String name, Iterable<Tag> tags);
+    
+    /**
+     * Counter increment by count.
+     *
+     * @param name name
+     * @param tags Key/value pairs
+     * @param count step
+     */
+    void counterIncrement(String name, Iterable<Tag> tags, long count);
+}
---
+++ metrics/src/main/java/com/alibaba/nacos/metrics/register/PrometheusMetricsRegister.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.metrics.register;
+
+import com.alibaba.nacos.metrics.MetricsRegister;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Timer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Supplier;
+
+/**
+ * PrometheusMetricsRegister.
+ * @author holdonbei
+ */
+@Component
+public class PrometheusMetricsRegister implements MetricsRegister {
+    
+    /**
+     * registry that contains all metrics from nacos.
+     */
+    private final MeterRegistry registry;
+    
+    /**
+     * registry is injected by spring.
+     * @param registry registry provided by spring-boot-actuator
+     */
+    @Autowired
+    public PrometheusMetricsRegister(MeterRegistry registry) {
+        this.registry = registry;
+    }
+    
+    @Override
+    public void registerGauge(String name, Iterable<Tag> tags, String description, Supplier<Number> number) {
+        Gauge.builder(name, number)
+                .description(description)
+                .tags(tags)
+                .register(registry);
+    }
+    
+    @Override
+    public Gauge registerGauge(String name, Iterable<Tag> tags, String description) {
+        return Gauge.builder(name, () -> 0)
+                .description(description)
+                .tags(tags)
+                .register(registry);
+    }
+    
+    @Override
+    public Counter registerCounter(String name, Iterable<Tag> tags, String description) {
+        return Counter.builder(name)
+                .description(description)
+                .tags(tags)
+                .register(registry);
+    }
+    
+    @Override
+    public Timer registerTimer(String name, Iterable<Tag> tags, String description) {
+        return Timer.builder(name)
+                .tags(tags)
+                .description(description)
+                .register(registry);
+    }
+    
+    @Override
+    public DistributionSummary summary(String name, Iterable<Tag> tags, String description) {
+        return DistributionSummary.builder(name)
+                .description(description)
+                .tags(tags)
+                .register(registry);
+    }
+    
+    @Override
+    public void counterIncrement(String name, Iterable<Tag> tags) {
+        registry.get(name)
+                .tags(tags)
+                .counter()
+                .increment();
+    }
+    
+    @Override
+    public void counterIncrement(String name, Iterable<Tag> tags, long count) {
+        registry.get(name)
+                .tags(tags)
+                .counter()
+                .increment(count);
+    }
+}
