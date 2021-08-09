0af7c0f2c8023260a9f30fa46b647b4a59e09baf
brotherlu-xcq <1285823170@qq.com>
2021-08-09 09:48:45 +0800

    [ISSUE #6551] fix the console log and the dismiss word. (#6590)

--- console-ui/src/pages/ServiceManagement/ServiceDetail/ServiceDetail.js
+++ console-ui/src/pages/ServiceManagement/ServiceDetail/ServiceDetail.js
@@ -187,7 +187,7 @@ class ServiceDetail extends React.Component {
                 </Button>
               }
             >
-              <InstanceFilter setFilters={this.setFilters(cluster.name)} />
+              <InstanceFilter setFilters={this.setFilters(cluster.name)} locale={locale.InstanceFilter} />
               <InstanceTable
                 clusterName={cluster.name}
                 serviceName={serviceName}
