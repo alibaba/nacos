ac878582e43a5efc99e68ac33a531d31c9f52005
Wuyunfan-BUPT <30968107+Wuyunfan-BUPT@users.noreply.github.com>
2021-08-13 00:43:58 -0500

    [ISSUE#5696] Add auth plugin interface. (#6593)
    * define plugin
    
    * add User model
    
    * add authContext
    
    * add SPI
    
    * add resource, modify request, reformat code
    
    * copyright, Serverloader, IdentityContext
    
    * add unit test
    
    * modify ClassLoader, and delete GrantType
    
    * delete redundancy code
    
    * remove authservice default name
    
    * Change to StringUtils

---
+++ auth/src/main/java/com/alibaba/nacos/auth/AuthService.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 1999-2021 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.auth;
+
+import com.alibaba.nacos.auth.context.IdentityContext;
+import com.alibaba.nacos.auth.exception.AccessException;
+import com.alibaba.nacos.auth.model.Permission;
+
+/**
+ * Auth service.
+ *
+ * @author Wuyfee
+ */
+public interface AuthService {
+    
+    /**
+     * Authentication of request, identify the user who request the resource.
+     *
+     * @param identityContext where we can find the user information
+     * @return boolean if the user identify success
+     * @throws AccessException if authentication is failed
+     */
+    Boolean login(IdentityContext identityContext) throws AccessException;
+    
+    
+    /**
+     * identity whether the user has the resource authority.
+     * @param identityContext where we can find the user information.
+     * @param permission permission to auth.
+     * @return Boolean if the user has the resource authority.
+     */
+    Boolean authorityAccess(IdentityContext identityContext, Permission permission);
+    
+    /**
+     * AuthService Name which for conveniently find AuthService instance.
+     * @return AuthServiceName mark a AuthService instance.
+     */
+    String getAuthServiceName();
+    
+}
---
+++ auth/src/main/java/com/alibaba/nacos/auth/context/GrpcIdentityContextBuilder.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 1999-2021 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.auth.context;
+
+import com.alibaba.nacos.api.remote.request.Request;
+import com.alibaba.nacos.auth.common.AuthConfigs;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class GrpcIdentityContextBuilder implements IdentityContextBuilder<Request> {
+    AuthConfigs authConfigs = new AuthConfigs();
+    /**
+     * get identity context from grpc.
+     * @param request grpc request
+     * @return IdentityContext request context
+     */
+    
+    @Override
+    public IdentityContext build(Request request) {
+        Set<String> keySet = new HashSet<String>(Arrays.asList(authConfigs.getAuthorityKey()));
+        IdentityContext identityContext = new IdentityContext();
+        Map<String, String> map = request.getHeaders();
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (keySet.contains(entry.getKey())) {
+                identityContext.setParameter(entry.getKey(), entry.getValue());
+            }
+        }
+        return identityContext;
+    }
+}
