01b21fd73d3c78b46be4c536941790c92b17439a
pirateskipper <blindfoldskipper@gmail.com>
2021-08-16 11:25:12 +0800

    start error with jdk8 & jdk11

--- distribution/bin/startup.sh
+++ distribution/bin/startup.sh
@@ -138,5 +138,5 @@ if [ ! -f "${BASE_DIR}/logs/start.out" ]; then
 fi
 # start
 echo "$JAVA $JAVA_OPT_EXT_FIX ${JAVA_OPT}" > ${BASE_DIR}/logs/start.out 2>&1 &
-nohup "$JAVA" "$JAVA_OPT_EXT_FIX" ${JAVA_OPT} nacos.nacos >> ${BASE_DIR}/logs/start.out 2>&1 &
+nohup ${JAVA} ${JAVA_OPT_EXT_FIX} ${JAVA_OPT} nacos.nacos >> ${BASE_DIR}/logs/start.out 2>&1 &
 echo "nacos is starting，you can check the ${BASE_DIR}/logs/start.out"
