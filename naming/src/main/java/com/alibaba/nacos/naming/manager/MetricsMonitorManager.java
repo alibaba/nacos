fa1c23718071148c0ccf7c020ef52477aa686db6
Holdonbei <283255249@qq.com>
2021-08-22 21:10:43 +0800

    add MetricsMonitorManager.

--- naming/src/main/java/com/alibaba/nacos/naming/manager/MetricsMonitorManager.java
+++ naming/src/main/java/com/alibaba/nacos/naming/manager/MetricsMonitorManager.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.nacos.naming.manager;
 
 import io.micrometer.core.instrument.Counter;
@@ -7,27 +23,30 @@ import io.micrometer.core.instrument.Tag;
 import io.micrometer.core.instrument.Timer;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 
+/**
+ * manager.
+ *
+ * @author holdonbei
+ */
 public class MetricsMonitorManager {
     
     /**
-     * 管理 Gauge
+     * 管理 Gauge.
      */
     private static final Map<String, AtomicLong> GAUGES_MAP = new ConcurrentHashMap<>();
     
     /**
-     * 管理 Counter
+     * 管理 Counter.
      */
     private static final Map<String, Counter> COUNTERS_MAP = new ConcurrentHashMap<>();
     
     /**
-     * 管理 Timer
+     * 管理 Timer.
      */
     private static final Map<String, Timer> TIMERS_MAP = new ConcurrentHashMap<>();
     
@@ -38,7 +57,10 @@ public class MetricsMonitorManager {
     public static Timer timer(String name, String... tags) {
         return TIMERS_MAP.computeIfAbsent(getKey(name, tags), s -> Metrics.timer(name, tags));
     }
-
+    
+    /**
+     * gauge.
+     */
     public static AtomicLong gauge(String name, String... tags) {
         if ((tags.length & 1) == 1) {
             // 报异常
@@ -54,7 +76,7 @@ public class MetricsMonitorManager {
     }
     
     /**
-     * 暴露 Manager 管理的 Metrics 信息
+     * 暴露 Manager 管理的 Metrics 信息.
      */
     public static List<Metrics> exporter() {
         return null;
