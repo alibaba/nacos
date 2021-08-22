38273a3b667be78ef5fc88528e46f982a1286ddb
Holdonbei <283255249@qq.com>
2021-08-22 21:10:43 +0800

    add MetricsMonitorManager.

--- naming/src/main/java/com/alibaba/nacos/naming/manager/test/MetricsManagerTest.java
+++ naming/src/main/java/com/alibaba/nacos/naming/manager/test/MetricsManagerTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.nacos.naming.manager.test;
 
 import com.alibaba.nacos.naming.manager.MetricsMonitorManager;
@@ -12,8 +28,18 @@ import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * manager test.
+ *
+ * @author holdonbei
+ */
 @Component
 public class MetricsManagerTest {
+    
+    /**
+     * Gauge.
+     */
+    private AtomicLong testGauge = new AtomicLong();
     
     // counter 测试
     @Scheduled(cron = "0/5 * * * * ?")
@@ -35,7 +61,6 @@ public class MetricsManagerTest {
         timer.record(10, TimeUnit.SECONDS);
     }
     
-    
     @Scheduled(cron = "0/5 * * * * ?")
     public void testTimerTwo() {
         Metrics.timer("nacos_monitor", "module", "naming", "name", "timerTwo").record(10, TimeUnit.SECONDS);
@@ -48,11 +73,9 @@ public class MetricsManagerTest {
         gauge.incrementAndGet();
     }
     
-    private AtomicLong testGauge = new AtomicLong();
     @Scheduled(cron = "0/5 * * * * ?")
     public void testGaugeTwo() {
         Metrics.gauge("nacos_monitor", Arrays.asList(new ImmutableTag("module", "naming"), new ImmutableTag("name", "gaugeTwo")), testGauge);
         testGauge.incrementAndGet();
-        
     }
 }
