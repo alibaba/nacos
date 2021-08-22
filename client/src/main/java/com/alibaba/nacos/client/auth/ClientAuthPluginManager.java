ec5a58f58f7dea5f9dbee695364b68ca3b12df57
Wuyunfan <1564396093@qq.com>
2021-08-22 11:56:08 +0800

    client plugin

---
+++ client/src/main/java/com/alibaba/nacos/client/auth/ClientAuthPluginManager.java
@@ -0,0 +1,57 @@
+package com.alibaba.nacos.client.auth;
+
+import com.alibaba.nacos.common.spi.NacosServiceLoader;
+import com.alibaba.nacos.common.utils.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class ClientAuthPluginManager {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientAuthPluginManager.class);
+    
+    private static final ClientAuthPluginManager INSTANCE = new ClientAuthPluginManager();
+    
+    /**
+     * The relationship of context type and {@link ClientAuthService}.
+     */
+    private Map<String, ClientAuthService> clientAuthServiceHashMap = new HashMap<>();
+    
+    public ClientAuthPluginManager() {
+        initClientAuthServices();
+    }
+    
+    public static ClientAuthPluginManager getInstance() {
+        return INSTANCE;
+    }
+    
+    /**
+     * init ClientAuthService.
+     */
+    private void initClientAuthServices() {
+        Collection<ClientAuthService> clientAuthServices = NacosServiceLoader.load(ClientAuthService.class);
+        for (ClientAuthService clientAuthService : clientAuthServices) {
+            if (StringUtils.isEmpty(clientAuthService.getClientAuthServiceName())) {
+                LOGGER.warn(
+                        "[ClientAuthPluginManager] Load ClientAuthService({}) ClientAuthServiceName(null/empty) fail. "
+                                + "Please Add ClientAuthServiceName to resolve.", clientAuthService.getClass());
+                continue;
+            }
+            clientAuthServiceHashMap.put(clientAuthService.getClientAuthServiceName(), clientAuthService);
+            LOGGER.info("[ClientAuthPluginManager] Load ClientAuthService({}) ClientAuthServiceName({}) successfully.",
+                    clientAuthService.getClass(), clientAuthService.getClientAuthServiceName());
+        }
+    }
+    
+    /**
+     * get ClientAuthService instance which ClientAuthService.getType() is type.
+     * @param clientAuthServiceName AuthServiceName, mark a AuthService instance.
+     * @return AuthService instance.
+     */
+    public Optional<ClientAuthService> findAuthServiceSpiImpl(String clientAuthServiceName) {
+        return Optional.ofNullable(clientAuthServiceHashMap.get(clientAuthServiceName));
+    }
+    
+}
---
+++ client/src/main/java/com/alibaba/nacos/client/auth/ClientAuthService.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 1999-2021 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.client.auth;
+import java.util.Properties;
+
+/**
+ * Client AuthService.
+ *
+ * @author wuyfee
+ */
+public interface ClientAuthService {
+    
+    /**
+     * login(request) to service and get response.
+     * @param properties login information.
+     * @return boolean whether login success.
+     */
+    boolean login(Properties properties);
+    
+    /**
+     * ClientAuthService Name which for conveniently find ClientAuthService instance.
+     * @return ClientAuthServiceName mark clientAuthService.
+     */
+    String getClientAuthServiceName();
+    
+    /**
+     * package request and execute request.
+     * @param properties login information.
+     * @param requestType HTTP or GRPC
+     * @return Object request result.
+     */
+    Object restRequest(Properties properties, String requestType);
+    
+}
