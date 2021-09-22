e24417a3dbef6cd4bae5834f3f084259ca6d5276
holdonbei <283255249@qq.com>
2021-09-22 11:45:34 +0800

    remove some unnecessary code in MetricsMonitor.java from naming module.

--- naming/src/main/java/com/alibaba/nacos/naming/monitor/MetricsMonitor.java
+++ naming/src/main/java/com/alibaba/nacos/naming/monitor/MetricsMonitor.java
@@ -18,18 +18,10 @@ package com.alibaba.nacos.naming.monitor;
 
 import com.alibaba.nacos.metrics.manager.MetricsManager;
 import com.alibaba.nacos.metrics.manager.NamingMetricsConstant;
-import com.alibaba.nacos.naming.misc.Loggers;
 import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.ImmutableTag;
 import io.micrometer.core.instrument.Metrics;
-import io.micrometer.core.instrument.Tag;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Metrics Monitor.
@@ -37,106 +29,6 @@ import java.util.concurrent.atomic.AtomicLong;
  * @author Nacos
  */
 public class MetricsMonitor {
-    
-    private static final MetricsMonitor INSTANCE = new MetricsMonitor();
-    
-    private final AtomicInteger mysqlHealthCheck = new AtomicInteger();
-    
-    private final AtomicInteger httpHealthCheck = new AtomicInteger();
-    
-    private final AtomicInteger tcpHealthCheck = new AtomicInteger();
-    
-    private final AtomicInteger serviceCount = new AtomicInteger();
-    
-    private final AtomicInteger ipCount = new AtomicInteger();
-    
-    private final AtomicInteger subscriberCount = new AtomicInteger();
-    
-    private final AtomicLong maxPushCost = new AtomicLong(-1);
-    
-    private final AtomicLong avgPushCost = new AtomicLong(-1);
-    
-    private final AtomicLong leaderStatus = new AtomicLong();
-    
-    private final AtomicInteger totalPush = new AtomicInteger();
-    
-    private final AtomicInteger totalPushCountForAvg = new AtomicInteger();
-    
-    private final AtomicLong totalPushCostForAvg = new AtomicLong();
-    
-    private final AtomicInteger failedPush = new AtomicInteger();
-    
-    private MetricsMonitor() {
-        for (Field each : MetricsMonitor.class.getDeclaredFields()) {
-            if (Number.class.isAssignableFrom(each.getType())) {
-                each.setAccessible(true);
-                try {
-                    registerToMetrics(each.getName(), (Number) each.get(this));
-                } catch (IllegalAccessException e) {
-                    Loggers.PERFORMANCE_LOG.error("Init metrics for {} failed", each.getName(), e);
-                }
-            }
-        }
-    }
-    
-    private <T extends Number> void registerToMetrics(String name, T number) {
-        List<Tag> tags = new ArrayList<>();
-        tags.add(new ImmutableTag("module", "naming"));
-        tags.add(new ImmutableTag("name", name));
-        Metrics.gauge("nacos_monitor", tags, number);
-    }
-    
-    public static AtomicInteger getMysqlHealthCheckMonitor() {
-        return INSTANCE.mysqlHealthCheck;
-    }
-    
-    public static AtomicInteger getHttpHealthCheckMonitor() {
-        return INSTANCE.httpHealthCheck;
-    }
-    
-    public static AtomicInteger getTcpHealthCheckMonitor() {
-        return INSTANCE.tcpHealthCheck;
-    }
-    
-    public static AtomicInteger getDomCountMonitor() {
-        return INSTANCE.serviceCount;
-    }
-    
-    public static AtomicInteger getIpCountMonitor() {
-        return INSTANCE.ipCount;
-    }
-    
-    public static AtomicInteger getSubscriberCount() {
-        return INSTANCE.subscriberCount;
-    }
-    
-    public static AtomicLong getMaxPushCostMonitor() {
-        return INSTANCE.maxPushCost;
-    }
-    
-    public static AtomicLong getAvgPushCostMonitor() {
-        return INSTANCE.avgPushCost;
-    }
-    
-    public static AtomicLong getLeaderStatusMonitor() {
-        return INSTANCE.leaderStatus;
-    }
-    
-    public static AtomicInteger getTotalPushMonitor() {
-        return INSTANCE.totalPush;
-    }
-    
-    public static AtomicInteger getFailedPushMonitor() {
-        return INSTANCE.failedPush;
-    }
-    
-    public static AtomicInteger getTotalPushCountForAvg() {
-        return INSTANCE.totalPushCountForAvg;
-    }
-    
-    public static AtomicLong getTotalPushCostForAvg() {
-        return INSTANCE.totalPushCostForAvg;
-    }
     
     /**
      * compareAndSetMaxPushCost.
