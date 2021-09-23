dc32969da2dc07a36287905bd0aa4164675f5acd
Wuyunfan <1564396093@qq.com>
2021-09-23 20:52:01 +0800

    Add Derby Database

---
+++ auth/src/main/java/com/alibaba/nacos/auth/persist/EmbeddedPermissionPersistServiceImpl.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.auth.persist;
+
+import com.alibaba.nacos.auth.configuration.ConditionOnEmbeddedStorage;
+import com.alibaba.nacos.auth.model.Page;
+import com.alibaba.nacos.auth.model.PermissionInfo;
+import com.alibaba.nacos.auth.persist.repository.PaginationHelper;
+import com.alibaba.nacos.auth.persist.repository.embedded.DatabaseOperate;
+import com.alibaba.nacos.auth.persist.repository.embedded.EmbeddedStoragePersistServiceImpl;
+import com.alibaba.nacos.common.utils.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * There is no self-augmented primary key.
+ *
+ * @author <a href="mailto:liaochuntao@live.com">liaochuntao</a>
+ */
+@Conditional(value = ConditionOnEmbeddedStorage.class)
+@Component
+public class EmbeddedPermissionPersistServiceImpl implements PermissionPersistService {
+    
+    public static final PermissionRowMapper PERMISSION_ROW_MAPPER = new PermissionRowMapper();
+    
+    @Autowired
+    private DatabaseOperate databaseOperate;
+    
+    @Autowired
+    private EmbeddedStoragePersistServiceImpl persistService;
+    
+    @Override
+    public Page<PermissionInfo> getPermissions(String role, int pageNo, int pageSize) {
+        PaginationHelper<PermissionInfo> helper = persistService.createPaginationHelper();
+        
+        String sqlCountRows = "SELECT count(*) FROM permissions WHERE ";
+
+        String sqlFetchRows = "SELECT role,resource,action FROM permissions WHERE ";
+    
+        String where = " role= ? ";
+        List<String> params = new ArrayList<>();
+        if (StringUtils.isNotBlank(role)) {
+            params = Collections.singletonList(role);
+        } else {
+            where = " 1=1 ";
+        }
+        
+        Page<PermissionInfo> pageInfo = helper
+                .fetchPage(sqlCountRows + where, sqlFetchRows + where, params.toArray(), pageNo,
+                        pageSize, PERMISSION_ROW_MAPPER);
+        
+        if (pageInfo == null) {
+            pageInfo = new Page<>();
+            pageInfo.setTotalCount(0);
+            pageInfo.setPageItems(new ArrayList<>());
+        }
+        return pageInfo;
+    }
+    
+    public static final class PermissionRowMapper implements RowMapper<PermissionInfo> {
+        
+        @Override
+        public PermissionInfo mapRow(ResultSet rs, int rowNum) throws SQLException {
+            PermissionInfo info = new PermissionInfo();
+            info.setResource(rs.getString("resource"));
+            info.setAction(rs.getString("action"));
+            info.setRole(rs.getString("role"));
+            return info;
+        }
+    }
+    
+}
--- auth/src/main/java/com/alibaba/nacos/auth/persist/datasource/ExternalDataSourceServiceImpl.java
+++ auth/src/main/java/com/alibaba/nacos/auth/persist/datasource/ExternalDataSourceServiceImpl.java
@@ -16,6 +16,8 @@
 
 package com.alibaba.nacos.auth.persist.datasource;
 
+import com.alibaba.nacos.auth.model.ConfigInfo4Beta;
+import com.alibaba.nacos.auth.persist.monitor.MetricsMonitor;
 import com.alibaba.nacos.auth.util.AuthPropertyUtil;
 import com.alibaba.nacos.common.utils.ConvertUtils;
 import com.alibaba.nacos.common.utils.InternetAddressUtil;
@@ -25,11 +27,14 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import static com.alibaba.nacos.auth.util.LogUtil.DEFAULT_LOG;
@@ -41,6 +46,8 @@ import static com.alibaba.nacos.auth.util.LogUtil.FATAL_LOG;
  * @author Nacos
  */
 public class ExternalDataSourceServiceImpl implements DataSourceService {
+    
+    public static final ConfigInfo4BetaRowMapper CONFIG_INFO4BETA_ROW_MAPPER = new ConfigInfo4BetaRowMapper();
     
     /**
      * JDBC execute timeout value, unit:second.
@@ -125,7 +132,7 @@ public class ExternalDataSourceServiceImpl implements DataSourceService {
                         isHealthList.add(Boolean.TRUE);
                     });
             new SelectMasterTask().run();
-            //new CheckDbHealthTask().run();
+            new CheckDbHealthTask().run();
         } catch (RuntimeException e) {
             FATAL_LOG.error(DB_LOAD_ERROR_MSG, e);
             throw new IOException(e);
@@ -222,11 +229,11 @@ public class ExternalDataSourceServiceImpl implements DataSourceService {
             
             if (!isFound) {
                 FATAL_LOG.error("[master-db] master db not found.");
-                //MetricsMonitor.getDbException().increment();
+                MetricsMonitor.getDbException().increment();
             }
         }
     }
-    /*
+    
     @SuppressWarnings("PMD.ClassNamingShouldBeCamelRule")
     class CheckDbHealthTask implements Runnable {
         
@@ -256,5 +263,33 @@ public class ExternalDataSourceServiceImpl implements DataSourceService {
                 }
             }
         }
-    }*/
+    }
+    
+    public static final class ConfigInfo4BetaRowMapper implements RowMapper<ConfigInfo4Beta> {
+        
+        @Override
+        public ConfigInfo4Beta mapRow(ResultSet rs, int rowNum) throws SQLException {
+            ConfigInfo4Beta info = new ConfigInfo4Beta();
+            
+            info.setDataId(rs.getString("data_id"));
+            info.setGroup(rs.getString("group_id"));
+            info.setTenant(rs.getString("tenant_id"));
+            info.setAppName(rs.getString("app_name"));
+            info.setBetaIps(rs.getString("beta_ips"));
+            
+            try {
+                info.setContent(rs.getString("content"));
+            } catch (SQLException ignore) {
+            }
+            try {
+                info.setId(rs.getLong("id"));
+            } catch (SQLException ignore) {
+            }
+            try {
+                info.setMd5(rs.getString("md5"));
+            } catch (SQLException ignore) {
+            }
+            return info;
+        }
+    }
 }
