3c0508d2dac0800d817591a78e9dd1730eda19ab
Wuyunfan-BUPT <30968107+Wuyunfan-BUPT@users.noreply.github.com>
2021-09-23 07:13:47 -0500

    Add MySQL DataBase (#6968)

---
+++ auth/src/main/java/com/alibaba/nacos/auth/persist/repository/externel/ExternalStoragePersistServiceImpl.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 1999-2021 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.auth.persist.repository.externel;
+
+import com.alibaba.nacos.auth.configuration.ConditionOnExternalStorage;
+import com.alibaba.nacos.auth.persist.datasource.DataSourceService;
+import com.alibaba.nacos.auth.persist.datasource.DynamicDataSource;
+import com.alibaba.nacos.auth.persist.repository.PaginationHelper;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@SuppressWarnings(value = {"PMD.MethodReturnWrapperTypeRule", "checkstyle:linelength"})
+@Conditional(value = ConditionOnExternalStorage.class)
+@Component
+public class ExternalStoragePersistServiceImpl {
+    
+    private DataSourceService dataSourceService;
+    
+    protected JdbcTemplate jt;
+    
+    /**
+     * init datasource.
+     */
+    @PostConstruct
+    public void init() {
+        dataSourceService = DynamicDataSource.getInstance().getDataSource();
+        
+        jt = getJdbcTemplate();
+    }
+    
+    /**
+     * For unit testing.
+     */
+    public JdbcTemplate getJdbcTemplate() {
+        return this.dataSourceService.getJdbcTemplate();
+    }
+    
+    public <E> PaginationHelper<E> createPaginationHelper() {
+        return new ExternalStoragePaginationHelperImpl<E>(jt);
+    }
+}
+
+
+
+
