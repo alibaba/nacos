f968f712f14ac8b9aa653ba3d4988b0a72147a32
lixiaoshuang <644968328@qq.com>
2021-09-26 22:49:10 +0800

    add unit test for ConfigCryptoFilter

---
+++ client/src/test/java/com/alibaba/nacos/client/config/filter/impl/ConfigCryptoFilterTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.client.config.filter.impl;
+
+import com.alibaba.nacos.api.config.filter.IConfigFilterChain;
+import com.alibaba.nacos.api.config.filter.IConfigRequest;
+import com.alibaba.nacos.api.config.filter.IConfigResponse;
+import com.alibaba.nacos.api.exception.NacosException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * ConfigCryptoFilterTest.
+ *
+ * @author lixiaoshuang
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ConfigCryptoFilterTest {
+    
+    private ConfigCryptoFilter configCryptoFilter;
+    
+    @Mock
+    private ConfigRequest configRequest;
+    
+    @Mock
+    private ConfigResponse configResponse;
+    
+    @Mock
+    private IConfigFilterChain iConfigFilterChain;
+    
+    @Before
+    public void setUp() throws Exception {
+        configCryptoFilter = new ConfigCryptoFilter();
+        
+        Mockito.when(configRequest.getDataId()).thenReturn("cipher-aes-test");
+        Mockito.when(configRequest.getContent()).thenReturn("nacos");
+        
+        Mockito.when(configResponse.getDataId()).thenReturn("cipher-aes-test");
+        Mockito.when(configResponse.getContent()).thenReturn("nacos");
+        Mockito.when(configResponse.getEncryptedDataKey()).thenReturn("1234567890");
+    }
+    
+    @Test
+    public void doFilter() throws NacosException {
+        configCryptoFilter.doFilter(configRequest, null, iConfigFilterChain);
+        
+        Mockito.verify(configRequest).getDataId();
+        Mockito.verify(configRequest).getContent();
+        
+        configCryptoFilter.doFilter(null, configResponse, iConfigFilterChain);
+        
+        Mockito.verify(configResponse).getDataId();
+        Mockito.verify(configResponse).getContent();
+        Mockito.verify(configResponse).getEncryptedDataKey();
+    }
+    
+    @Test
+    public void getOrder() {
+        int order = configCryptoFilter.getOrder();
+        Assert.assertEquals(order, 0);
+    }
+}
