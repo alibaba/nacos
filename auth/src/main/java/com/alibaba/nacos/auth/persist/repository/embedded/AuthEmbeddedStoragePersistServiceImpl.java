e94761efe61f6c421ae529a9672b5e3486205180
Wuyunfan <1564396093@qq.com>
2021-09-27 11:41:33 +0800

    add derby database

---
+++ auth/src/main/java/com/alibaba/nacos/auth/persist/repository/embedded/AuthEmbeddedStoragePersistServiceImpl.java
@@ -0,0 +1,47 @@
+package com.alibaba.nacos.auth.persist.repository.embedded;
+
+import com.alibaba.nacos.auth.configuration.ConditionOnEmbeddedStorage;
+import com.alibaba.nacos.auth.persist.datasource.DataSourceService;
+import com.alibaba.nacos.auth.persist.datasource.AuthDynamicDataSource;
+import com.alibaba.nacos.auth.persist.repository.PaginationHelper;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+
+@Conditional(value = ConditionOnEmbeddedStorage.class)
+@Component
+public class AuthEmbeddedStoragePersistServiceImpl {
+    
+    private DataSourceService dataSourceService;
+    
+    private final DatabaseOperate databaseOperate;
+    
+    public AuthEmbeddedStoragePersistServiceImpl(DatabaseOperate databaseOperate) {
+        this.databaseOperate = databaseOperate;
+    }
+    
+    /**
+     * init datasource.
+     */
+    @PostConstruct
+    public void init() {
+        dataSourceService = AuthDynamicDataSource.getInstance().getDataSource();
+    }
+    
+    public <E> PaginationHelper<E> createPaginationHelper() {
+        return new EmbeddedPaginationHelperImpl<E>(databaseOperate);
+    }
+    
+    /**
+     * For unit testing.
+     */
+    public JdbcTemplate getJdbcTemplate() {
+        return this.dataSourceService.getJdbcTemplate();
+    }
+    
+    public DatabaseOperate getDatabaseOperate() {
+        return databaseOperate;
+    }
+    
+}
