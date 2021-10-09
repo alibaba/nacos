1e32ebc0cea400e0876a09a5a354d6e886b66c73
SunJiFeng <sunjifeng@sfmail.sf-express.com>
2021-10-09 15:16:30 +0800

    move unit test case to ResponseHandlerTest

--- test/core-test/src/test/java/com/alibaba/nacos/test/common/HttpUtils_ITCase.java
+++ test/core-test/src/test/java/com/alibaba/nacos/test/common/HttpUtils_ITCase.java
@@ -16,17 +16,8 @@
 
 package com.alibaba.nacos.test.common;
 
-import com.alibaba.nacos.common.http.handler.ResponseHandler;
-import com.alibaba.nacos.common.model.RestResult;
-import com.alibaba.nacos.common.utils.JacksonUtils;
-import com.alibaba.nacos.core.utils.ClassUtils;
-import com.alibaba.nacos.core.utils.GenericType;
-import org.junit.Assert;
-import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author <a href="mailto:liaochuntao@live.com">liaochuntao</a>
@@ -37,29 +28,5 @@ public class HttpUtils_ITCase {
 
 	private ArrayList<Integer> list = new ArrayList<Integer>(
 			Arrays.asList(1, 2, 3, 4, 5, 6));
-
-	@Test
-	public void test_deserialization_type() throws Exception {
-		String json = JacksonUtils.toJson(list);
-		ArrayList<Integer> tmp = ResponseHandler.convert(json, new GenericType<List<Integer>>(){}.getType());
-		Assert.assertEquals(list, tmp);
-	}
-
-	@Test
-	public void test_rest_result() throws Exception {
-		String json = "{\"code\":200,\"message\":null,\"data\":[{\"USERNAME\":\"nacos\",\"PASSWORD\":\"$2a$10$EuWPZHzz32dJN7jexM34MOeYirDdFAZm2kuWj7VEOJhhZkDrxfvUu\",\"ENABLED\":true}]}";
-		RestResult<Object> result = ResponseHandler.convert(json, new GenericType<RestResult<Object>>(){}.getType());
-		Assert.assertEquals(200, result.getCode());
-		Assert.assertNull(result.getMessage());
-		Assert.assertNotNull(result.getData());
-	}
-
-	@Test
-	public void test_deserialization_class() throws Exception {
-		String json = JacksonUtils.toJson(list);
-		ArrayList<Integer> tmp = ResponseHandler.convert(json, ClassUtils.resolveGenericType(new GenericType<List<Integer>>(){}.getClass()));
-		Assert.assertEquals(list, tmp);
-	}
-
 
 }
