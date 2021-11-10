1c583094e62876e7798f5ef87c2f0eadd47b749c
KomachiSion <xiweng.yy@alibaba-inc.com>
2021-11-10 20:25:14 +0800

    Add service rest api for v2.

--- naming/src/main/java/com/alibaba/nacos/naming/pojo/ClusterDetailInfo.java
+++ naming/src/main/java/com/alibaba/nacos/naming/pojo/ClusterDetailInfo.java
@@ -19,42 +19,20 @@ package com.alibaba.nacos.naming.pojo;
 import com.alibaba.nacos.api.naming.pojo.healthcheck.AbstractHealthChecker;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.Map;
 
 /**
- * Cluster info.
+ * Cluster detail info.
  *
- * @author caogu.wyp
- * @version $Id: ClusterInfo.java, v 0.1 2018-09-17 上午11:36 caogu.wyp Exp $$
+ * @author xiweng.yy
  */
-public class ClusterInfo implements Serializable {
+public class ClusterDetailInfo implements Serializable {
     
     private String clusterName;
     
     private AbstractHealthChecker healthChecker;
     
     private Map<String, String> metadata;
-    
-    private List<IpAddressInfo> hosts;
-    
-    /**
-     * Getter method for property <tt>hosts</tt>.
-     *
-     * @return property value of hosts
-     */
-    public List<IpAddressInfo> getHosts() {
-        return hosts;
-    }
-    
-    /**
-     * Setter method for property <tt>hosts </tt>.
-     *
-     * @param hosts value to be assigned to property hosts
-     */
-    public void setHosts(List<IpAddressInfo> hosts) {
-        this.hosts = hosts;
-    }
     
     public String getClusterName() {
         return clusterName;
