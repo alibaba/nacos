b59de4c21753d6c99e00ba3bdc1f35ac9a5925ae
Cherish 蔡 <hongwen.chw@antgroup.com>
2021-11-16 11:32:57 +0800

    fix 1.4.2 EncryptedData Long-Polling listerning bug in client(LongPollingRunnable).  (#7191)
    * fix(#7039): tmp fix EncryptedData Long-Polling listerning bug.
    
    * fix(#7039): tmp fix EncryptedData Long-Polling listerning bug.

--- client/src/main/java/com/alibaba/nacos/client/config/impl/ClientWorker.java
+++ client/src/main/java/com/alibaba/nacos/client/config/impl/ClientWorker.java
@@ -259,10 +259,11 @@ public class ClientWorker implements Closeable {
             final String md5 = MD5Utils.md5Hex(content, Constants.ENCODE);
             cacheData.setUseLocalConfigInfo(true);
             cacheData.setLocalConfigInfoVersion(path.lastModified());
-            cacheData.setContent(content);
+            // FIXME temporary fix https://github.com/alibaba/nacos/issues/7039
             String encryptedDataKey = LocalEncryptedDataKeyProcessor
                     .getEncryptDataKeyFailover(agent.getName(), dataId, group, tenant);
             cacheData.setEncryptedDataKey(encryptedDataKey);
+            cacheData.setContent(content);
             
             LOGGER.warn(
                     "[{}] [failover-change] failover file created. dataId={}, group={}, tenant={}, md5={}, content={}",
@@ -285,10 +286,11 @@ public class ClientWorker implements Closeable {
             final String md5 = MD5Utils.md5Hex(content, Constants.ENCODE);
             cacheData.setUseLocalConfigInfo(true);
             cacheData.setLocalConfigInfoVersion(path.lastModified());
-            cacheData.setContent(content);
+            // FIXME temporary fix https://github.com/alibaba/nacos/issues/7039
             String encryptedDataKey = LocalEncryptedDataKeyProcessor
                     .getEncryptDataKeyFailover(agent.getName(), dataId, group, tenant);
             cacheData.setEncryptedDataKey(encryptedDataKey);
+            cacheData.setContent(content);
             LOGGER.warn(
                     "[{}] [failover-change] failover file changed. dataId={}, group={}, tenant={}, md5={}, content={}",
                     agent.getName(), dataId, group, tenant, md5, ContentUtils.truncateContent(content));
@@ -547,8 +549,9 @@ public class ClientWorker implements Closeable {
                     try {
                         ConfigResponse response = getServerConfig(dataId, group, tenant, 3000L);
                         CacheData cache = cacheMap.get(GroupKey.getKeyTenant(dataId, group, tenant));
-                        cache.setContent(response.getContent());
+                        // FIXME temporary fix https://github.com/alibaba/nacos/issues/7039
                         cache.setEncryptedDataKey(response.getEncryptedDataKey());
+                        cache.setContent(response.getContent());
                         if (null != response.getConfigType()) {
                             cache.setType(response.getConfigType());
                         }
