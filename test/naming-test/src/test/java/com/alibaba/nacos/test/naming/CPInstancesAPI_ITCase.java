dce94b0f4a18fe95e2d839eebee1b68b3bd4fd20
zrlw <zrlw@sina.com>
2022-02-05 11:36:17 +0800

    delay CPInstancesAPI_ITCase to make sure registry server is working

--- test/naming-test/src/test/java/com/alibaba/nacos/test/naming/CPInstancesAPI_ITCase.java
+++ test/naming-test/src/test/java/com/alibaba/nacos/test/naming/CPInstancesAPI_ITCase.java
@@ -87,6 +87,9 @@ public class CPInstancesAPI_ITCase {
         properties.put(PropertyKeyConst.NAMESPACE, TEST_NAMESPACE_2);
         properties.put(PropertyKeyConst.SERVER_ADDR, "127.0.0.1" + ":" + port);
         naming2 = NamingFactory.createNamingService(properties);
+
+        // Nacos server will not work temporarily if it just started in less than DEFAULT_HEART_BEAT_TIMEOUT ms.   
+        Thread.sleep(Constants.DEFAULT_HEART_BEAT_TIMEOUT + 1000L);
     }
 
     @After
