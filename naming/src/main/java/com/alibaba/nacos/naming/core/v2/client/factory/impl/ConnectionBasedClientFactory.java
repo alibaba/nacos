8eb1a366c4ca71625bad7cfac4d7279ba78f54f6
三辰 <gengtuo.ygt@alibaba-inc.com>
2022-02-21 19:27:56 +0800

    [ISSUE #9210] Fix long class cast exception to REVISION.

--- naming/src/main/java/com/alibaba/nacos/naming/core/v2/client/factory/impl/ConnectionBasedClientFactory.java
+++ naming/src/main/java/com/alibaba/nacos/naming/core/v2/client/factory/impl/ConnectionBasedClientFactory.java
@@ -37,13 +37,13 @@ public class ConnectionBasedClientFactory implements ClientFactory<ConnectionBas
     
     @Override
     public ConnectionBasedClient newClient(String clientId, ClientAttributes attributes) {
-        Long revision = attributes.getClientAttribute(REVISION);
+        long revision = attributes.getClientAttribute(REVISION, 0);
         return new ConnectionBasedClient(clientId, true, revision);
     }
     
     @Override
     public ConnectionBasedClient newSyncedClient(String clientId, ClientAttributes attributes) {
-        Long revision = attributes.getClientAttribute(REVISION);
+        long revision = attributes.getClientAttribute(REVISION, 0);
         return new ConnectionBasedClient(clientId, false, revision);
     }
 }
