7477c02511b0af6645c50c7fdecbf1efb4946c24
hujun3 <510830970@qq.com>
2022-02-21 20:27:15 +0800

    style

--- config/src/test/java/com/alibaba/nacos/config/server/remote/ConfigChangeBatchListenRequestHandlerTest.java
+++ config/src/test/java/com/alibaba/nacos/config/server/remote/ConfigChangeBatchListenRequestHandlerTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.nacos.config.server.remote;
 
 import com.alibaba.nacos.api.config.remote.request.ConfigBatchListenRequest;
@@ -30,7 +46,6 @@ public class ConfigChangeBatchListenRequestHandlerTest extends TestCase {
 
     private RequestMeta requestMeta;
 
-
     @Before
     public void setUp() {
         configQueryRequestHandler = new ConfigChangeBatchListenRequestHandler();
@@ -38,23 +53,22 @@ public class ConfigChangeBatchListenRequestHandlerTest extends TestCase {
         requestMeta = new RequestMeta();
         requestMeta.setClientIp("1.1.1.1");
         Mockito.mockStatic(ConfigCacheService.class);
-
     }
 
     @Test
     public void testHandle() {
-        String dataId="dataId";
-        String group="group";
-        String tenant="tenant";
+        String dataId = "dataId";
+        String group = "group";
+        String tenant = "tenant";
         String groupKey = GroupKey2
                 .getKey(dataId, group, tenant);
         groupKey = StringPool.get(groupKey);
-        when(ConfigCacheService.isUptodate(eq(groupKey),Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(false);
+        when(ConfigCacheService.isUptodate(eq(groupKey), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(false);
         ConfigBatchListenRequest configChangeListenRequest = new ConfigBatchListenRequest();
         configChangeListenRequest.addConfigListenContext(group, dataId, tenant, " ");
         try {
-            ConfigChangeBatchListenResponse configChangeBatchListenResponse = configQueryRequestHandler.handle(configChangeListenRequest
-                    , requestMeta);
+            ConfigChangeBatchListenResponse configChangeBatchListenResponse = configQueryRequestHandler
+                    .handle(configChangeListenRequest, requestMeta);
             boolean hasChange = false;
             for (ConfigChangeBatchListenResponse.ConfigContext changedConfig : configChangeBatchListenResponse.getChangedConfigs()) {
                 if (changedConfig.getDataId().equals(dataId)) {
@@ -67,4 +81,5 @@ public class ConfigChangeBatchListenRequestHandlerTest extends TestCase {
             e.printStackTrace();
         }
     }
+
 }
