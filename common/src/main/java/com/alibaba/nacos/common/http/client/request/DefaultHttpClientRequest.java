23afdfb397dab8d17ab01b1778e4b349c490536c
zrlw <zrlw@sina.com>
2022-02-21 20:25:30 +0800

    set default connection lease request timeout

--- common/src/main/java/com/alibaba/nacos/common/http/client/request/DefaultHttpClientRequest.java
+++ common/src/main/java/com/alibaba/nacos/common/http/client/request/DefaultHttpClientRequest.java
@@ -44,19 +44,24 @@ public class DefaultHttpClientRequest implements HttpClientRequest {
     
     private final CloseableHttpClient client;
     
-    public DefaultHttpClientRequest(CloseableHttpClient client) {
+    private final RequestConfig defaultConfig;
+
+    private static final int DEFAULT_CONNECTION_REQUEST_TIMEOUT = 5000;
+
+    public DefaultHttpClientRequest(CloseableHttpClient client, RequestConfig defaultConfig) {
         this.client = client;
+        this.defaultConfig = defaultConfig;
     }
     
     @Override
     public HttpClientResponse execute(URI uri, String httpMethod, RequestHttpEntity requestHttpEntity)
             throws Exception {
-        HttpRequestBase request = build(uri, httpMethod, requestHttpEntity);
+        HttpRequestBase request = build(uri, httpMethod, requestHttpEntity, defaultConfig);
         CloseableHttpResponse response = client.execute(request);
         return new DefaultClientHttpResponse(response);
     }
     
-    static HttpRequestBase build(URI uri, String method, RequestHttpEntity requestHttpEntity) throws Exception {
+    static HttpRequestBase build(URI uri, String method, RequestHttpEntity requestHttpEntity, RequestConfig defaultConfig) throws Exception {
         final Header headers = requestHttpEntity.getHeaders();
         final BaseHttpMethod httpMethod = BaseHttpMethod.sourceOf(method);
         final HttpRequestBase httpRequestBase = httpMethod.init(uri.toString());
@@ -67,21 +72,29 @@ public class DefaultHttpClientRequest implements HttpClientRequest {
         } else {
             HttpUtils.initRequestEntity(httpRequestBase, requestHttpEntity.getBody(), headers);
         }
-        replaceDefaultConfig(httpRequestBase, requestHttpEntity.getHttpClientConfig());
+        mergeDefaultConfig(httpRequestBase, requestHttpEntity.getHttpClientConfig(), defaultConfig);
         return httpRequestBase;
     }
     
     /**
-     * Replace the HTTP config created by default with the HTTP config specified in the request.
+     * Merge the HTTP config created by default with the HTTP config specified in the request, set connection request timeout if needed.
      *
      * @param requestBase      requestBase
      * @param httpClientConfig http config
      */
-    private static void replaceDefaultConfig(HttpRequestBase requestBase, HttpClientConfig httpClientConfig) {
+    private static void mergeDefaultConfig(HttpRequestBase requestBase, HttpClientConfig httpClientConfig, RequestConfig defaultConfig) {
         if (httpClientConfig == null) {
+            if (defaultConfig.getConnectionRequestTimeout() <= 0) {
+                requestBase.setConfig(RequestConfig.copy(defaultConfig)
+                        .setConnectionRequestTimeout(DEFAULT_CONNECTION_REQUEST_TIMEOUT).build());
+            }
             return;
         }
-        requestBase.setConfig(RequestConfig.custom()
+        requestBase.setConfig(RequestConfig.copy(defaultConfig)
+                .setConnectionRequestTimeout(httpClientConfig.getConnectionRequestTimeout() > 0
+                        ? httpClientConfig.getConnectionRequestTimeout()
+                        : (defaultConfig.getConnectionRequestTimeout() > 0
+                                ? defaultConfig.getConnectionRequestTimeout() : DEFAULT_CONNECTION_REQUEST_TIMEOUT))
                 .setConnectTimeout(httpClientConfig.getConTimeOutMillis())
                 .setSocketTimeout(httpClientConfig.getReadTimeOutMillis()).build());
     }
