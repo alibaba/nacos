56b6b1888023f88a840c2cdfcebbf26715f44d3d
chaos <2499904609@qq.com>
2022-03-03 16:42:01 +0800

    check code style

--- config/src/test/java/com/alibaba/nacos/config/server/remote/RpcConfigChangeNotifierTest.java
+++ config/src/test/java/com/alibaba/nacos/config/server/remote/RpcConfigChangeNotifierTest.java
@@ -1,6 +1,5 @@
 package com.alibaba.nacos.config.server.remote;
 
-import com.alibaba.nacos.api.config.remote.response.ConfigQueryResponse;
 import com.alibaba.nacos.config.server.model.event.LocalDataChangeEvent;
 import com.alibaba.nacos.config.server.utils.GroupKey2;
 import com.alibaba.nacos.core.remote.ConnectionManager;
@@ -10,15 +9,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
 
 /**
  * @ClassName: RpcConfigChangeNotifierTest
@@ -28,15 +24,18 @@ import static org.mockito.Mockito.when;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class RpcConfigChangeNotifierTest {
-
+    
     private RpcConfigChangeNotifier rpcConfigChangeNotifier;
     
     @Mock
     private TpsMonitorManager tpsMonitorManager;
+    
     @Mock
     private ConfigChangeListenContext configChangeListenContext;
+    
     @Mock
     private RpcPushService rpcPushService;
+    
     @Mock
     private ConnectionManager connectionManager;
     
@@ -52,10 +51,11 @@ public class RpcConfigChangeNotifierTest {
     @Test
     public void testOnEvent() {
         final String groupKey = GroupKey2.getKey("nacos.internal.tps.control_rule_1", "nacos", "tenant");
-        final String limitGroupKey = GroupKey2.getKey("nacos.internal.tps.nacos.internal.connection.limit.rule", "nacos", "tenant");
+        final String limitGroupKey = GroupKey2.getKey("nacos.internal.tps.nacos.internal.connection.limit.rule",
+                "nacos", "tenant");
         List<String> betaIps = new ArrayList<>();
         betaIps.add("1.1.1.1");
-        rpcConfigChangeNotifier.onEvent(new LocalDataChangeEvent(groupKey,true,betaIps));
+        rpcConfigChangeNotifier.onEvent(new LocalDataChangeEvent(groupKey, true, betaIps));
         rpcConfigChangeNotifier.onEvent(new LocalDataChangeEvent(limitGroupKey));
     }
 }
