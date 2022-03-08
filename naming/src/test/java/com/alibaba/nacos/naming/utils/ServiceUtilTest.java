60cb8ac693ed7cbde9d95ea13cfddd811693c0d2
chenhao26 <35129699+Chaos-coder@users.noreply.github.com>
2022-03-08 20:16:44 +0800

    add junit for InstanceUtilTest and ServiceUtilTest (#7909)

--- naming/src/test/java/com/alibaba/nacos/naming/utils/ServiceUtilTest.java
+++ naming/src/test/java/com/alibaba/nacos/naming/utils/ServiceUtilTest.java
@@ -16,6 +16,7 @@
 
 package com.alibaba.nacos.naming.utils;
 
+import com.alibaba.nacos.api.naming.pojo.ServiceInfo;
 import com.alibaba.nacos.naming.core.Service;
 import org.junit.Test;
 
@@ -24,6 +25,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ServiceUtilTest {
     
@@ -36,5 +38,47 @@ public class ServiceUtilTest {
         assertEquals(2, actual.size());
         assertEquals("Service", actual.get(0));
         assertEquals("Service", actual.get(1));
+    }
+    
+    @Test
+    public void testSelectServiceWithGroupName() {
+        Service service1 = new Service();
+        service1.setEnabled(true);
+        service1.setName("serviceName");
+        service1.setGroupName("groupName");
+        service1.setAppName("appName");
+        service1.setNamespaceId("namespaceId");
+        service1.setResetWeight(true);
+        Map<String, Service> services = new HashMap<>();
+        services.put("service1", service1);
+        Map<String, Service> resultMap = ServiceUtil.selectServiceWithGroupName(services, "groupName");
+        assertNotNull(resultMap);
+    }
+    
+    @Test
+    public void testSelectServiceBySelector() {
+        Service service1 = new Service();
+        service1.setEnabled(true);
+        service1.setName("serviceName");
+        service1.setGroupName("groupName");
+        service1.setAppName("appName");
+        service1.setNamespaceId("namespaceId");
+        service1.setResetWeight(true);
+        Map<String, Service> serviceMap = new HashMap<>();
+        serviceMap.put("service1", service1);
+        Map<String, Service> resultMap = ServiceUtil.selectServiceBySelector(serviceMap,
+                "{\"type\":\"label\",\"expression\":\"msg\"}");
+        assertNotNull(resultMap);
+    }
+    
+    @Test
+    public void testSelectInstances() {
+        ServiceInfo serviceInfo = new ServiceInfo();
+        serviceInfo.setGroupName("groupName");
+        serviceInfo.setName("serviceName");
+        serviceInfo.setChecksum("checkSum");
+        serviceInfo.setAllIPs(false);
+        ServiceInfo cluster = ServiceUtil.selectInstances(serviceInfo, "cluster");
+        assertNotNull(cluster);
     }
 }
