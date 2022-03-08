fdf90133a2d4ebdf92d78b96e5fdd3c7f28e487b
胡俊 <510830970@qq.com>
2022-03-08 20:11:21 +0800

    [ISSUE#7886]Feature package scan (#7895)
    * for #7886
    
    * nacos style
    
    * nacos style
    
    * code style
    
    * .
    
    * scan nacos style
    
    * class reader
    
    * class reader
    
    * class reader
    
    * class reader
    
    * add comment
    
    * update dependency
    
    * update dependency
    
    * rat update
    check style update
    
    * package change
    
    * pdm exclude
    
    * pdm update
    
    * comment
    
    * comment
    
    * change pkg  final
    
    * code structure update
    
    * code style
    
    * code style update

---
+++ common/src/main/java/com/alibaba/nacos/common/packagescan/DefaultPackageScan.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.common.packagescan;
+
+import com.alibaba.nacos.common.packagescan.classreading.ClassReader;
+import com.alibaba.nacos.common.packagescan.resource.PathMatchingResourcePatternResolver;
+import com.alibaba.nacos.common.packagescan.resource.Resource;
+import com.alibaba.nacos.common.packagescan.resource.ResourcePatternResolver;
+import com.alibaba.nacos.common.packagescan.util.NestedIoException;
+import com.alibaba.nacos.common.utils.ClassUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Scan all appropriate Class object through the package name.
+ *
+ * @author hujun
+ */
+public class DefaultPackageScan implements PackageScan {
+    
+    protected static final Logger LOGGER = LoggerFactory.getLogger(DefaultPackageScan.class);
+    
+    private final PathMatchingResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+    
+    public DefaultPackageScan() {
+    }
+    
+    /**
+     * Scan all appropriate Class object through the package name and Class object.
+     *
+     * @param pkg          package name,for example, com.alibaba.nacos.common
+     * @param requestClass super class
+     * @param <T>          Class type
+     * @return a set contains Class
+     */
+    @Override
+    public <T> Set<Class<T>> getSubTypesOf(String pkg, Class<T> requestClass) {
+        Set<Class<T>> set = new HashSet<>(16);
+        String packageSearchPath =
+                ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + ClassUtils.convertClassNameToResourcePath(pkg) + '/'
+                        + "**/*.class";
+        try {
+            Resource[] resources = resourcePatternResolver.getResources(packageSearchPath);
+            for (Resource resource : resources) {
+                Class<?> scanClass = getClassByResource(resource);
+                if (requestClass.isAssignableFrom(scanClass)) {
+                    set.add((Class<T>) scanClass);
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            LOGGER.error("scan path: {} failed", packageSearchPath, e);
+        }
+        return set;
+    }
+    
+    /**
+     * Scan all appropriate Class object through the package name and annotation.
+     *
+     * @param pkg        package name,for example, com.alibaba.nacos.common
+     * @param annotation annotation
+     * @param <T>        Class type
+     * @return a set contains Class object
+     */
+    @Override
+    public <T> Set<Class<T>> getTypesAnnotatedWith(String pkg, Class<? extends Annotation> annotation) {
+        Set<Class<T>> set = new HashSet<>(16);
+        String packageSearchPath =
+                ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + ClassUtils.convertClassNameToResourcePath(pkg) + '/'
+                        + "**/*.class";
+        try {
+            Resource[] resources = resourcePatternResolver.getResources(packageSearchPath);
+            for (Resource resource : resources) {
+                Class<?> scanClass = getClassByResource(resource);
+                if (scanClass.isAnnotationPresent(annotation)) {
+                    set.add((Class<T>) scanClass);
+                }
+            }
+        } catch (IOException e) {
+            LOGGER.error("scan path: {} failed", packageSearchPath, e);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return set;
+    }
+    
+    private Class<?> getClassByResource(Resource resource) throws IOException, ClassNotFoundException {
+        String className = getClassReader(resource).getClassName();
+        return Class.forName(ClassUtils.resourcePathToConvertClassName(className));
+    }
+    
+    private static ClassReader getClassReader(Resource resource) throws IOException {
+        try (InputStream is = resource.getInputStream()) {
+            try {
+                return new ClassReader(is);
+            } catch (IllegalArgumentException ex) {
+                throw new NestedIoException("ASM ClassReader failed to parse class file - "
+                        + "probably due to a new Java class file version that isn't supported yet: " + resource, ex);
+            }
+        }
+    }
+    
+}
---
+++ common/src/main/java/com/alibaba/nacos/common/packagescan/resource/VfsResource.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.common.packagescan.resource;
+
+import com.alibaba.nacos.common.utils.AbstractAssert;
+import com.alibaba.nacos.common.packagescan.util.NestedIoException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+
+/**
+ * Copy from https://github.com/spring-projects/spring-framework.git, with less modifications
+ * JBoss VFS based {@link Resource} implementation.
+ *
+ * <p>As of Spring 4.0, this class supports VFS 3.x on JBoss AS 6+
+ * (package {@code org.jboss.vfs}) and is in particular compatible with
+ * JBoss AS 7 and WildFly 8+.
+ *
+ * @author Ales Justin
+ * @author Juergen Hoeller
+ * @author Costin Leau
+ * @author Sam Brannen
+ * @see org.jboss.vfs.VirtualFile
+ * @since 3.0
+ */
+public class VfsResource extends AbstractResource {
+
+    private final Object resource;
+
+    /**
+     * Create a new {@code VfsResource} wrapping the given resource handle.
+     *
+     * @param resource a {@code org.jboss.vfs.VirtualFile} instance
+     *                 (untyped in order to avoid a static dependency on the VFS API)
+     */
+    public VfsResource(Object resource) {
+        AbstractAssert.notNull(resource, "VirtualFile must not be null");
+        this.resource = resource;
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return VfsUtils.getInputStream(this.resource);
+    }
+
+    @Override
+    public boolean exists() {
+        return VfsUtils.exists(this.resource);
+    }
+
+    @Override
+    public boolean isReadable() {
+        return VfsUtils.isReadable(this.resource);
+    }
+
+    @Override
+    public URL getUrl() throws IOException {
+        try {
+            return VfsUtils.getUrl(this.resource);
+        } catch (Exception ex) {
+            throw new NestedIoException("Failed to obtain URL for file " + this.resource, ex);
+        }
+    }
+
+    @Override
+    public URI getUri() throws IOException {
+        try {
+            return VfsUtils.getUri(this.resource);
+        } catch (Exception ex) {
+            throw new NestedIoException("Failed to obtain URI for " + this.resource, ex);
+        }
+    }
+
+    @Override
+    public File getFile() throws IOException {
+        return VfsUtils.getFile(this.resource);
+    }
+
+    @Override
+    public long contentLength() throws IOException {
+        return VfsUtils.getSize(this.resource);
+    }
+
+    @Override
+    public long lastModified() throws IOException {
+        return VfsUtils.getLastModified(this.resource);
+    }
+
+    @Override
+    public Resource createRelative(String relativePath) throws IOException {
+        if (!relativePath.startsWith(".") && relativePath.contains("/")) {
+            try {
+                return new VfsResource(VfsUtils.getChild(this.resource, relativePath));
+            } catch (IOException ex) {
+                // fall back to getRelative
+            }
+        }
+
+        return new VfsResource(VfsUtils.getRelative(new URL(getUrl(), relativePath)));
+    }
+
+    @Override
+    public String getFilename() {
+        return VfsUtils.getName(this.resource);
+    }
+
+    @Override
+    public String getDescription() {
+        return "VFS resource [" + this.resource + "]";
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return (this == other || (other instanceof VfsResource
+                && this.resource.equals(((VfsResource) other).resource)));
+    }
+
+    @Override
+    public int hashCode() {
+        return this.resource.hashCode();
+    }
+
+}
