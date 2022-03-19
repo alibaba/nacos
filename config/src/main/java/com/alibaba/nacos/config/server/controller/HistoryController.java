97deaae995d6e81594268cfb62bb5b9cb8bdd195
onewe <admin@onew.me>
2022-03-19 13:36:14 +0800

    remove unused import

--- config/src/main/java/com/alibaba/nacos/config/server/controller/HistoryController.java
+++ config/src/main/java/com/alibaba/nacos/config/server/controller/HistoryController.java
@@ -30,7 +30,6 @@ import com.alibaba.nacos.plugin.auth.constant.ActionTypes;
 import com.alibaba.nacos.plugin.auth.constant.SignType;
 import com.alibaba.nacos.plugin.auth.exception.AccessException;
 import com.alibaba.nacos.plugin.encryption.handler.EncryptionHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
