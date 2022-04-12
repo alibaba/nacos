fad8f6e23580f00a3d71f05011850785dabd4065
lmw <pinkpig_zsy@163.com>
2022-04-12 23:12:58 +0800

    [Optimize] Delete redundant code

--- consistency/src/test/java/com/alibaba/nacos/consistency/SerializeFactoryTest.java
+++ consistency/src/test/java/com/alibaba/nacos/consistency/SerializeFactoryTest.java
@@ -31,48 +31,44 @@ import java.util.concurrent.CopyOnWriteArraySet;
 public class SerializeFactoryTest {
     
     @Test
-    public void testListSerialize() throws Exception {
-        byte[] data = new byte[0];
+    public void testListSerialize() {
         Serializer serializer = SerializeFactory.getDefault();
         
         List<Integer> logsList = new ArrayList<>();
         for (int i = 0; i < 4; i++) {
             logsList.add(i);
         }
-        
-        data = serializer.serialize(logsList);
+        byte[] data = serializer.serialize(logsList);
         Assert.assertNotEquals(0, data.length);
         
-        ArrayList<Integer> list = serializer.deserialize(data, ArrayList.class);
+        ArrayList list = serializer.deserialize(data, ArrayList.class);
         System.out.println(list);
     }
     
     @Test
     public void testMapSerialize() {
-        byte[] data = new byte[0];
         Serializer serializer = SerializeFactory.getDefault();
         Map<Integer, Integer> logsMap = new HashMap<>();
         for (int i = 0; i < 4; i++) {
             logsMap.put(i, i);
         }
-        data = serializer.serialize(logsMap);
+        byte[] data = serializer.serialize(logsMap);
         Assert.assertNotEquals(0, data.length);
-        Map<Integer, Integer> result = serializer.deserialize(data, HashMap.class);
+        HashMap result = serializer.deserialize(data, HashMap.class);
         System.out.println(result);
     }
     
     @Test
     public void testSetSerialize() {
-        byte[] data = new byte[0];
         Serializer serializer = SerializeFactory.getDefault();
         Set<Integer> logsMap = new CopyOnWriteArraySet<>();
         for (int i = 0; i < 4; i++) {
             logsMap.add(i);
         }
         
-        data = serializer.serialize(logsMap);
+        byte[] data = serializer.serialize(logsMap);
         Assert.assertNotEquals(0, data.length);
-        Set<Integer> result = serializer.deserialize(data, HashSet.class);
+        HashSet result = serializer.deserialize(data, HashSet.class);
         System.out.println(result);
     }
     
