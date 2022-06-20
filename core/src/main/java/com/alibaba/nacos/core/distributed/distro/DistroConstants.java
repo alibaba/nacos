c8d609efc1d8d6502dbb62770609f62a302fd6e5
liqipeng <liqipeng@users.noreply.github.com>
2022-06-20 11:24:12 +0800

    specify nacos.core.protocol.distro.data.load.timeoutMs default 30s

--- core/src/main/java/com/alibaba/nacos/core/distributed/distro/DistroConstants.java
+++ core/src/main/java/com/alibaba/nacos/core/distributed/distro/DistroConstants.java
@@ -49,6 +49,6 @@ public class DistroConstants {
     
     public static final String DATA_LOAD_TIMEOUT_MILLISECONDS = "nacos.core.protocol.distro.data.load.timeoutMs";
     
-    public static final long DEFAULT_DATA_LOAD_TIMEOUT_MILLISECONDS = 3000L;
+    public static final long DEFAULT_DATA_LOAD_TIMEOUT_MILLISECONDS = 30000L;
     
 }
