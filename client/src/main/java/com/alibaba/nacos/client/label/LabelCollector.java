14c8ebc83110fdcbe815ec5d4c42f13705bbea1b
shiyiue1102 <liuzunfei@gmail.com>
2022-06-23 14:47:54 +0800

    checkstyle

--- client/src/main/java/com/alibaba/nacos/client/label/LabelCollector.java
+++ client/src/main/java/com/alibaba/nacos/client/label/LabelCollector.java
@@ -2,15 +2,20 @@ package com.alibaba.nacos.client.label;
 
 import java.util.Map;
 
+/**
+ * client labels collector.
+ */
 public interface LabelCollector {
+    
     /**
-     *
-     * @return labels set by developers
+     * get all labels.
+     * @return labels set by developers.
      */
     Map<String, String> getLabels();
-
+    
     /**
-     * get label value by label name
+     * get label value by label name.
+     *
      * @param labelName label name
      * @return label value
      */
