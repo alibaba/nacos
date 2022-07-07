491814ec6cfb9f281505a3ad80584076f9dd770e
onewe <admin@onew.me>
2022-07-07 16:41:02 +0800

    add some default values and enhance SystemEnvPropertySource

--- client/src/main/java/com/alibaba/nacos/client/env/SystemEnvPropertySource.java
+++ client/src/main/java/com/alibaba/nacos/client/env/SystemEnvPropertySource.java
@@ -33,8 +33,44 @@ class SystemEnvPropertySource extends AbstractPropertySource {
     
     @Override
     String getProperty(String key) {
-        // nacos.home == NACOS_HOME
-        key = key.replace('.', '_').toUpperCase();
-        return env.get(key);
+        String checkedKey = checkPropertyName(key);
+        if (checkedKey == null) {
+            final String upperCaseKey = key.toUpperCase();
+            if (!upperCaseKey.equals(key)) {
+                checkedKey = checkPropertyName(upperCaseKey);
+            }
+        }
+        if (checkedKey == null) {
+            return null;
+        }
+        return env.get(checkedKey);
+    }
+    
+    private String checkPropertyName(String name) {
+        // Check name as-is
+        if (containsKey(name)) {
+            return name;
+        }
+        // Check name with just dots replaced
+        String noDotName = name.replace('.', '_');
+        if (!name.equals(noDotName) && containsKey(noDotName)) {
+            return noDotName;
+        }
+        // Check name with just hyphens replaced
+        String noHyphenName = name.replace('-', '_');
+        if (!name.equals(noHyphenName) && containsKey(noHyphenName)) {
+            return noHyphenName;
+        }
+        // Check name with dots and hyphens replaced
+        String noDotNoHyphenName = noDotName.replace('-', '_');
+        if (!noDotName.equals(noDotNoHyphenName) && containsKey(noDotNoHyphenName)) {
+            return noDotNoHyphenName;
+        }
+        // Give up
+        return null;
+    }
+    
+    private boolean containsKey(String name) {
+        return this.env.containsKey(name);
     }
 }
