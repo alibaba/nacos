111da60d9e83948517e203e608c49bea4a00cdd8
onewe <admin@onew.me>
2022-07-11 20:22:02 +0800

    add some unit test for auth
    - use http basic authentication
    - disable csrf

--- address/src/main/java/com/alibaba/nacos/address/config/NacosSecurityConfiguration.java
+++ address/src/main/java/com/alibaba/nacos/address/config/NacosSecurityConfiguration.java
@@ -17,8 +17,7 @@
 package com.alibaba.nacos.address.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -29,7 +28,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class NacosSecurityConfiguration extends WebSecurityConfigurerAdapter {
     
     @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().mvcMatchers(HttpMethod.GET, "/*/*");
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requestMatcherRegistry -> requestMatcherRegistry.mvcMatchers("/nacos/v1/as/**").authenticated())
+                .csrf().disable().httpBasic();
     }
 }
