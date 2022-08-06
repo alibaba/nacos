9c0e681a1f6346e9dff0489529a156e1192b95e8
RocketEngine26 <tmingyu26@gmail.com>
2022-08-06 15:14:15 +0800

    Implement event listening and remove polling for NacosServiceInfoResourceWatcher

--- istio/src/main/java/com/alibaba/nacos/istio/common/NacosServiceInfoResourceWatcher.java
+++ istio/src/main/java/com/alibaba/nacos/istio/common/NacosServiceInfoResourceWatcher.java
@@ -17,24 +17,33 @@
 package com.alibaba.nacos.istio.common;
 
 import com.alibaba.nacos.api.naming.pojo.ServiceInfo;
+import com.alibaba.nacos.common.notify.NotifyCenter;
+import com.alibaba.nacos.common.notify.listener.SmartSubscriber;
 import com.alibaba.nacos.istio.model.IstioService;
 import com.alibaba.nacos.istio.util.IstioCrdUtil;
 import com.alibaba.nacos.naming.core.v2.ServiceManager;
+import com.alibaba.nacos.naming.core.v2.event.publisher.NamingEventPublisherFactory;
+import com.alibaba.nacos.naming.core.v2.event.service.ServiceEvent;
 import com.alibaba.nacos.naming.core.v2.index.ServiceStorage;
 import com.alibaba.nacos.naming.core.v2.pojo.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ * NacosServiceInfoResourceWatcher.
+ *
  * @author special.fy
  */
 @org.springframework.stereotype.Service
-public class NacosServiceInfoResourceWatcher implements Runnable {
+public class NacosServiceInfoResourceWatcher extends SmartSubscriber {
 
     private final Map<String, IstioService> serviceInfoMap = new ConcurrentHashMap<>(16);
 
@@ -44,58 +53,78 @@ public class NacosServiceInfoResourceWatcher implements Runnable {
     @Autowired
     private EventProcessor eventProcessor;
 
-    @Override
-    public void run() {
-        boolean changed = false;
-
-        // Query all services to see if any of them have changes.
-        Set<String> namespaces =  ServiceManager.getInstance().getAllNamespaces();
-        Set<String> allServices = new HashSet<>();
-        for (String namespace : namespaces) {
-            Set<Service>  services = ServiceManager.getInstance().getSingletons(namespace);
-            if (services.isEmpty()) {
-                continue;
-            }
-
-            for (Service service : services) {
-                String serviceName = IstioCrdUtil.buildServiceNameForServiceEntry(service);
-                allServices.add(serviceName);
-
-                IstioService old = serviceInfoMap.get(serviceName);
-                // Service not changed
-                if (old != null && old.getRevision().equals(service.getRevision())) {
-                    continue;
-                }
-
-                // Update the resource
-                changed = true;
-                ServiceInfo serviceInfo = serviceStorage.getPushData(service);
-                if (!serviceInfo.isValid()) {
-                    serviceInfoMap.remove(serviceName);
-                    continue;
-                }
-
-                if (old != null) {
-                    serviceInfoMap.put(serviceName, new IstioService(service, serviceInfo, old));
-                } else {
-                    serviceInfoMap.put(serviceName, new IstioService(service, serviceInfo));
-                }
-            }
-        }
-
-        for (String key : serviceInfoMap.keySet()) {
-            if (!allServices.contains(key)) {
-                changed = true;
-                serviceInfoMap.remove(key);
-            }
-        }
-
-        if (changed) {
-            eventProcessor.notify(Event.SERVICE_UPDATE_EVENT);
-        }
+    public NacosServiceInfoResourceWatcher() {
+        NotifyCenter.registerSubscriber(this, NamingEventPublisherFactory.getInstance());
     }
 
     public Map<String, IstioService> snapshot() {
         return new HashMap<>(serviceInfoMap);
+    }
+    
+    @Override
+    public List<Class<? extends com.alibaba.nacos.common.notify.Event>> subscribeTypes() {
+        List<Class<? extends com.alibaba.nacos.common.notify.Event>> result = new LinkedList<>();
+        result.add(ServiceEvent.ServiceChangedEvent.class);
+        return result;
+    }
+    
+    /**
+     * description:onEvent.
+     * @param: event
+     * @return: void
+     */
+    public void onEvent(com.alibaba.nacos.common.notify.Event event) {
+        if (event instanceof ServiceEvent.ServiceChangedEvent) {
+            // If service changed, push to all subscribers.
+            ServiceEvent.ServiceChangedEvent serviceChangedEvent = (ServiceEvent.ServiceChangedEvent) event;
+            Service service = serviceChangedEvent.getService();
+            
+            try {
+                handleEvent(service);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    
+    /**
+     * description:handleEvent.
+     * @param: [service]
+     * @return: void
+     */
+    public void handleEvent(Service eventService) throws IOException {
+        String namespace = eventService.getNamespace();
+        Set<Service> services = ServiceManager.getInstance().getSingletons(namespace);
+        Set<String> allServices = new HashSet<>();
+        
+        for (Service service : services) {
+            String serviceName = IstioCrdUtil.buildServiceNameForServiceEntry(service);
+            allServices.add(serviceName);
+    
+            IstioService old = serviceInfoMap.get(serviceName);
+            // Service not changed
+            if (old != null && old.getRevision().equals(service.getRevision())) {
+                continue;
+            }
+            // Update the resource
+            ServiceInfo serviceInfo = serviceStorage.getPushData(service);
+            if (!serviceInfo.isValid()) {
+                serviceInfoMap.remove(serviceName);
+            }
+    
+            if (old != null) {
+                serviceInfoMap.put(serviceName, new IstioService(service, serviceInfo, old));
+            } else {
+                serviceInfoMap.put(serviceName, new IstioService(service, serviceInfo));
+            }
+        }
+        
+        for (String key : serviceInfoMap.keySet()) {
+            if (!allServices.contains(key)) {
+                serviceInfoMap.remove(key);
+            }
+        }
+        
+        eventProcessor.notify(Event.SERVICE_UPDATE_EVENT);
     }
 }
