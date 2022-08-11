f939cd554e03ac4908b7ea62ffbde3362bf8b9c3
longyu <2009longyu@163.com>
2022-08-11 15:43:17 +0800

    fix update serialVersionUID auto generate

--- naming/src/main/java/com/alibaba/nacos/naming/core/v2/pojo/BatchInstanceData.java
+++ naming/src/main/java/com/alibaba/nacos/naming/core/v2/pojo/BatchInstanceData.java
@@ -27,7 +27,7 @@ import java.util.List;
  */
 public class BatchInstanceData implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 7845847904043098494L;
     
     private List<String> namespaces;
     
