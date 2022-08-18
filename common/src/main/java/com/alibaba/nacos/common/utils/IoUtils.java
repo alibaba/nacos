2a15bb7d80a3879c76d62669fb48452149d1fc43
Shaoyu Liu <455028117@qq.com>
2022-08-18 21:23:05 +0800

    [ISSUE #8993]Close input stream and output stream by try with resource[nacos-common]

--- common/src/main/java/com/alibaba/nacos/common/utils/IoUtils.java
+++ common/src/main/java/com/alibaba/nacos/common/utils/IoUtils.java
@@ -86,13 +86,15 @@ public class IoUtils {
      * @param encoding encoding.
      * @return byte[]
      */
-    public static byte[] tryCompress(String str, String encoding) throws Exception {
+    public static byte[] tryCompress(String str, String encoding) {
         if (str == null || str.length() == 0) {
             return new byte[0];
         }
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         try (GZIPOutputStream gzip = new GZIPOutputStream(out)) {
             gzip.write(str.getBytes(encoding));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return out.toByteArray();
     }
