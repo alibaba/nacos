4585c12d40f413ad01c560043bf677444a8b19e0
chenhao26 <35129699+chenhao26-nineteen@users.noreply.github.com>
2022-08-22 15:43:22 +0800

    add some utils UT (#9006)
    add some utils UT

---
+++ common/src/test/java/com/alibaba/nacos/common/utils/to/User.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.common.utils.to;
+
+public class User {
+    private Integer id;
+
+    private String name;
+    
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
