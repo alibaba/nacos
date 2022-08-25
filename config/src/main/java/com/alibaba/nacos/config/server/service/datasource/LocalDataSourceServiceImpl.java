43de737056d1146bc78491d86e30689af2e80d41
yjqg6666 <jinking.this@gmail.com>
2022-08-25 16:54:18 +0800

    [#344] rename initialization sql files (#8969)

--- config/src/main/java/com/alibaba/nacos/config/server/service/datasource/LocalDataSourceServiceImpl.java
+++ config/src/main/java/com/alibaba/nacos/config/server/service/datasource/LocalDataSourceServiceImpl.java
@@ -94,12 +94,12 @@ public class LocalDataSourceServiceImpl implements DataSourceService {
             throw new RuntimeException("datasource is null");
         }
         try {
-            execute(ds.getConnection(), "META-INF/schema.sql");
+            execute(ds.getConnection(), "META-INF/derby-schema.sql");
         } catch (Exception e) {
             if (LogUtil.DEFAULT_LOG.isErrorEnabled()) {
                 LogUtil.DEFAULT_LOG.error(e.getMessage(), e);
             }
-            throw new NacosRuntimeException(NacosException.SERVER_ERROR, "load schema.sql error.", e);
+            throw new NacosRuntimeException(NacosException.SERVER_ERROR, "load derby-schema.sql error.", e);
         }
     }
     
@@ -210,7 +210,7 @@ public class LocalDataSourceServiceImpl implements DataSourceService {
         InputStream sqlFileIn = null;
         try {
             File file = new File(
-                    EnvUtil.getNacosHome() + File.separator + "conf" + File.separator + "schema.sql");
+                    EnvUtil.getNacosHome() + File.separator + "conf" + File.separator + "derby-schema.sql");
             if (StringUtils.isBlank(EnvUtil.getNacosHome()) || !file.exists()) {
                 ClassLoader classLoader = getClass().getClassLoader();
                 URL url = classLoader.getResource(sqlFile);
