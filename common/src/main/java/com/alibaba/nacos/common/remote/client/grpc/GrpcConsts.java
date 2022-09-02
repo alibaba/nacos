93142a3272882f8f9610b2c217917b6c24cdd304
Karson <karsontao@hotmail.com>
2022-09-02 15:48:14 +0800

    Add configurable for GrpcClient health check retry times (#9056)
    * add configurable for GrpcClient health check retry.
    
    * add some unit test for GrpcClient health check retry.
    
    * reformat code style
    
    * some grpc params move to GrpcConsts
    
    * add GRpcConfigLabel
    
    * reformat style
    
    * reformat style
    
    * reformat style
    
    * reformat style
    
    * reformat style

---
+++ common/src/main/java/com/alibaba/nacos/common/remote/client/grpc/GrpcConsts.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 1999-2020 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.common.remote.client.grpc;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * GrpcConsts.
+ *
+ * @author karsonto
+ */
+public class GrpcConsts {
+    
+    public static final String NACOS_SERVER_GRPC_PORT_OFFSET_KEY = "nacos.server.grpc.port.offset";
+    
+    public static final String NACOS_CLIENT_GRPC = "nacos.remote.client.grpc";
+    
+    @GRpcConfigLabel
+    public static final String NACOS_CLIENT_GRPC_THREADPOOL_KEEPALIVETIME = NACOS_CLIENT_GRPC + ".pool.alive";
+    
+    @GRpcConfigLabel
+    public static final String NACOS_CLIENT_GRPC_TIMEOUT = NACOS_CLIENT_GRPC + ".timeout";
+    
+    @GRpcConfigLabel
+    public static final String NACOS_CLIENT_GRPC_QUEUESIZE = NACOS_CLIENT_GRPC + ".queue.size";
+    
+    @GRpcConfigLabel
+    public static final String NACOS_CLIENT_GRPC_HEALTHCHECK_RETRY_TIMES = NACOS_CLIENT_GRPC + ".health.retry";
+    
+    @GRpcConfigLabel
+    public static final String NACOS_CLIENT_GRPC_HEALTHCHECK_TIMEOUT = NACOS_CLIENT_GRPC + ".health.timeout";
+    
+    @GRpcConfigLabel
+    public static final String MAX_INBOUND_MESSAGE_SIZE = NACOS_CLIENT_GRPC + ".maxinbound.message.size";
+    
+    @GRpcConfigLabel
+    public static final String KEEP_ALIVE_TIME = NACOS_CLIENT_GRPC + ".keep.alive.millis";
+    
+    private static final Set<String> CONFIG_NAMES = new HashSet<>();
+    
+    @Documented
+    @Target(ElementType.FIELD)
+    @Retention(RetentionPolicy.RUNTIME)
+    protected @interface GRpcConfigLabel {
+    
+    }
+    
+    static {
+        Class clazz = GrpcConsts.class;
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            declaredField.setAccessible(true);
+            if (declaredField.getType().equals(String.class) && null != declaredField.getAnnotation(
+                    GRpcConfigLabel.class)) {
+                try {
+                    CONFIG_NAMES.add((String) declaredField.get(null));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    public static Set<String> getRpcParams() {
+        return Collections.unmodifiableSet(CONFIG_NAMES);
+    }
+}
