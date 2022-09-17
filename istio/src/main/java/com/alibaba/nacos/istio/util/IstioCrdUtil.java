86b0b5402eb37bd410e6638ff8ba324dfff74ee6
Roger3581321 <roger.yxt@aliyun.com>
2022-09-17 19:49:28 +0800

    Update IstioCrdUtil.java

--- istio/src/main/java/com/alibaba/nacos/istio/util/IstioCrdUtil.java
+++ istio/src/main/java/com/alibaba/nacos/istio/util/IstioCrdUtil.java
@@ -73,7 +73,7 @@ public class IstioCrdUtil {
             if (StringUtils.isNotEmpty(instance.getMetadata().get("protocol"))) {
                 protocol = instance.getMetadata().get("protocol");
 
-                if ("triple".equals(protocol)||"tri".equals(protocol)){
+                if ("triple".equals(protocol) || "tri".equals(protocol)){
                     protocol = "grpc";
                 }
             }
