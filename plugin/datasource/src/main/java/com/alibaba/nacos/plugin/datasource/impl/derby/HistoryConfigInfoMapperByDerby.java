96029b736d22963a5d34474c83ce5c522dbc5f9c
李晓双 Li Xiao Shuang <644968328@qq.com>
2022-10-25 00:58:34 +0800

    fix sql (#9386)

--- plugin/datasource/src/main/java/com/alibaba/nacos/plugin/datasource/impl/derby/HistoryConfigInfoMapperByDerby.java
+++ plugin/datasource/src/main/java/com/alibaba/nacos/plugin/datasource/impl/derby/HistoryConfigInfoMapperByDerby.java
@@ -52,10 +52,9 @@ public class HistoryConfigInfoMapperByDerby extends AbstractMapper implements Hi
     }
     
     @Override
-    public String findConfigHistoryFetchRows(int startRow, int pageSize) {
-        return "SELECT nid,data_id,group_id,tenant_id,app_name,src_ip,src_user,op_type,gmt_create,gmt_modified "
-                + "FROM his_config_info WHERE data_id = ? AND group_id = ? AND tenant_id = ? "
-                + "ORDER BY nid DESC" + " OFFSET " + startRow + " ROWS FETCH NEXT " + pageSize + " ROWS ONLY";
+    public String findConfigHistoryFetchRows() {
+        return  "SELECT nid,data_id,group_id,tenant_id,app_name,src_ip,src_user,op_type,gmt_create,gmt_modified FROM his_config_info "
+                + "WHERE data_id = ? AND group_id = ? AND tenant_id = ? ORDER BY nid DESC";
     }
     
     @Override
