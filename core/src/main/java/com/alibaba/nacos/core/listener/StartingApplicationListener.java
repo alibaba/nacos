21683957e5a875b71ac9cbf9805e59f6a025b903
无处不在 <42382506+wuchubuzai2018@users.noreply.github.com>
2022-11-07 17:17:52 +0800

    [ISSUE #9415]update setExternalStorage logic，support more db check,add 8 count unit test (#9470)
    * [impr]update setExternalStorage method logic，support more db check,add 8 count unit test method
    
    * update comment desc

--- core/src/main/java/com/alibaba/nacos/core/listener/StartingApplicationListener.java
+++ core/src/main/java/com/alibaba/nacos/core/listener/StartingApplicationListener.java
@@ -72,6 +72,8 @@ public class StartingApplicationListener implements NacosApplicationListener {
     
     private static final String DEFAULT_DATABASE = "mysql";
     
+    private static final String DERBY_DATABASE = "derby";
+    
     private static final String DATASOURCE_PLATFORM_PROPERTY = "spring.datasource.platform";
     
     private static final String DEFAULT_DATASOURCE_PLATFORM = "";
@@ -243,8 +245,10 @@ public class StartingApplicationListener implements NacosApplicationListener {
     private void judgeStorageMode(ConfigurableEnvironment env) {
         
         // External data sources are used by default in cluster mode
-        boolean useExternalStorage = (DEFAULT_DATABASE.equalsIgnoreCase(
-                env.getProperty(DATASOURCE_PLATFORM_PROPERTY, DEFAULT_DATASOURCE_PLATFORM)));
+        // External data sources are used by default in cluster mode
+        String platform = env.getProperty(DATASOURCE_PLATFORM_PROPERTY, DEFAULT_DATASOURCE_PLATFORM);
+        boolean useExternalStorage = !DEFAULT_DATASOURCE_PLATFORM.equalsIgnoreCase(platform)
+                && !DERBY_DATABASE.equalsIgnoreCase(platform);
         
         // must initialize after setUseExternalDB
         // This value is true in stand-alone mode and false in cluster mode
