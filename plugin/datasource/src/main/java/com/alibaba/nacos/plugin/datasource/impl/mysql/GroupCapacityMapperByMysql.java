aec4342b1d04f67c1f8f1d9b41ce716f2be824a0
李晓双 Li Xiao Shuang <644968328@qq.com>
2022-11-09 09:24:23 +0800

    [ISSUE #9506] Sql extraction of group_capacity table (#9507)
    * sql extraction of group_capacity table
    
    * sql extraction of group_capacity table
    
    * sql extraction of group_capacity table
    
    * sql extraction of group_capacity table

---
+++ plugin/datasource/src/main/java/com/alibaba/nacos/plugin/datasource/impl/mysql/GroupCapacityMapperByMysql.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 1999-2022 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.plugin.datasource.impl.mysql;
+
+import com.alibaba.nacos.plugin.datasource.constants.DataSourceConstant;
+import com.alibaba.nacos.plugin.datasource.constants.TableConstant;
+import com.alibaba.nacos.plugin.datasource.mapper.AbstractMapper;
+import com.alibaba.nacos.plugin.datasource.mapper.GroupCapacityMapper;
+
+/**
+ * The derby implementation of {@link GroupCapacityMapper}.
+ *
+ * @author lixiaoshuang
+ */
+public class GroupCapacityMapperByMysql extends AbstractMapper implements GroupCapacityMapper {
+    
+    @Override
+    public String getTableName() {
+        return TableConstant.GROUP_CAPACITY;
+    }
+    
+    @Override
+    public String getDataSource() {
+        return DataSourceConstant.MYSQL;
+    }
+    
+    @Override
+    public String insertIntoSelect() {
+        return "INSERT INTO group_capacity (group_id, quota, `usage`, `max_size`, max_aggr_count, max_aggr_size,gmt_create,"
+                + " gmt_modified) SELECT ?, ?, count(*), ?, ?, ?, ?, ? FROM config_info";
+    }
+    
+    @Override
+    public String insertIntoSelectByWhere() {
+        return "INSERT INTO group_capacity (group_id, quota,`usage`, `max_size`, max_aggr_count, max_aggr_size, gmt_create,"
+                + " gmt_modified) SELECT ?, ?, count(*), ?, ?, ?, ?, ? FROM config_info WHERE group_id=? AND tenant_id = ''";
+    }
+    
+    @Override
+    public String incrementUsageByWhereQuotaEqualZero() {
+        return "UPDATE group_capacity SET `usage` = `usage` + 1, gmt_modified = ? WHERE group_id = ? AND `usage` < ? AND quota = 0";
+    }
+    
+    @Override
+    public String incrementUsageByWhereQuotaNotEqualZero() {
+        return "UPDATE group_capacity SET `usage` = `usage` + 1, gmt_modified = ? WHERE group_id = ? AND `usage` < quota AND quota != 0";
+    }
+    
+    @Override
+    public String incrementUsageByWhere() {
+        return "UPDATE group_capacity SET `usage` = `usage` + 1, gmt_modified = ? WHERE group_id = ?";
+    }
+    
+    @Override
+    public String decrementUsageByWhere() {
+        return "UPDATE group_capacity SET `usage` = `usage` - 1, gmt_modified = ? WHERE group_id = ? AND `usage` > 0";
+    }
+    
+    @Override
+    public String updateUsage() {
+        return "UPDATE group_capacity SET `usage` = (SELECT count(*) FROM config_info), gmt_modified = ? WHERE group_id = ?";
+    }
+    
+    @Override
+    public String updateUsageByWhere() {
+        return "UPDATE group_capacity SET `usage` = (SELECT count(*) FROM config_info WHERE group_id=? AND tenant_id = ''),"
+                + " gmt_modified = ? WHERE group_id= ?";
+    }
+    
+    @Override
+    public String selectGroupInfoBySize() {
+        return "SELECT id, group_id FROM group_capacity WHERE id > ? LIMIT ?";
+    }
+}
+
