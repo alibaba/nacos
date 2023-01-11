cf3829a3fcdc14c62f9ec305ec3a93ac6f352449
shanhy <xz.xiaoshan@gmail.com>
2023-01-11 10:27:26 +0800

    code style

--- common/src/main/java/com/alibaba/nacos/common/utils/NamespaceUtil.java
+++ common/src/main/java/com/alibaba/nacos/common/utils/NamespaceUtil.java
@@ -17,50 +17,50 @@
 package com.alibaba.nacos.common.utils;
 
 /**
- * namespace(tenant) util.
- * Because config and naming treat namespace(tenant) differently,
- * this tool class can only be used by the config module.
- * @author klw(213539@qq.com)
+ * namespace(tenant) util. Because config and naming treat namespace(tenant) differently, this tool class can only be
+ * used by the config module.
+ *
+ * @author klw(213539 @ qq.com)
  * @date 2020/10/12 17:56
  */
 public class NamespaceUtil {
     
     private static final String NAMESPACE_PUBLIC_KEY = "public";
-
+    
     // public id，默认值为 ""
     private static String NAMESPACE_PUBLIC_ID_DEFAULT = "";
-
+    
     private static final String NAMESPACE_NULL_KEY = "null";
-
+    
     /**
      * Treat the namespace(tenant) parameters with values of "public" and "null" as an empty string.
+     *
      * @param tenant namespace(tenant) id
      * @return java.lang.String A namespace(tenant) string processed
      */
     public static String processNamespaceParameter(String tenant) {
-        if (StringUtils.isBlank(tenant) || NAMESPACE_PUBLIC_KEY.equalsIgnoreCase(tenant) || NAMESPACE_NULL_KEY
-                .equalsIgnoreCase(tenant)) {
+        if (StringUtils.isBlank(tenant) || NAMESPACE_PUBLIC_KEY.equalsIgnoreCase(tenant)
+                || NAMESPACE_NULL_KEY.equalsIgnoreCase(tenant)) {
             return getNamespaceDefaultId();
         }
         return tenant.trim();
     }
-
+    
     /**
-     * Set default namespace id.
-     * Invoke settings at system startup
+     * Set default namespace id. Invoke settings at system startup
      *
      * @param namespaceDefaultId
      */
-    public static void setNamespaceDefaultId(String namespaceDefaultId){
+    public static void setNamespaceDefaultId(String namespaceDefaultId) {
         NAMESPACE_PUBLIC_ID_DEFAULT = namespaceDefaultId;
     }
-
+    
     /**
      * Get default namespace id
      *
      * @return
      */
-    public static String getNamespaceDefaultId(){
+    public static String getNamespaceDefaultId() {
         return NAMESPACE_PUBLIC_ID_DEFAULT;
     }
 }
