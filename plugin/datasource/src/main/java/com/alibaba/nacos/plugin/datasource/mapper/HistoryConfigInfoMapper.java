92451578bafa665c031ff000b1ad08c5ec6a7e56
redscarfz <893486496@qq.com>
2023-01-16 20:24:23 +0800

    fix 配置管理历史版本分页

--- plugin/datasource/src/main/java/com/alibaba/nacos/plugin/datasource/mapper/HistoryConfigInfoMapper.java
+++ plugin/datasource/src/main/java/com/alibaba/nacos/plugin/datasource/mapper/HistoryConfigInfoMapper.java
@@ -71,6 +71,14 @@ public interface HistoryConfigInfoMapper extends Mapper {
      */
     String detailPreviousConfigHistory();
 
+    /**
+     * Get previous config page.
+     * The default sql:
+     * SELECT nid,data_id,group_id,tenant_id,app_name,content,md5,src_user,src_ip,op_type,gmt_create,gmt_modified
+     * FROM his_config_info WHERE nid = (SELECT max(nid) FROM his_config_info WHERE id = ?) limit pageSize offset (pageNo-1)*pageSize
+     *
+     * @return The sql of getting previous config detail.
+     */
     String findConfigHistory4PageFetchRows(int pageNo, int pageSize);
 
 }
