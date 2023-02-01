7dd15d16f1737b44a141114a3e40f0417388585f
Weizhan Yun <yunweizhan@gmail.com>
2023-02-01 21:13:29 +0800

    add license

--- plugin-default-impl/src/test/java/com/alibaba/nacos/plugin/auth/impl/jwt/NacosJwtParserTest.java
+++ plugin-default-impl/src/test/java/com/alibaba/nacos/plugin/auth/impl/jwt/NacosJwtParserTest.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright 1999-2022 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.nacos.plugin.auth.impl.jwt;
 
-import com.alibaba.nacos.plugin.auth.exception.AccessException;
 import junit.framework.TestCase;
 import org.junit.Test;
 
@@ -21,7 +36,7 @@ public class NacosJwtParserTest extends TestCase {
     }
     
     @Test
-    public void testParseWith16Key() throws AccessException {
+    public void testParseWith16Key() {
         Exception e = null;
         try {
             new NacosJwtParser("SecretKey0123456");
@@ -29,6 +44,7 @@ public class NacosJwtParserTest extends TestCase {
             e = exception;
         }
         
+        assertNotNull(e);
         assertEquals(IllegalArgumentException.class, e.getClass());
         
     }
