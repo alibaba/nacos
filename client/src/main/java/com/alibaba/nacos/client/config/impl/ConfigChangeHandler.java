6fa58025ed12dd734f7339857e20ac0f76f41b83
zhangshenao <zhangshenao@kkworld.com>
2023-02-08 16:40:35 +0800

    Remove Unused Imports

--- client/src/main/java/com/alibaba/nacos/client/config/impl/ConfigChangeHandler.java
+++ client/src/main/java/com/alibaba/nacos/client/config/impl/ConfigChangeHandler.java
@@ -23,7 +23,6 @@ import com.alibaba.nacos.common.spi.NacosServiceLoader;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
