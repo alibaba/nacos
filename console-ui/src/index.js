ef556a7fbdc9b954e35766a537ebe00fb43f528a
onewe <admin@onew.me>
2023-02-22 19:34:07 +0800

    [ISSUE #9448] use local font files
    - add some font files to local path
    
    Close #9448

--- console-ui/src/index.js
+++ console-ui/src/index.js
@@ -55,8 +55,6 @@ import reducers from './reducers';
 import { changeLanguage } from './reducers/locale';
 
 import './index.scss';
-import '@alifd/theme-design-pro/variables.css';
-import '@alifd/theme-design-pro/dist/next.var.css';
 import PropTypes from 'prop-types';
 
 module.hot && module.hot.accept();
