bec28e9c98ebcbfdec95b768d444eb2298a2591e
4yDX3906 <don_liming@163.com>
2023-02-24 18:25:25 +0800

    fix:The if statement should be followed by parentheses

--- api/src/main/java/com/alibaba/nacos/api/grpc/auto/Payload.java
+++ api/src/main/java/com/alibaba/nacos/api/grpc/auto/Payload.java
@@ -165,8 +165,12 @@ private static final long serialVersionUID = 0L;
   @Override
   public final boolean isInitialized() {
     byte isInitialized = memoizedIsInitialized;
-    if (isInitialized == 1) return true;
-    if (isInitialized == 0) return false;
+    if (isInitialized == 1) {
+      return true;
+    }
+    if (isInitialized == 0) {
+      return false;
+    }
 
     memoizedIsInitialized = 1;
     return true;
