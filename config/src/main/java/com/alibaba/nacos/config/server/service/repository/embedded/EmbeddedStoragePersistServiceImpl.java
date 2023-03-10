52ebaf80df8f671e210f5201daafb11e51072bfb
KomachiSion <xiweng.yy@alibaba-inc.com>
2023-03-10 09:54:17 +0800

    Revert "[ISSUE #9821] Update data-source-plugin input and output parameters (#9851)"
    This reverts commit 1ff7a3f6

--- config/src/main/java/com/alibaba/nacos/config/server/service/repository/embedded/EmbeddedStoragePersistServiceImpl.java
+++ config/src/main/java/com/alibaba/nacos/config/server/service/repository/embedded/EmbeddedStoragePersistServiceImpl.java
@@ -58,8 +58,6 @@ import com.alibaba.nacos.plugin.datasource.mapper.ConfigInfoTagMapper;
 import com.alibaba.nacos.plugin.datasource.mapper.ConfigTagsRelationMapper;
 import com.alibaba.nacos.plugin.datasource.mapper.HistoryConfigInfoMapper;
 import com.alibaba.nacos.plugin.datasource.mapper.TenantInfoMapper;
-import com.alibaba.nacos.plugin.datasource.model.MapperContext;
-import com.alibaba.nacos.plugin.datasource.model.MapperResult;
 import com.alibaba.nacos.plugin.encryption.handler.EncryptionHandler;
 import com.alibaba.nacos.sys.env.EnvUtil;
 import org.apache.commons.collections.CollectionUtils;
@@ -755,18 +753,12 @@ public class EmbeddedStoragePersistServiceImpl implements PersistService {
         final String tenantTmp = StringUtils.isBlank(tenant) ? StringUtils.EMPTY : tenant;
         ConfigInfoAggrMapper configInfoAggrMapper = mapperManager.findMapper(dataSourceService.getDataSourceType(),
                 TableConstant.CONFIG_INFO_AGGR);
-    
-        MapperContext context = new MapperContext();
-        context.put("datum_id", datumList);
-        context.put("data_id", dataId);
-        context.put("group_id", group);
-        context.put("tenant_id", tenantTmp);
-        
-        MapperResult mapperResult = configInfoAggrMapper.batchRemoveAggr(context);
-        String sql = mapperResult.getSql();
-        List<Object> paramList = mapperResult.getParamList();
-        Object[] args = paramList.toArray();
-        
+        final String sql = configInfoAggrMapper.batchRemoveAggr(datumList.size());
+        final Object[] args = new Object[3 + datumList.size()];
+        args[0] = dataId;
+        args[1] = group;
+        args[2] = tenantTmp;
+        System.arraycopy(datumList.toArray(), 0, args, 3, datumList.size());
         EmbeddedStorageContextUtils.addSqlContext(sql, args);
         
         try {
@@ -1089,20 +1081,12 @@ public class EmbeddedStoragePersistServiceImpl implements PersistService {
         }
         final String tenantTmp = StringUtils.isBlank(tenant) ? StringUtils.EMPTY : tenant;
         ConfigInfoAggrMapper configInfoAggrMapper = mapperManager.findMapper(dataSourceService.getDataSourceType(), TableConstant.CONFIG_INFO_AGGR);
-    
-        MapperContext context = new MapperContext();
-        context.put("datum_id", datumIds);
-        context.put("isIn", true);
-        context.put("data_id", dataId);
-        context.put("group_id", group);
-        context.put("tenant_id", tenantTmp);
+        String sql = configInfoAggrMapper.aggrConfigInfoCount(datumIds.size(), isIn);
         
-        MapperResult mapperResult = configInfoAggrMapper.aggrConfigInfoCount(context);
+        List<Object> objectList = com.alibaba.nacos.common.utils.CollectionUtils.list(dataId, group, tenantTmp);
+        objectList.addAll(datumIds);
         
-        String sql = mapperResult.getSql();
-        Object[] args = mapperResult.getParamList().toArray();
-    
-        Integer result = databaseOperate.queryOne(sql, args, Integer.class);
+        Integer result = databaseOperate.queryOne(sql, objectList.toArray(), Integer.class);
         if (result == null) {
             throw new IllegalArgumentException("aggrConfigInfoCount error");
         }
@@ -1404,17 +1388,10 @@ public class EmbeddedStoragePersistServiceImpl implements PersistService {
     public List<ConfigInfoAggr> findConfigInfoAggr(String dataId, String group, String tenant) {
         String tenantTmp = StringUtils.isBlank(tenant) ? StringUtils.EMPTY : tenant;
         ConfigInfoAggrMapper configInfoAggrMapper = mapperManager.findMapper(dataSourceService.getDataSourceType(), TableConstant.CONFIG_INFO_AGGR);
-    
-        MapperContext context = new MapperContext();
-        context.put("data_id", dataId);
-        context.put("group_id", group);
-        context.put("tenant_id", tenantTmp);
-    
-        MapperResult mapperResult = configInfoAggrMapper.findConfigInfoAggrIsOrdered(context);
-        String sql = mapperResult.getSql();
-        Object[] args = mapperResult.getParamList().toArray();
+        String sql = configInfoAggrMapper.findConfigInfoAggrIsOrdered();
         
-        return databaseOperate.queryMany(sql, args, CONFIG_INFO_AGGR_ROW_MAPPER);
+        return databaseOperate.queryMany(sql, new Object[] {dataId, group, tenantTmp}, CONFIG_INFO_AGGR_ROW_MAPPER);
+        
     }
     
     @Override
@@ -1423,22 +1400,13 @@ public class EmbeddedStoragePersistServiceImpl implements PersistService {
         String tenantTmp = StringUtils.isBlank(tenant) ? StringUtils.EMPTY : tenant;
         ConfigInfoAggrMapper configInfoAggrMapper = mapperManager.findMapper(dataSourceService.getDataSourceType(), TableConstant.CONFIG_INFO_AGGR);
         final int startRow = (pageNo - 1) * pageSize;
-        final String sqlCountRows = configInfoAggrMapper
+        String sqlCountRows = configInfoAggrMapper
                 .select(Arrays.asList("count(*)"), Arrays.asList("data_id", "group_id", "tenant_id"));
-        
-        MapperContext context = new MapperContext();
-        context.put("data_id", dataId);
-        context.put("group_id", group);
-        context.put("tenant_id", tenantTmp);
-        context.put("startRow", startRow);
-        context.put("pageSize", pageSize);
-        MapperResult mapperResult = configInfoAggrMapper.findConfigInfoAggrByPageFetchRows(context);
-        String sqlFetchRows = mapperResult.getSql();
-        Object[] sqlFethcArgs = mapperResult.getParamList().toArray();
+        String sqlFetchRows = configInfoAggrMapper.findConfigInfoAggrByPageFetchRows(startRow, pageSize);
         
         PaginationHelper<ConfigInfoAggr> helper = createPaginationHelper();
         return helper.fetchPageLimit(sqlCountRows, new Object[] {dataId, group, tenantTmp}, sqlFetchRows,
-               sqlFethcArgs, pageNo, pageSize, CONFIG_INFO_AGGR_ROW_MAPPER);
+                new Object[] {dataId, group, tenantTmp}, pageNo, pageSize, CONFIG_INFO_AGGR_ROW_MAPPER);
     }
     
     @Override
