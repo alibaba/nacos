10b6e14c0390c0be2c2a6bac1e51ae5977782966
杨翊 SionYang <xiweng.yy@alibaba-inc.com>
2023-03-10 10:25:25 +0800

    Develop revert#9851 #9967 (#10069)
    * Revert "[ISSUE #9821] Update ConfigInfoBetaMapper data-source-plugin input and output parameters (#9967)"
    
    This reverts commit d16023c1
    
    * Revert "[ISSUE #9821] Update data-source-plugin input and output parameters (#9851)"
    
    This reverts commit 1ff7a3f6

--- plugin/datasource/src/test/java/com/alibaba/nacos/plugin/datasource/impl/mysql/ConfigInfoAggrMapperByMySqlTest.java
+++ plugin/datasource/src/test/java/com/alibaba/nacos/plugin/datasource/impl/mysql/ConfigInfoAggrMapperByMySqlTest.java
@@ -16,19 +16,13 @@
 
 package com.alibaba.nacos.plugin.datasource.impl.mysql;
 
-import com.alibaba.nacos.common.utils.CollectionUtils;
 import com.alibaba.nacos.plugin.datasource.constants.DataSourceConstant;
 import com.alibaba.nacos.plugin.datasource.constants.TableConstant;
-import com.alibaba.nacos.plugin.datasource.model.MapperContext;
-import com.alibaba.nacos.plugin.datasource.model.MapperResult;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
-import java.util.Arrays;
-import java.util.List;
 
 @RunWith(JUnit4.class)
 public class ConfigInfoAggrMapperByMySqlTest {
@@ -42,99 +36,32 @@ public class ConfigInfoAggrMapperByMySqlTest {
     
     @Test
     public void testBatchRemoveAggr() {
-        List<String> datumList = Arrays.asList("1", "2", "3", "4", "5");
-        String dataId = "data-id";
-        String groupId = "group-id";
-        String tenantId = "tenant-id";
-    
-        List<String> argList = CollectionUtils.list(dataId, groupId, tenantId);
-        argList.addAll(datumList);
-    
-        MapperContext context = new MapperContext();
-        context.putWhereParameter("datum_id", datumList);
-        context.putWhereParameter("data_id", dataId);
-        context.putWhereParameter("group_id", groupId);
-        context.putWhereParameter("tenant_id", tenantId);
-        
-        MapperResult mapperResult = configInfoAggrMapperByMySql.batchRemoveAggr(context);
-        String sql = mapperResult.getSql();
-        List<Object> paramList = mapperResult.getParamList();
-        
+        String sql = configInfoAggrMapperByMySql.batchRemoveAggr(5);
         Assert.assertEquals(sql, "DELETE FROM config_info_aggr WHERE data_id = ? AND group_id = ? AND tenant_id = ? "
                 + "AND datum_id IN (?, ?, ?, ?, ?)");
-        
-        Assert.assertEquals(paramList, argList);
     }
     
     @Test
     public void testAggrConfigInfoCount() {
-        List<String> datumIds = Arrays.asList("1", "2", "3", "4", "5");
-        String dataId = "data-id";
-        String groupId = "group-id";
-        String tenantId = "tenant-id";
-    
-        List<String> argList = CollectionUtils.list(dataId, groupId, tenantId);
-        argList.addAll(datumIds);
-    
-        MapperContext context = new MapperContext();
-        context.putWhereParameter("datum_id", datumIds);
-        context.putWhereParameter("isIn", true);
-        context.putWhereParameter("data_id", dataId);
-        context.putWhereParameter("group_id", groupId);
-        context.putWhereParameter("tenant_id", tenantId);
-        MapperResult mapperResult = configInfoAggrMapperByMySql.aggrConfigInfoCount(context);
-        String sql = mapperResult.getSql();
-        List<Object> paramList = mapperResult.getParamList();
-    
+        String sql = configInfoAggrMapperByMySql.aggrConfigInfoCount(5, true);
         Assert.assertEquals(sql,
                 "SELECT count(*) FROM config_info_aggr WHERE data_id = ? AND group_id = ? AND tenant_id = ? "
                         + "AND datum_id IN (?, ?, ?, ?, ?)");
-        Assert.assertEquals(paramList, argList);
     }
     
     @Test
     public void testFindConfigInfoAggrIsOrdered() {
-        String dataId = "data-id";
-        String groupId = "group-id";
-        String tenantId = "tenant-id";
-        
-        MapperContext context = new MapperContext();
-        context.putWhereParameter("data_id", dataId);
-        context.putWhereParameter("group_id", groupId);
-        context.putWhereParameter("tenant_id", tenantId);
-        
-        MapperResult mapperResult = configInfoAggrMapperByMySql.findConfigInfoAggrIsOrdered(context);
-        String sql = mapperResult.getSql();
-        List<Object> paramList = mapperResult.getParamList();
-    
+        String sql = configInfoAggrMapperByMySql.findConfigInfoAggrIsOrdered();
         Assert.assertEquals(sql, "SELECT data_id,group_id,tenant_id,datum_id,app_name,content FROM "
                 + "config_info_aggr WHERE data_id = ? AND group_id = ? AND tenant_id = ? ORDER BY datum_id");
-        Assert.assertEquals(paramList, Arrays.asList(dataId, groupId, tenantId));
     }
     
     @Test
     public void testFindConfigInfoAggrByPageFetchRows() {
-        String dataId = "data-id";
-        String groupId = "group-id";
-        String tenantId = "tenant-id";
-        Integer startRow = 0;
-        Integer pageSize = 5;
-        
-        MapperContext context = new MapperContext();
-        context.putWhereParameter("data_id", dataId);
-        context.putWhereParameter("group_id", groupId);
-        context.putWhereParameter("tenant_id", tenantId);
-        context.putWhereParameter("startRow", startRow);
-        context.putWhereParameter("pageSize", pageSize);
-        
-        MapperResult mapperResult = configInfoAggrMapperByMySql.findConfigInfoAggrByPageFetchRows(context);
-        String sql = mapperResult.getSql();
-        List<Object> paramList = mapperResult.getParamList();
-        
+        String sql = configInfoAggrMapperByMySql.findConfigInfoAggrByPageFetchRows(0, 5);
         Assert.assertEquals(sql,
                 "SELECT data_id,group_id,tenant_id,datum_id,app_name,content FROM config_info_aggr WHERE "
                         + "data_id= ? AND group_id= ? AND tenant_id= ? ORDER BY datum_id LIMIT 0,5");
-        Assert.assertEquals(paramList, Arrays.asList(dataId, groupId, tenantId));
     }
     
     @Test
