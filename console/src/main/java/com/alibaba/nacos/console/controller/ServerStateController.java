9fe0be6b738094d5929d01e01663b47f448a5119
KomachiSion <xiweng.yy@alibaba-inc.com>
2023-03-27 16:06:19 +0800

    Add announcement api.

--- console/src/main/java/com/alibaba/nacos/console/controller/ServerStateController.java
+++ console/src/main/java/com/alibaba/nacos/console/controller/ServerStateController.java
@@ -16,13 +16,18 @@
 
 package com.alibaba.nacos.console.controller;
 
+import com.alibaba.nacos.common.model.RestResult;
+import com.alibaba.nacos.common.model.RestResultUtils;
+import com.alibaba.nacos.sys.env.EnvUtil;
 import com.alibaba.nacos.sys.module.ModuleState;
 import com.alibaba.nacos.sys.module.ModuleStateHolder;
+import com.alibaba.nacos.sys.utils.DiskUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,6 +39,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("/v1/console/server")
 public class ServerStateController {
+    
+    private static final String ANNOUNCEMENT_FILE = "announcement.conf";
     
     /**
      * Get server state of current server.
@@ -49,4 +56,13 @@ public class ServerStateController {
         return ResponseEntity.ok().body(serverState);
     }
     
+    @GetMapping("/announcement")
+    public RestResult<String> getAnnouncement() {
+        File announcementFile = new File(EnvUtil.getConfPath(), ANNOUNCEMENT_FILE);
+        String announcement = null;
+        if (announcementFile.exists() && announcementFile.isFile()) {
+            announcement = DiskUtils.readFile(announcementFile);
+        }
+        return RestResultUtils.success(announcement);
+    }
 }
