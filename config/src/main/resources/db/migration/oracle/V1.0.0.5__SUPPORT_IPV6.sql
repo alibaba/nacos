23e230f781ed6dd27ef9bb702cba94206b54197d
Lei Zhang <zhanglei@apache.org>
2023-03-27 14:37:13 +0800

    221 Support Flyway MySQL 8.0

---
+++ config/src/main/resources/db/migration/oracle/V1.0.0.5__SUPPORT_IPV6.sql
@@ -0,0 +1,4 @@
+alter table config_info_tag modify src_ip varchar(50);
+alter table his_config_info modify src_ip varchar(50);
+alter table config_info modify src_ip varchar(50);
+alter table config_info_beta modify src_ip varchar(50);
