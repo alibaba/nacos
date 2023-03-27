a52c77d2853372680b33387a1e132853d3a6c947
Lei Zhang <zhanglei@apache.org>
2023-03-27 10:04:12 +0800

    221 Flyway Support Oracle

--- config/src/main/resources/db/migration/oracle/V1.0.0.4__NACOS221PATH.sql
+++ config/src/main/resources/db/migration/oracle/V1.0.0.4__NACOS221PATH.sql
@@ -1,8 +1,8 @@
-alter table config_info modify group_id varchar(128) null;
-alter table config_info_aggr modify group_id varchar(128) null;
-alter table config_info add encrypted_data_key text not null;
-alter table config_info_beta add encrypted_data_key text not null;
-alter table his_config_info add encrypted_data_key text not null;
+alter table config_info modify group_id varchar(128);
+alter table config_info_aggr modify group_id varchar(128);
+alter table config_info add encrypted_data_key varchar2(4000) not null;
+alter table config_info_beta add encrypted_data_key varchar2(4000) not null;
+alter table his_config_info add encrypted_data_key varchar2(4000) not null;
 
 
 
