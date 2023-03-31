ef447b2e26dcd8736dfc8dd12ce312483211ebbb
杨翊 SionYang <xiweng.yy@alibaba-inc.com>
2023-03-31 16:41:11 +0800

    Add unit test for api module (#10241)
    * Add more situation UT for NetUtils
    
    * Add UT for api module selector package
    
    * Add UT for api module exception package
    
    * Add UT for api module cmdb package

---
+++ api/src/test/java/com/alibaba/nacos/api/selector/context/CmdbContextTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 1999-2023 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.api.selector.context;
+
+import com.alibaba.nacos.api.naming.pojo.Instance;
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+public class CmdbContextTest {
+    
+    @Test
+    public void testToString() {
+        CmdbContext<Instance> cmdbContext = new CmdbContext<>();
+        cmdbContext.setProviders(Collections.singletonList(new CmdbContext.CmdbInstance<>()));
+        cmdbContext.setConsumer(new CmdbContext.CmdbInstance<>());
+        System.out.println(cmdbContext.toString());
+        assertEquals(
+                "CmdbContext{consumer=CmdbInstance{entity=null, instance=null}, providers=[CmdbInstance{entity=null, instance=null}]}",
+                cmdbContext.toString());
+    }
+}
