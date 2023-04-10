7f2496f0238e6e0e7e739b292cbddd7e6e306acd
985492783 <985492783@qq.com>
2023-04-10 18:03:08 +0800

    修复test bug

--- console/src/test/java/com/alibaba/nacos/console/controller/ServerStateControllerTest.java
+++ console/src/test/java/com/alibaba/nacos/console/controller/ServerStateControllerTest.java
@@ -27,6 +27,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.mock.env.MockEnvironment;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -51,8 +53,12 @@ public class ServerStateControllerTest {
     
     private static final String CONSOLE_URL = "/v1/console/server/state";
     
+    private ConfigurableEnvironment environment;
+    
     @Before
     public void setUp() {
+        environment = new MockEnvironment();
+        EnvUtil.setEnvironment(environment);
         mockmvc = MockMvcBuilders.standaloneSetup(serverStateController).build();
     }
     
