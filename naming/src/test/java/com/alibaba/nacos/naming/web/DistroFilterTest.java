5b6caa2db6b7d0d0a310fe48e714f2679955040f
yuzhaohui <zhaohui.yu@bianlifeng.com>
2023-04-15 16:29:53 +0800

    Add test for async forward for DistroFilter

---
+++ naming/src/test/java/com/alibaba/nacos/naming/web/DistroFilterTest.java
@@ -0,0 +1,131 @@
+package com.alibaba.nacos.naming.web;
+
+import com.alibaba.nacos.auth.config.AuthConfigs;
+import com.alibaba.nacos.core.code.ControllerMethodsCache;
+import com.alibaba.nacos.core.distributed.distro.DistroConstants;
+import com.alibaba.nacos.naming.controllers.InstanceController;
+import com.alibaba.nacos.naming.core.DistroMapper;
+import com.alibaba.nacos.sys.env.EnvUtil;
+import com.alibaba.nacos.sys.utils.ApplicationUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockserver.client.server.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.mock.env.MockEnvironment;
+import org.springframework.mock.web.MockFilterChain;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.util.ReflectionUtils;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.AsyncEvent;
+import javax.servlet.AsyncListener;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+public class DistroFilterTest {
+    
+    @Mock
+    private DistroMapper distroMapper;
+    
+    @Mock
+    private ControllerMethodsCache controllerMethodsCache;
+    
+    @Mock
+    private DistroTagGenerator distroTagGenerator;
+    
+    @Spy
+    protected MockEnvironment environment;
+    
+    @Mock
+    private ConfigurableApplicationContext context;
+    
+    @InjectMocks
+    private DistroFilter distroFilter;
+    
+    private ClientAndServer mockServer;
+    
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        EnvUtil.setContextPath("/nacos");
+        
+        EnvUtil.setEnvironment(environment);
+        when(environment.getProperty(DistroConstants.NACOS_ASYNC_DISTRO_FORWARD_NAME, Boolean.class,
+                DistroConstants.DEFAULT_ASYNC_DISTRO_FORWARD_VALUE)).thenReturn(true);
+        
+        ApplicationUtils.injectContext(context);
+        when(context.getBean(AuthConfigs.class)).thenReturn(new AuthConfigs());
+        
+        System.setProperty(DistroConstants.NACOS_ASYNC_DISTRO_FORWARD_NAME, "true");
+        mockServer = ClientAndServer.startClientAndServer(8080);
+        
+        final Method register = ReflectionUtils.findMethod(InstanceController.class, "register",
+                HttpServletRequest.class);
+        when(controllerMethodsCache.getMethod(any())).thenReturn(register);
+        when(distroTagGenerator.getResponsibleTag(any())).thenReturn("tag");
+        when(distroMapper.responsible(anyString())).thenReturn(false);
+        when(distroMapper.mapSrv(anyString())).thenReturn("127.0.0.1:8080");
+    }
+    
+    @Test
+    public void given_async_can_forward_request_to_target() throws ServletException, IOException, InterruptedException {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setMethod("POST");
+        request.setRequestURI("/nacos/v1/ns/instance");
+        request.setAsyncSupported(true);
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        final MockFilterChain filterChain = new MockFilterChain();
+        
+        new MockServerClient("127.0.0.1", 8080).when(request().withMethod("POST").withPath("/nacos/v1/ns/instance"))
+                .respond(response().withStatusCode(200).withBody("ok").withDelay(TimeUnit.SECONDS, 1));
+        distroFilter.doFilter(request, response, filterChain);
+        final AsyncContext asyncContext = request.getAsyncContext();
+        CountDownLatch latch = new CountDownLatch(1);
+        asyncContext.addListener(new AsyncListener() {
+            @Override
+            public void onComplete(AsyncEvent event) throws IOException {
+                latch.countDown();
+            }
+            
+            @Override
+            public void onTimeout(AsyncEvent event) throws IOException {
+                latch.countDown();
+            }
+            
+            @Override
+            public void onError(AsyncEvent event) throws IOException {
+                latch.countDown();
+            }
+            
+            @Override
+            public void onStartAsync(AsyncEvent event) throws IOException {
+            
+            }
+        });
+        latch.await();
+        Assert.assertEquals("ok", response.getContentAsString());
+    }
+    
+    @After
+    public void tearDown() {
+        mockServer.stop();
+    }
+}
