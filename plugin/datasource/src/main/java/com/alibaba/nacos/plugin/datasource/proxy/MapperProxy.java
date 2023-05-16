f12a8b272b252da3653b7ab7d0e50c6cf1b887d2
scwlkq <1575711583@qq.com>
2023-05-16 12:45:45 +0800

    规范格式

--- plugin/datasource/src/main/java/com/alibaba/nacos/plugin/datasource/proxy/MapperProxy.java
+++ plugin/datasource/src/main/java/com/alibaba/nacos/plugin/datasource/proxy/MapperProxy.java
@@ -18,6 +18,7 @@ package com.alibaba.nacos.plugin.datasource.proxy;
 
 import com.alibaba.nacos.common.utils.JacksonUtils;
 import com.alibaba.nacos.plugin.datasource.mapper.Mapper;
+import com.alibaba.nacos.plugin.datasource.model.MapperResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,20 +36,20 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  * @author hyx
  **/
 public class MapperProxy implements InvocationHandler {
-    
+
     private static final Logger LOGGER = LoggerFactory.getLogger(MapperProxy.class);
-    
+
     private Mapper mapper;
-    
+
     private static final Map<String, MapperProxy> SINGLE_MAPPER_PROXY_MAP = new HashMap<>(16);
-    
+
     private static final ReadWriteLock LOCK = new ReentrantReadWriteLock(true);
-    
+
     public <R> R createProxy(Mapper mapper) {
         this.mapper = mapper;
         return (R) Proxy.newProxyInstance(MapperProxy.class.getClassLoader(), mapper.getClass().getInterfaces(), this);
     }
-    
+
     /**
      * create proxy-mapper single instead of using method createProxy.
      */
@@ -72,21 +73,21 @@ public class MapperProxy implements InvocationHandler {
             LOCK.readLock().unlock();
         }
     }
-    
+
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         Object invoke = method.invoke(mapper, args);
-        
+
         String className = mapper.getClass().getSimpleName();
         String methodName = method.getName();
-        
+
         String sql;
-        if(invoke instanceof MapperResult){
-            sql = ((MapperResult)invoke).getSql();
-        }else{
+        if (invoke instanceof MapperResult) {
+            sql = ((MapperResult) invoke).getSql();
+        } else {
             sql = invoke.toString();
         }
-        
+
         LOGGER.info("[{}] METHOD : {}, SQL : {}, ARGS : {}", className, methodName, sql, JacksonUtils.toJson(args));
         return invoke;
     }
