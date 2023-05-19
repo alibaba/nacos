51903edd82a3f244a5e21f173f011485298f348f
nov.lzf <liuzunfei@gmail.com>
2023-05-19 10:20:37 +0800

    add out date connection active detection,expel it when client no response. (#10509)
    * add out date connection active detection,expel it when client no response.
    
    * add out date connection active detection,expel it when client no response.

--- core/src/main/java/com/alibaba/nacos/core/remote/NacosRuntimeConnectionEjector.java
+++ core/src/main/java/com/alibaba/nacos/core/remote/NacosRuntimeConnectionEjector.java
@@ -18,10 +18,20 @@
 
 package com.alibaba.nacos.core.remote;
 
+import com.alibaba.nacos.api.remote.RequestCallBack;
+import com.alibaba.nacos.api.remote.request.ClientDetectionRequest;
+import com.alibaba.nacos.api.remote.response.Response;
+import com.alibaba.nacos.common.remote.exception.ConnectionAlreadyClosedException;
+import com.alibaba.nacos.common.utils.CollectionUtils;
 import com.alibaba.nacos.core.monitor.MetricsMonitor;
 import com.alibaba.nacos.plugin.control.Loggers;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * nacos runtime connection ejector.
@@ -39,16 +49,87 @@ public class NacosRuntimeConnectionEjector extends RuntimeConnectionEjector {
      */
     public void doEject() {
         try {
-            
+    
+            Loggers.CONNECTION.info("Connection check task start");
+    
             Map<String, Connection> connections = connectionManager.connections;
             int totalCount = connections.size();
-            Loggers.CONNECTION.info("Connection check task start");
             MetricsMonitor.getLongConnectionMonitor().set(totalCount);
             int currentSdkClientCount = connectionManager.currentSdkClientCount();
             
-            Loggers.CONNECTION
-                    .info("Long connection metrics detail ,Total count ={}, sdkCount={},clusterCount={}", totalCount,
-                            currentSdkClientCount, (totalCount - currentSdkClientCount));
+            Loggers.CONNECTION.info("Long connection metrics detail ,Total count ={}, sdkCount={},clusterCount={}",
+                    totalCount, currentSdkClientCount, (totalCount - currentSdkClientCount));
+            
+            Set<String> outDatedConnections = new HashSet<>();
+            long now = System.currentTimeMillis();
+            //outdated connections collect.
+            for (Map.Entry<String, Connection> entry : connections.entrySet()) {
+                Connection client = entry.getValue();
+                if (now - client.getMetaInfo().getLastActiveTime() >= KEEP_ALIVE_TIME) {
+                    outDatedConnections.add(client.getMetaInfo().getConnectionId());
+                }
+            }
+            
+            // check out date connection
+            Loggers.CONNECTION.info("Out dated connection ,size={}", outDatedConnections.size());
+            if (CollectionUtils.isNotEmpty(outDatedConnections)) {
+                Set<String> successConnections = new HashSet<>();
+                final CountDownLatch latch = new CountDownLatch(outDatedConnections.size());
+                for (String outDateConnectionId : outDatedConnections) {
+                    try {
+                        Connection connection = connectionManager.getConnection(outDateConnectionId);
+                        if (connection != null) {
+                            ClientDetectionRequest clientDetectionRequest = new ClientDetectionRequest();
+                            connection.asyncRequest(clientDetectionRequest, new RequestCallBack() {
+                                @Override
+                                public Executor getExecutor() {
+                                    return null;
+                                }
+                                
+                                @Override
+                                public long getTimeout() {
+                                    return 5000L;
+                                }
+                                
+                                @Override
+                                public void onResponse(Response response) {
+                                    latch.countDown();
+                                    if (response != null && response.isSuccess()) {
+                                        connection.freshActiveTime();
+                                        successConnections.add(outDateConnectionId);
+                                    }
+                                }
+                                
+                                @Override
+                                public void onException(Throwable e) {
+                                    latch.countDown();
+                                }
+                            });
+                            
+                            Loggers.CONNECTION.info("[{}]send connection active request ", outDateConnectionId);
+                        } else {
+                            latch.countDown();
+                        }
+                        
+                    } catch (ConnectionAlreadyClosedException e) {
+                        latch.countDown();
+                    } catch (Exception e) {
+                        Loggers.CONNECTION.error("[{}]Error occurs when check client active detection ,error={}",
+                                outDateConnectionId, e);
+                        latch.countDown();
+                    }
+                }
+                
+                latch.await(5000L, TimeUnit.MILLISECONDS);
+                Loggers.CONNECTION.info("Out dated connection check successCount={}", successConnections.size());
+                
+                for (String outDateConnectionId : outDatedConnections) {
+                    if (!successConnections.contains(outDateConnectionId)) {
+                        Loggers.CONNECTION.info("[{}]Unregister Out dated connection....", outDateConnectionId);
+                        connectionManager.unregister(outDateConnectionId);
+                    }
+                }
+            }
             
             Loggers.CONNECTION.info("Connection check task end");
             
