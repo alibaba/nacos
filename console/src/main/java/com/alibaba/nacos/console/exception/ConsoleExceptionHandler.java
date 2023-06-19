c990dba9ec2b0da96436474ce02d1620749141f4
DarrenLuo <jn296@qq.com>
2023-06-19 10:52:31 +0800

    根据 nacos code style 重新格式化该文件

--- console/src/main/java/com/alibaba/nacos/console/exception/ConsoleExceptionHandler.java
+++ console/src/main/java/com/alibaba/nacos/console/exception/ConsoleExceptionHandler.java
@@ -38,28 +38,27 @@ import javax.servlet.http.HttpServletRequest;
  */
 @ControllerAdvice
 public class ConsoleExceptionHandler {
-
+    
     private static final Logger LOGGER = LoggerFactory.getLogger(ConsoleExceptionHandler.class);
-
+    
     @ExceptionHandler(AccessException.class)
     private ResponseEntity<String> handleAccessException(AccessException e) {
         LOGGER.error("got exception. {}", e.getErrMsg());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getErrMsg());
     }
-
+    
     @ExceptionHandler(IllegalArgumentException.class)
     private ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ExceptionUtil.getAllExceptionMsg(e));
     }
-
+    
     @ExceptionHandler(Exception.class)
     private ResponseEntity<Object> handleException(HttpServletRequest request, Exception e) {
         String uri = request.getRequestURI();
         LOGGER.error("CONSOLE {}", uri, e);
         if (uri.contains(Commons.NACOS_SERVER_VERSION_V2)) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(RestResultUtils.failed(
-                            HtmlUtils.htmlEscape(ExceptionUtil.getAllExceptionMsg(e), "utf-8")));
+                    .body(RestResultUtils.failed(HtmlUtils.htmlEscape(ExceptionUtil.getAllExceptionMsg(e), "utf-8")));
         }
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(HtmlUtils.htmlEscape(ExceptionUtil.getAllExceptionMsg(e), "utf-8"));
