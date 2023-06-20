3aa3231254a7034e96f2ed9f1854f8252f562aa0
shiyiyue1102 <liuzunfei@gmail.com>
2023-06-20 11:05:32 +0800

    checkstyle

--- core/src/main/java/com/alibaba/nacos/core/utils/StringPool.java
+++ core/src/main/java/com/alibaba/nacos/core/utils/StringPool.java
@@ -33,7 +33,6 @@ public class StringPool {
     private static Cache<String, String> groupKeyCache = CacheBuilder.newBuilder().maximumSize(5000000)
             .expireAfterAccess(180, TimeUnit.SECONDS).build();
     
-    
     /**
      * get singleton string value from the pool.
      *
