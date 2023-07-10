771e2dd0b5be0edb90535431f660c5bf66488d5d
zhuoguang <1336317033@qq.com>
2023-07-10 15:48:04 +0800

    For #10734,add the naming http request param check filter and implement the naming http request param extractors

---
+++ naming/src/main/java/com/alibaba/nacos/naming/paramcheck/NamingInstanceMetadataBatchHttpParamExtractor.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 1999-2023 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.naming.paramcheck;
+
+import com.alibaba.nacos.api.common.Constants;
+import com.alibaba.nacos.api.naming.pojo.Instance;
+import com.alibaba.nacos.common.paramcheck.ParamCheckUtils;
+import com.alibaba.nacos.common.paramcheck.ParamInfo;
+import com.alibaba.nacos.common.utils.HttpMethod;
+import com.alibaba.nacos.common.utils.JacksonUtils;
+import com.alibaba.nacos.common.utils.StringUtils;
+import com.alibaba.nacos.core.paramcheck.AbstractHttpParamExtractor;
+import com.alibaba.nacos.naming.misc.UtilsAndCommons;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * Naming instance metadata batch http param extractor.
+ *
+ * @author zhuoguang
+ */
+public class NamingInstanceMetadataBatchHttpParamExtractor extends AbstractHttpParamExtractor {
+    
+    @Override
+    public void init() {
+        addTargetRequest(UtilsAndCommons.NACOS_NAMING_CONTEXT + UtilsAndCommons.NACOS_NAMING_INSTANCE_CONTEXT + "/metadata/batch",
+                HttpMethod.PUT);
+        addTargetRequest(UtilsAndCommons.NACOS_NAMING_CONTEXT + UtilsAndCommons.NACOS_NAMING_INSTANCE_CONTEXT + "/metadata/batch",
+                HttpMethod.DELETE);
+        addTargetRequest(UtilsAndCommons.DEFAULT_NACOS_NAMING_CONTEXT_V2 + UtilsAndCommons.NACOS_NAMING_INSTANCE_CONTEXT + "/metadata/batch",
+                HttpMethod.PUT);
+        addTargetRequest(UtilsAndCommons.DEFAULT_NACOS_NAMING_CONTEXT_V2 + UtilsAndCommons.NACOS_NAMING_INSTANCE_CONTEXT + "/metadata/batch",
+                HttpMethod.DELETE);
+    }
+    
+    @Override
+    public void extractParamAndCheck(HttpServletRequest request) throws Exception {
+        ParamInfo paramInfo = new ParamInfo();
+        String serviceName = request.getParameter("serviceName");
+        String groupName = request.getParameter("groupName");
+        String groupServiceName = serviceName;
+        if (StringUtils.isNotBlank(groupServiceName) && groupServiceName.contains(Constants.SERVICE_INFO_SPLITER)) {
+            String[] splits = groupServiceName.split(Constants.SERVICE_INFO_SPLITER, 2);
+            groupName = splits[0];
+            serviceName = splits[1];
+        }
+        paramInfo.setServiceName(serviceName);
+        paramInfo.setGroup(groupName);
+        paramInfo.setNamespaceId(request.getParameter("namespaceId"));
+        paramInfo.setMetadata(UtilsAndCommons.parseMetadata(request.getParameter("metadata")));
+        ParamCheckUtils.checkParamInfoFormat(paramInfo);
+        
+        String instances = request.getParameter("instances");
+        if (StringUtils.isNotBlank(instances)) {
+            List<Instance> targetInstances = JacksonUtils.toObj(instances, new TypeReference<List<Instance>>() {
+            });
+            for (Instance instance : targetInstances) {
+                ParamInfo instanceParamInfo = new ParamInfo();
+                instanceParamInfo.setIp(instance.getIp());
+                instanceParamInfo.setPort(String.valueOf(instance.getPort()));
+                instanceParamInfo.setCluster(instance.getClusterName());
+                ParamCheckUtils.checkParamInfoFormat(instanceParamInfo);
+            }
+        }
+    }
+}
