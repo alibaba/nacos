96faa48fb4476acfaa278518c3ca390a73e8e8b6
Dale Lee <dale_mail@163.com>
2023-08-02 20:06:50 +0800

    Rename NamingChangeEventTest

--- client/src/test/java/com/alibaba/nacos/client/naming/listener/NamingChangeEventTest.java
+++ client/src/test/java/com/alibaba/nacos/client/naming/listener/NamingChangeEventTest.java
@@ -29,7 +29,7 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
-public class NamingChaneEventTest {
+public class NamingChangeEventTest {
 
     private MockNamingEventListener eventListener;
 
