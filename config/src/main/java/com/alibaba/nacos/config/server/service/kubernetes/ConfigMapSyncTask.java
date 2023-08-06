325dc75ff0e04df22fb29eae0500367631d0336c
peter <peterwangyixing@gmail.com>
2023-08-06 17:25:37 +0800

    update KubernetesConfigMapSyncService add ConfigMapSyncTask

---
+++ config/src/main/java/com/alibaba/nacos/config/server/service/kubernetes/ConfigMapSyncTask.java
@@ -0,0 +1,77 @@
+/*
+ *
+ * Copyright 1999-2023 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.alibaba.nacos.config.server.service.kubernetes;
+
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1ConfigMap;
+import io.kubernetes.client.openapi.models.V1ConfigMapList;
+import io.kubernetes.client.openapi.models.V1ObjectMeta;
+import io.kubernetes.client.util.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class ConfigMapSyncTask {
+    
+    private final CoreV1Api coreV1Api;
+    
+    private final ApiClient apiClient;
+    
+    private final ConfigService nacosConfigService;
+    
+    
+    public ConfigMapSyncTask() throws IOException, NacosException {
+        this.apiClient = Config.defaultClient();
+        Configuration.setDefaultApiClient(apiClient);
+        coreV1Api = new CoreV1Api();
+        
+        nacosConfigService = NacosFactory.createConfigService("TODO");
+    }
+    
+    // 每小时从k8s中list所有的configmaps, 如果nacos中没有则添加
+    @Scheduled(fixedDelay = 3600000) // 每小时对帐
+    public void checkAndSyncConfigMaps() {
+        try {
+            V1ConfigMapList configMapList = (V1ConfigMapList) coreV1Api.listConfigMapForAllNamespacesCall(null, null,
+                    null, null, null, null, null, null, null, null, null);
+            for (V1ConfigMap configMap : configMapList.getItems()) {
+                String dataId = configMap.getMetadata().getName();
+                String group = "K8S_GROUP";
+                String content = configMap.getData().toString();
+                
+                boolean existInNacos = nacosConfigService.publishConfig(dataId, group, content);
+                if (!existInNacos) {
+                    Loggers.MAIN.info("find config missed");
+                    coreV1Api.createNamespacedConfigMap("your-namespace", configMap, null, null, null);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
