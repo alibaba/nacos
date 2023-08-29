b82af66dfb3fe2c0ae63765170d3b9127384beed
Kevin Ling <Kevin_lgh@outlook.com>
2023-08-29 12:11:57 +0800

    Add trace unit test

--- client/src/main/java/com/alibaba/nacos/client/monitor/TraceMonitor.java
+++ client/src/main/java/com/alibaba/nacos/client/monitor/TraceMonitor.java
@@ -3,16 +3,17 @@ package com.alibaba.nacos.client.monitor;
 import com.alibaba.nacos.common.utils.HttpMethod;
 import com.alibaba.nacos.common.utils.VersionUtils;
 import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
 
 public class TraceMonitor {
     
-    private static final Tracer TRACER = GlobalOpenTelemetry.getTracer("Nacos-Java-Client", VersionUtils.version);
+    private static Tracer tracer = GlobalOpenTelemetry.getTracer("Nacos-Java-Client", VersionUtils.version);
     
     public static Span getClientConfigRpcSpan() {
-        return TRACER.spanBuilder("nacos.client.config.rpc").setSpanKind(SpanKind.CLIENT)
+        return tracer.spanBuilder("nacos.client.config.rpc").setSpanKind(SpanKind.CLIENT)
                 .setAttribute("nacos.client.version", VersionUtils.getFullClientVersion()).startSpan();
     }
     
@@ -26,16 +27,16 @@ public class TraceMonitor {
     public static Span getClientConfigHttpSpan(String method) throws IllegalArgumentException {
         switch (method) {
             case HttpMethod.GET:
-                return TRACER.spanBuilder("nacos.client.config.http.get").setSpanKind(SpanKind.CLIENT)
+                return tracer.spanBuilder("nacos.client.config.http.get").setSpanKind(SpanKind.CLIENT)
                         .setAttribute("nacos.client.version", VersionUtils.getFullClientVersion()).startSpan();
             case HttpMethod.POST:
-                return TRACER.spanBuilder("nacos.client.config.http.post").setSpanKind(SpanKind.CLIENT)
+                return tracer.spanBuilder("nacos.client.config.http.post").setSpanKind(SpanKind.CLIENT)
                         .setAttribute("nacos.client.version", VersionUtils.getFullClientVersion()).startSpan();
             case HttpMethod.DELETE:
-                return TRACER.spanBuilder("nacos.client.config.http.delete").setSpanKind(SpanKind.CLIENT)
+                return tracer.spanBuilder("nacos.client.config.http.delete").setSpanKind(SpanKind.CLIENT)
                         .setAttribute("nacos.client.version", VersionUtils.getFullClientVersion()).startSpan();
             case HttpMethod.PUT:
-                return TRACER.spanBuilder("nacos.client.config.http.put").setSpanKind(SpanKind.CLIENT)
+                return tracer.spanBuilder("nacos.client.config.http.put").setSpanKind(SpanKind.CLIENT)
                         .setAttribute("nacos.client.version", VersionUtils.getFullClientVersion()).startSpan();
             default:
                 throw new IllegalArgumentException("Unsupported http method: " + method);
@@ -43,7 +44,7 @@ public class TraceMonitor {
     }
     
     public static Span getClientNamingRpcSpan() {
-        return TRACER.spanBuilder("nacos.client.naming.rpc").setSpanKind(SpanKind.CLIENT)
+        return tracer.spanBuilder("nacos.client.naming.rpc").setSpanKind(SpanKind.CLIENT)
                 .setAttribute("nacos.client.version", VersionUtils.getFullClientVersion()).startSpan();
     }
     
@@ -57,16 +58,16 @@ public class TraceMonitor {
     public static Span getClientNamingHttpSpan(String method) throws IllegalArgumentException {
         switch (method) {
             case HttpMethod.GET:
-                return TRACER.spanBuilder("nacos.client.naming.http.get").setSpanKind(SpanKind.CLIENT)
+                return tracer.spanBuilder("nacos.client.naming.http.get").setSpanKind(SpanKind.CLIENT)
                         .setAttribute("nacos.client.version", VersionUtils.getFullClientVersion()).startSpan();
             case HttpMethod.POST:
-                return TRACER.spanBuilder("nacos.client.naming.http.post").setSpanKind(SpanKind.CLIENT)
+                return tracer.spanBuilder("nacos.client.naming.http.post").setSpanKind(SpanKind.CLIENT)
                         .setAttribute("nacos.client.version", VersionUtils.getFullClientVersion()).startSpan();
             case HttpMethod.DELETE:
-                return TRACER.spanBuilder("nacos.client.naming.http.delete").setSpanKind(SpanKind.CLIENT)
+                return tracer.spanBuilder("nacos.client.naming.http.delete").setSpanKind(SpanKind.CLIENT)
                         .setAttribute("nacos.client.version", VersionUtils.getFullClientVersion()).startSpan();
             case HttpMethod.PUT:
-                return TRACER.spanBuilder("nacos.client.naming.http.put").setSpanKind(SpanKind.CLIENT)
+                return tracer.spanBuilder("nacos.client.naming.http.put").setSpanKind(SpanKind.CLIENT)
                         .setAttribute("nacos.client.version", VersionUtils.getFullClientVersion()).startSpan();
             default:
                 throw new IllegalArgumentException("Unsupported http method: " + method);
@@ -79,6 +80,20 @@ public class TraceMonitor {
      * @return the OpenTelemetry tracer
      */
     public static Tracer getTracer() {
-        return TRACER;
+        return tracer;
+    }
+    
+    /**
+     * Set the Nacos client OpenTelemetry tracer.
+     *
+     * @param openTelemetry the OpenTelemetry instance
+     */
+    public static Tracer setTracer(OpenTelemetry openTelemetry) {
+        return setTracer(openTelemetry, "Nacos-Java-Client", VersionUtils.version);
+    }
+    
+    public static Tracer setTracer(OpenTelemetry openTelemetry, String name, String version) {
+        tracer = openTelemetry.getTracer(name, version);
+        return tracer;
     }
 }
---
+++ client/src/test/java/com/alibaba/nacos/client/monitor/TraceMonitorTest.java
@@ -0,0 +1,143 @@
+package com.alibaba.nacos.client.monitor;
+
+import com.alibaba.nacos.common.utils.VersionUtils;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.common.CompletableResultCode;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.data.SpanData;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class TraceMonitorTest {
+    
+    public static Tracer testTracer = null;
+    
+    public static OpenTelemetry testOpenTelemetry = null;
+    
+    public static TestExporter testExporter = null;
+    
+    @BeforeClass
+    public static void init() {
+        testExporter = new TestExporter();
+        testOpenTelemetry = OpenTelemetrySdk.builder().setTracerProvider(
+                SdkTracerProvider.builder().addSpanProcessor(SimpleSpanProcessor.create(testExporter)).build()).build();
+        testTracer = TraceMonitor.setTracer(testOpenTelemetry);
+    }
+    
+    @After
+    public void clear() {
+        testExporter.exportedSpans.clear();
+    }
+    
+    @Test
+    public void testGetClientConfigRpcSpan() {
+        Span span = TraceMonitor.getClientConfigRpcSpan();
+        AttributeKey<String> testKey = AttributeKey.stringKey("test.key");
+        AttributeKey<String> versionKey = AttributeKey.stringKey("nacos.client.version");
+        runSpan(span);
+        
+        for (SpanData spanData : testExporter.exportedSpans) {
+            Attributes attributes = spanData.getAttributes();
+            
+            Assert.assertEquals(attributes.get(testKey), "test.value");
+            Assert.assertEquals(attributes.get(versionKey), VersionUtils.getFullClientVersion());
+            Assert.assertEquals(spanData.getName(), "nacos.client.config.rpc");
+        }
+    }
+    
+    @Test
+    public void testGetClientConfigHttpSpan() {
+        Span span = TraceMonitor.getClientConfigHttpSpan("GET");
+        AttributeKey<String> testKey = AttributeKey.stringKey("test.key");
+        AttributeKey<String> versionKey = AttributeKey.stringKey("nacos.client.version");
+        runSpan(span);
+        
+        for (SpanData spanData : testExporter.exportedSpans) {
+            Attributes attributes = spanData.getAttributes();
+            
+            Assert.assertEquals(attributes.get(testKey), "test.value");
+            Assert.assertEquals(attributes.get(versionKey), VersionUtils.getFullClientVersion());
+            Assert.assertEquals(spanData.getName(), "nacos.client.config.http.get");
+        }
+    }
+    
+    @Test
+    public void testGetClientNamingRpcSpan() {
+        Span span = TraceMonitor.getClientNamingRpcSpan();
+        AttributeKey<String> testKey = AttributeKey.stringKey("test.key");
+        AttributeKey<String> versionKey = AttributeKey.stringKey("nacos.client.version");
+        runSpan(span);
+        
+        for (SpanData spanData : testExporter.exportedSpans) {
+            Attributes attributes = spanData.getAttributes();
+            
+            Assert.assertEquals(attributes.get(testKey), "test.value");
+            Assert.assertEquals(attributes.get(versionKey), VersionUtils.getFullClientVersion());
+            Assert.assertEquals(spanData.getName(), "nacos.client.naming.rpc");
+        }
+    }
+    
+    @Test
+    public void testGetClientNamingHttpSpan() {
+        Span span = TraceMonitor.getClientNamingHttpSpan("GET");
+        AttributeKey<String> testKey = AttributeKey.stringKey("test.key");
+        AttributeKey<String> versionKey = AttributeKey.stringKey("nacos.client.version");
+        runSpan(span);
+        
+        for (SpanData spanData : testExporter.exportedSpans) {
+            Attributes attributes = spanData.getAttributes();
+            
+            Assert.assertEquals(attributes.get(testKey), "test.value");
+            Assert.assertEquals(attributes.get(versionKey), VersionUtils.getFullClientVersion());
+            Assert.assertEquals(spanData.getName(), "nacos.client.naming.http.get");
+        }
+    }
+    
+    private void runSpan(Span span) {
+        try (Scope ignored = span.makeCurrent()) {
+            span.setStatus(StatusCode.OK);
+            span.setAttribute("test.key", "test.value");
+        } finally {
+            span.end();
+        }
+    }
+    
+    public static class TestExporter implements SpanExporter {
+        
+        public final List<SpanData> exportedSpans = Collections.synchronizedList(new ArrayList<>());
+        
+        @ParametersAreNonnullByDefault
+        @Override
+        public CompletableResultCode export(Collection<SpanData> collection) {
+            exportedSpans.addAll(collection);
+            return CompletableResultCode.ofSuccess();
+        }
+        
+        @Override
+        public CompletableResultCode flush() {
+            return CompletableResultCode.ofSuccess();
+        }
+        
+        @Override
+        public CompletableResultCode shutdown() {
+            return CompletableResultCode.ofSuccess();
+        }
+    }
+}
