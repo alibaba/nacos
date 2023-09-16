fbca9ce5e922e7c66e40dadd9dceadac20117bfc
985492783 <985492783@qq.com>
2023-09-17 02:57:21 +0800

    fix lock auth.

--- plugin-default-impl/nacos-default-auth-plugin/src/main/java/com/alibaba/nacos/plugin/auth/impl/roles/NacosRoleServiceImpl.java
+++ plugin-default-impl/nacos-default-auth-plugin/src/main/java/com/alibaba/nacos/plugin/auth/impl/roles/NacosRoleServiceImpl.java
@@ -23,6 +23,7 @@ import com.alibaba.nacos.common.utils.ConcurrentHashSet;
 import com.alibaba.nacos.common.utils.StringUtils;
 import com.alibaba.nacos.persistence.model.Page;
 import com.alibaba.nacos.core.utils.Loggers;
+import com.alibaba.nacos.persistence.model.Page;
 import com.alibaba.nacos.plugin.auth.api.Permission;
 import com.alibaba.nacos.plugin.auth.api.Resource;
 import com.alibaba.nacos.plugin.auth.constant.Constants;
@@ -60,6 +61,8 @@ public class NacosRoleServiceImpl {
     
     private static final int DEFAULT_PAGE_NO = 1;
     
+    private static final Set<String> WHITE_PERMISSION = new HashSet<>();
+    
     @Autowired
     private AuthConfigs authConfigs;
     
@@ -77,6 +80,11 @@ public class NacosRoleServiceImpl {
     private volatile Map<String, List<RoleInfo>> roleInfoMap = new ConcurrentHashMap<>();
     
     private volatile Map<String, List<PermissionInfo>> permissionInfoMap = new ConcurrentHashMap<>();
+    
+    static {
+        WHITE_PERMISSION.add(AuthConstants.UPDATE_PASSWORD_ENTRY_POINT);
+        WHITE_PERMISSION.add(AuthConstants.LOCK_OPERATOR_POINT);
+    }
     
     @Scheduled(initialDelay = 5000, fixedDelay = 15000)
     private void reload() {
@@ -122,6 +130,11 @@ public class NacosRoleServiceImpl {
      * @return true if granted, false otherwise
      */
     public boolean hasPermission(NacosUser nacosUser, Permission permission) {
+        //white permission
+        if (WHITE_PERMISSION.contains(permission.getResource().getName())) {
+            return true;
+        }
+        
         if (isUpdatePasswordPermission(permission)) {
             return true;
         }
