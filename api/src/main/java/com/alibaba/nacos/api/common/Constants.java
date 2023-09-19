8b83aa11c0e540ff1737d3c686806f2e42cbed25
Chionanthus <33491015+Chionanthus@users.noreply.github.com>
2023-09-20 00:54:52 +0800

    fix word spell

--- api/src/main/java/com/alibaba/nacos/api/common/Constants.java
+++ api/src/main/java/com/alibaba/nacos/api/common/Constants.java
@@ -241,7 +241,7 @@ public class Constants {
     }
     
     /**
-     * The constants in Watch Pattern Match Rule directory.
+     * The constants in config directory.
      */
     public static class WatchMatchRule {
         
@@ -251,9 +251,6 @@ public class Constants {
         
     }
     
-    /**
-     * The constants in Watch Notify Event directory.
-     */
     public static class WatchEventType {
         
         public static final String ADD_SERVICE = "ADD_SERVICE";
