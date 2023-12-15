c8eb1399be126a93489ebfdeeb8f8e7ae26efc7a
shiyiyue1102 <liuzunfei@gmail.com>
2023-12-15 15:35:44 +0800

    修复测试用例

--- config/src/test/java/com/alibaba/nacos/config/server/service/dump/DumpProcessorTest.java
+++ config/src/test/java/com/alibaba/nacos/config/server/service/dump/DumpProcessorTest.java
@@ -8,24 +8,15 @@ import com.alibaba.nacos.config.server.model.ConfigInfoWrapper;
 import com.alibaba.nacos.config.server.service.ConfigCacheService;
 import com.alibaba.nacos.config.server.service.dump.disk.ConfigDiskService;
 import com.alibaba.nacos.config.server.service.dump.disk.ConfigDiskServiceFactory;
-import com.alibaba.nacos.config.server.service.dump.disk.ConfigRawDiskService;
 import com.alibaba.nacos.config.server.service.dump.disk.ConfigRocksDbDiskService;
 import com.alibaba.nacos.config.server.service.dump.processor.DumpProcessor;
 import com.alibaba.nacos.config.server.service.dump.task.DumpTask;
 import com.alibaba.nacos.config.server.service.repository.ConfigInfoBetaPersistService;
 import com.alibaba.nacos.config.server.service.repository.ConfigInfoPersistService;
 import com.alibaba.nacos.config.server.service.repository.ConfigInfoTagPersistService;
-import com.alibaba.nacos.config.server.service.repository.HistoryConfigInfoPersistService;
-import com.alibaba.nacos.config.server.service.repository.extrnal.ExternalConfigInfoBetaPersistServiceImpl;
-import com.alibaba.nacos.config.server.service.repository.extrnal.ExternalConfigInfoPersistServiceImpl;
-import com.alibaba.nacos.config.server.service.repository.extrnal.ExternalConfigInfoTagPersistServiceImpl;
-import com.alibaba.nacos.config.server.service.repository.extrnal.ExternalHistoryConfigInfoPersistServiceImpl;
 import com.alibaba.nacos.config.server.utils.GroupKey2;
-import com.alibaba.nacos.persistence.datasource.DataSourceService;
-import com.alibaba.nacos.persistence.datasource.DynamicDataSource;
 import com.alibaba.nacos.plugin.datasource.constants.CommonConstant;
 import com.alibaba.nacos.sys.env.EnvUtil;
-import org.checkerframework.checker.units.qual.C;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -35,17 +26,11 @@ import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 
-import static com.alibaba.nacos.config.server.service.repository.ConfigRowMapperInjector.CONFIG_INFO_BETA_WRAPPER_ROW_MAPPER;
-import static com.alibaba.nacos.config.server.service.repository.ConfigRowMapperInjector.CONFIG_INFO_TAG_WRAPPER_ROW_MAPPER;
-import static com.alibaba.nacos.config.server.service.repository.ConfigRowMapperInjector.CONFIG_INFO_WRAPPER_ROW_MAPPER;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
@@ -53,55 +38,29 @@ import static org.mockito.Mockito.when;
 public class DumpProcessorTest {
     
     @Mock
-    DynamicDataSource dynamicDataSource;
-    
-    @Mock
-    DataSourceService dataSourceService;
-    
-    @Mock
-    JdbcTemplate jdbcTemplate;
-    
-    HistoryConfigInfoPersistService historyConfigInfoPersistService;
-    
     ConfigInfoPersistService configInfoPersistService;
     
+    @Mock
     ConfigInfoBetaPersistService configInfoBetaPersistService;
     
+    @Mock
     ConfigInfoTagPersistService configInfoTagPersistService;
     
     ExternalDumpService dumpService;
     
     DumpProcessor dumpProcessor;
     
-    MockedStatic<DynamicDataSource> dynamicDataSourceMockedStatic;
-    
     MockedStatic<EnvUtil> envUtilMockedStatic;
     
     @Before
     public void init() throws Exception {
-        dynamicDataSourceMockedStatic = Mockito.mockStatic(DynamicDataSource.class);
         envUtilMockedStatic = Mockito.mockStatic(EnvUtil.class);
-        
+        when(EnvUtil.getNacosHome()).thenReturn(System.getProperty("user.home"));
         when(EnvUtil.getProperty(eq(CommonConstant.NACOS_PLUGIN_DATASOURCE_LOG), eq(Boolean.class),
                 eq(false))).thenReturn(false);
-        dynamicDataSourceMockedStatic.when(DynamicDataSource::getInstance).thenReturn(dynamicDataSource);
-        
-        when(dynamicDataSource.getDataSource()).thenReturn(dataSourceService);
-        when(dataSourceService.getDataSourceType()).thenReturn("mysql");
-        
-        historyConfigInfoPersistService = new ExternalHistoryConfigInfoPersistServiceImpl();
-        configInfoPersistService = new ExternalConfigInfoPersistServiceImpl(historyConfigInfoPersistService);
-        configInfoBetaPersistService = new ExternalConfigInfoBetaPersistServiceImpl();
-        configInfoTagPersistService = new ExternalConfigInfoTagPersistServiceImpl();
-        ReflectionTestUtils.setField(historyConfigInfoPersistService, "jt", jdbcTemplate);
-        ReflectionTestUtils.setField(configInfoPersistService, "jt", jdbcTemplate);
-        ReflectionTestUtils.setField(configInfoBetaPersistService, "jt", jdbcTemplate);
-        ReflectionTestUtils.setField(configInfoTagPersistService, "jt", jdbcTemplate);
-        
         dumpService = new ExternalDumpService(configInfoPersistService, null, null, null, configInfoBetaPersistService,
                 configInfoTagPersistService, null);
         dumpProcessor = new DumpProcessor(dumpService);
-        ReflectionTestUtils.setField(historyConfigInfoPersistService, "jt", jdbcTemplate);
         Field[] declaredFields = ConfigDiskServiceFactory.class.getDeclaredFields();
         for (Field filed : declaredFields) {
             if (filed.getName().equals("configDiskService")) {
@@ -109,16 +68,21 @@ public class DumpProcessorTest {
                 filed.set(null, createDiskService());
             }
         }
+        
     }
     
-    protected ConfigDiskService createDiskService(){
+    protected ConfigDiskService createDiskService() {
         return new ConfigRocksDbDiskService();
     }
     
     @After
     public void after() {
-        dynamicDataSourceMockedStatic.close();
         envUtilMockedStatic.close();
+        ConfigDiskServiceFactory.getInstance().clearAll();
+        ConfigDiskServiceFactory.getInstance().clearAllBatch();
+        ConfigDiskServiceFactory.getInstance().clearAllBeta();
+        ConfigDiskServiceFactory.getInstance().clearAllTag();
+    
     }
     
     @Test
@@ -134,8 +98,9 @@ public class DumpProcessorTest {
         configInfoWrapper.setTenant(tenant);
         configInfoWrapper.setContent(content);
         configInfoWrapper.setLastModified(time);
-        when(jdbcTemplate.queryForObject(anyString(), eq(new Object[] {dataId, group, tenant}),
-                eq(CONFIG_INFO_WRAPPER_ROW_MAPPER))).thenReturn(configInfoWrapper);
+        
+        Mockito.when(configInfoPersistService.findConfigInfo(eq(dataId), eq(group), eq(tenant)))
+                .thenReturn(configInfoWrapper);
         
         String handlerIp = "127.0.0.1";
         long lastModified = System.currentTimeMillis();
@@ -153,8 +118,8 @@ public class DumpProcessorTest {
         Assert.assertEquals(content, contentFromDisk);
         
         // remove
-        when(jdbcTemplate.queryForObject(anyString(), eq(new Object[] {dataId, group, tenant}),
-                eq(CONFIG_INFO_WRAPPER_ROW_MAPPER))).thenReturn(null);
+        Mockito.when(configInfoPersistService.findConfigInfo(eq(dataId), eq(group), eq(tenant))).thenReturn(null);
+        
         boolean processRemove = dumpProcessor.process(dumpTask);
         Assert.assertTrue(processRemove);
         
@@ -182,8 +147,9 @@ public class DumpProcessorTest {
         configInfoWrapper.setLastModified(time);
         String betaIps = "127.0.0.1123,127.0.0.11";
         configInfoWrapper.setBetaIps(betaIps);
-        when(jdbcTemplate.queryForObject(anyString(), eq(new Object[] {dataId, group, tenant}),
-                eq(CONFIG_INFO_BETA_WRAPPER_ROW_MAPPER))).thenReturn(configInfoWrapper);
+        
+        Mockito.when(configInfoBetaPersistService.findConfigInfo4Beta(eq(dataId), eq(group), eq(tenant)))
+                .thenReturn(configInfoWrapper);
         
         String handlerIp = "127.0.0.1";
         long lastModified = System.currentTimeMillis();
@@ -202,8 +168,8 @@ public class DumpProcessorTest {
         Assert.assertEquals(content, contentFromDisk);
         
         // remove
-        when(jdbcTemplate.queryForObject(anyString(), eq(new Object[] {dataId, group, tenant}),
-                eq(CONFIG_INFO_BETA_WRAPPER_ROW_MAPPER))).thenReturn(null);
+        Mockito.when(configInfoBetaPersistService.findConfigInfo4Beta(eq(dataId), eq(group), eq(tenant)))
+                .thenReturn(null);
         boolean processRemove = dumpProcessor.process(dumpTask);
         Assert.assertTrue(processRemove);
         
@@ -232,8 +198,8 @@ public class DumpProcessorTest {
         configInfoWrapper.setContent(content);
         configInfoWrapper.setLastModified(time);
         configInfoWrapper.setTag(tag);
-        when(jdbcTemplate.queryForObject(anyString(), eq(new Object[] {dataId, group, tenant,tag}),
-                eq(CONFIG_INFO_TAG_WRAPPER_ROW_MAPPER))).thenReturn(configInfoWrapper);
+        Mockito.when(configInfoTagPersistService.findConfigInfo4Tag(eq(dataId), eq(group), eq(tenant), eq(tag)))
+                .thenReturn(configInfoWrapper);
         
         String handlerIp = "127.0.0.1";
         long lastModified = System.currentTimeMillis();
@@ -251,8 +217,8 @@ public class DumpProcessorTest {
         Assert.assertEquals(content, contentFromDisk);
         
         // remove
-        when(jdbcTemplate.queryForObject(anyString(), eq(new Object[] {dataId, group, tenant,tag}),
-                eq(CONFIG_INFO_TAG_WRAPPER_ROW_MAPPER))).thenReturn(null);
+        Mockito.when(configInfoTagPersistService.findConfigInfo4Tag(eq(dataId), eq(group), eq(tenant), eq(tag)))
+                .thenReturn(null);
         boolean processRemove = dumpProcessor.process(dumpTask);
         Assert.assertTrue(processRemove);
         
@@ -262,7 +228,7 @@ public class DumpProcessorTest {
                 || contentCache.getConfigCacheTags().get(tag) == null);
         //check disk
         String contentFromDiskAfterRemove = ConfigDiskServiceFactory.getInstance()
-                .getTagContent(dataId, group, tenant,tag);
+                .getTagContent(dataId, group, tenant, tag);
         Assert.assertNull(contentFromDiskAfterRemove);
         
     }
--- config/src/test/java/com/alibaba/nacos/config/server/service/dump/DumpProcessorUserRwaDiskTest.java
+++ config/src/test/java/com/alibaba/nacos/config/server/service/dump/DumpProcessorUserRwaDiskTest.java
@@ -1,51 +1,16 @@
 package com.alibaba.nacos.config.server.service.dump;
 
-import com.alibaba.nacos.common.utils.MD5Utils;
-import com.alibaba.nacos.config.server.model.CacheItem;
-import com.alibaba.nacos.config.server.model.ConfigInfoBetaWrapper;
-import com.alibaba.nacos.config.server.model.ConfigInfoTagWrapper;
-import com.alibaba.nacos.config.server.model.ConfigInfoWrapper;
-import com.alibaba.nacos.config.server.service.ConfigCacheService;
 import com.alibaba.nacos.config.server.service.dump.disk.ConfigDiskService;
-import com.alibaba.nacos.config.server.service.dump.disk.ConfigDiskServiceFactory;
 import com.alibaba.nacos.config.server.service.dump.disk.ConfigRawDiskService;
-import com.alibaba.nacos.config.server.service.dump.disk.ConfigRocksDbDiskService;
-import com.alibaba.nacos.config.server.service.dump.processor.DumpProcessor;
-import com.alibaba.nacos.config.server.service.dump.task.DumpTask;
-import com.alibaba.nacos.config.server.service.repository.ConfigInfoBetaPersistService;
-import com.alibaba.nacos.config.server.service.repository.ConfigInfoPersistService;
-import com.alibaba.nacos.config.server.service.repository.ConfigInfoTagPersistService;
-import com.alibaba.nacos.config.server.service.repository.HistoryConfigInfoPersistService;
-import com.alibaba.nacos.config.server.service.repository.extrnal.ExternalConfigInfoBetaPersistServiceImpl;
-import com.alibaba.nacos.config.server.service.repository.extrnal.ExternalConfigInfoPersistServiceImpl;
-import com.alibaba.nacos.config.server.service.repository.extrnal.ExternalConfigInfoTagPersistServiceImpl;
-import com.alibaba.nacos.config.server.service.repository.extrnal.ExternalHistoryConfigInfoPersistServiceImpl;
-import com.alibaba.nacos.config.server.utils.GroupKey2;
-import com.alibaba.nacos.persistence.datasource.DataSourceService;
-import com.alibaba.nacos.persistence.datasource.DynamicDataSource;
-import com.alibaba.nacos.plugin.datasource.constants.CommonConstant;
 import com.alibaba.nacos.sys.env.EnvUtil;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.Arrays;
 
-import static com.alibaba.nacos.config.server.service.repository.ConfigRowMapperInjector.CONFIG_INFO_BETA_WRAPPER_ROW_MAPPER;
-import static com.alibaba.nacos.config.server.service.repository.ConfigRowMapperInjector.CONFIG_INFO_TAG_WRAPPER_ROW_MAPPER;
-import static com.alibaba.nacos.config.server.service.repository.ConfigRowMapperInjector.CONFIG_INFO_WRAPPER_ROW_MAPPER;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -54,7 +19,6 @@ public class DumpProcessorUserRwaDiskTest extends DumpProcessorTest{
     @Before
     public void init() throws Exception {
         super.init();
-        when(EnvUtil.getNacosHome()).thenReturn("./");
     }
     
     @Override
@@ -65,7 +29,6 @@ public class DumpProcessorUserRwaDiskTest extends DumpProcessorTest{
     @After
     public void after() {
         super.after();
-        ConfigDiskServiceFactory.getInstance().clearAll();
     }
     
     @Test
