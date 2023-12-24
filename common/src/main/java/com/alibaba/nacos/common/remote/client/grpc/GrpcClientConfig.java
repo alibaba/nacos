2498821ed663e62b076270de628bae8319f45391
stone-98 <670569467@qq.com>
2023-12-24 18:14:03 +0800

    [ISSUE #11456]Indentation handling

--- common/src/main/java/com/alibaba/nacos/common/remote/client/grpc/GrpcClientConfig.java
+++ common/src/main/java/com/alibaba/nacos/common/remote/client/grpc/GrpcClientConfig.java
@@ -26,63 +26,63 @@ import com.alibaba.nacos.common.remote.client.RpcClientTlsConfig;
  * @author karsonto
  */
 public interface GrpcClientConfig extends RpcClientConfig {
-    
+
     /**
      * get threadPoolCoreSize.
      *
      * @return threadPoolCoreSize.
      */
     int threadPoolCoreSize();
-    
+
     /**
      * get threadPoolMaxSize.
      *
      * @return threadPoolMaxSize.
      */
     int threadPoolMaxSize();
-    
+
     /**
      * get thread pool keep alive time.
      *
      * @return threadPoolKeepAlive.
      */
     long threadPoolKeepAlive();
-    
+
     /**
      * get server check time out.
      *
      * @return serverCheckTimeOut.
      */
     long serverCheckTimeOut();
-    
+
     /**
      * get thread pool queue size.
      *
      * @return threadPoolQueueSize.
      */
     int threadPoolQueueSize();
-    
+
     /**
      * get maxInboundMessage size.
      *
      * @return maxInboundMessageSize.
      */
     int maxInboundMessageSize();
-    
+
     /**
      * get channelKeepAlive time.
      *
      * @return channelKeepAlive.
      */
     int channelKeepAlive();
-    
+
     /**
      * get channelKeepAliveTimeout.
      *
      * @return channelKeepAliveTimeout.
      */
     long channelKeepAliveTimeout();
-    
+
     /**
      * getTlsConfig.
      *
@@ -91,7 +91,7 @@ public interface GrpcClientConfig extends RpcClientConfig {
     TlsConfig tlsConfig();
 
     /**
-     *Set TlsConfig.
+     * Set TlsConfig.
      *
      * @param tlsConfig tlsConfig of client.
      */
