0d08e6c88bb55a6a39abac382eb5cb36530701d8
karsonto <karsontao@hotmail.com>
2024-01-04 11:30:44 +0800

    remove deprecated config

--- address/src/main/java/com/alibaba/nacos/address/config/AddressServerSecurityConfiguration.java
+++ address/src/main/java/com/alibaba/nacos/address/config/AddressServerSecurityConfiguration.java
@@ -16,22 +16,31 @@
 
 package com.alibaba.nacos.address.config;
 
+import com.alibaba.nacos.common.utils.StringUtils;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
 
 /**
  * nacos web security configuration.
+ *
  * @author onewe
  */
 @Configuration
-@Order(99)
-public class AddressServerSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class AddressServerSecurityConfiguration {
     
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(requestMatcherRegistry -> requestMatcherRegistry.mvcMatchers("/nacos/v1/as/**").authenticated())
-                .csrf().disable().httpBasic();
+    @Bean
+    @Order(99)
+    public SecurityFilterChain addressServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                        requestMatcherRegistry -> requestMatcherRegistry.mvcMatchers("/nacos/v1/as/**").authenticated()).csrf()
+                .disable().httpBasic();
+        return http.build();
     }
 }
