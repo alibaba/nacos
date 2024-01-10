f734215df879fa7ec11b80338262735c377c1164
floor07 <floor07@126.com>
2024-01-10 14:04:54 +0800

    fix：默认的nacos反脆弱插件的问题

--- plugin/control/src/main/java/com/alibaba/nacos/plugin/control/tps/barrier/SimpleCountRuleBarrier.java
+++ plugin/control/src/main/java/com/alibaba/nacos/plugin/control/tps/barrier/SimpleCountRuleBarrier.java
@@ -58,12 +58,12 @@ public abstract class SimpleCountRuleBarrier extends RuleBarrier {
     
     @Override
     public TpsCheckResponse applyTps(BarrierCheckRequest barrierCheckRequest) {
-        long count = rateCounter.getCount(barrierCheckRequest.getTimestamp());
+        long count = rateCounter.add(barrierCheckRequest.getTimestamp(), barrierCheckRequest.getCount());
         long maxCount = getMaxCount();
         if (MonitorType.INTERCEPT.getType().equals(getMonitorType()) && maxCount >= 0 && count >= maxCount) {
+            rateCounter.addInterceptedCount(barrierCheckRequest.getTimestamp(), barrierCheckRequest.getCount());
             return new TpsCheckResponse(false, TpsResultCode.DENY_BY_POINT, "tps over limit :" + maxCount);
         }
-        rateCounter.add(barrierCheckRequest.getTimestamp(), barrierCheckRequest.getCount());
         return new TpsCheckResponse(true, TpsResultCode.PASS_BY_POINT, "success");
     }
     
