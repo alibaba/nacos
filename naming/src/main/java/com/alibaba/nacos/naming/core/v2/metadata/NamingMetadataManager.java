d38ef0b2494bbe77520040eeea891ee2bf734259
JianweiWang <786594722@qq.com>
2024-03-15 16:43:02 +0800

    Revert "fix #11835"
    This reverts commit b41abcd8841351971db61cc48d10d32ed36dc1db.

--- naming/src/main/java/com/alibaba/nacos/naming/core/v2/metadata/NamingMetadataManager.java
+++ naming/src/main/java/com/alibaba/nacos/naming/core/v2/metadata/NamingMetadataManager.java
@@ -189,8 +189,7 @@ public class NamingMetadataManager extends SmartSubscriber {
      */
     public void loadServiceMetadataSnapshot(ConcurrentMap<Service, ServiceMetadata> snapshot) {
         for (Service each : snapshot.keySet()) {
-            Service  service = Service.newService(each.getNamespace(), each.getGroup(), each.getName(), each.isEphemeral());
-            ServiceManager.getInstance().getSingleton(service);
+            ServiceManager.getInstance().getSingleton(each);
         }
         ConcurrentMap<Service, ServiceMetadata> oldSnapshot = serviceMetadataMap;
         serviceMetadataMap = snapshot;
