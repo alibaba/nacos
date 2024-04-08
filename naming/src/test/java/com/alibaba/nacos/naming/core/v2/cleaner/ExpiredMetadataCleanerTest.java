7e61d032d3bdd74503c8e276cb1821d1cd7fffb0
nkorange <zhupf073@outlook.com>
2024-04-08 15:32:07 +0800

    Add unit test for expired metadata cleaner

--- naming/src/test/java/com/alibaba/nacos/naming/core/v2/cleaner/ExpiredMetadataCleanerTest.java
+++ naming/src/test/java/com/alibaba/nacos/naming/core/v2/cleaner/ExpiredMetadataCleanerTest.java
@@ -16,13 +16,19 @@
 
 package com.alibaba.nacos.naming.core.v2.cleaner;
 
+import com.alibaba.nacos.api.common.Constants;
+import com.alibaba.nacos.api.naming.pojo.Instance;
+import com.alibaba.nacos.api.naming.pojo.ServiceInfo;
 import com.alibaba.nacos.common.utils.ConcurrentHashSet;
 import com.alibaba.nacos.naming.core.v2.index.ServiceStorage;
 import com.alibaba.nacos.naming.core.v2.metadata.ExpiredMetadataInfo;
 import com.alibaba.nacos.naming.core.v2.metadata.NamingMetadataManager;
 import com.alibaba.nacos.naming.core.v2.metadata.NamingMetadataOperateService;
+import com.alibaba.nacos.naming.core.v2.pojo.InstancePublishInfo;
+import com.alibaba.nacos.naming.core.v2.pojo.Service;
 import com.alibaba.nacos.sys.env.EnvUtil;
 import junit.framework.TestCase;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,8 +36,11 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.mock.env.MockEnvironment;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -50,6 +59,9 @@ public class ExpiredMetadataCleanerTest extends TestCase {
 
     @Mock
     private ExpiredMetadataInfo expiredMetadataInfoMock;
+    
+    @Mock
+    private ExpiredMetadataInfo existingMetadataInfoMock;
     
     @Mock
     private ServiceStorage serviceStorage;
@@ -72,5 +84,42 @@ public class ExpiredMetadataCleanerTest extends TestCase {
         expiredMetadataCleaner.doClean();
         verify(metadataManagerMock).getExpiredMetadataInfos();
         verify(metadataOperateServiceMock).deleteServiceMetadata(expiredMetadataInfoMock.getService());
+    }
+    
+    @Test
+    public void testCleanExistingInstanceMetadata() {
+        String serviceName = "test.1";
+        String ip = "1.1.1.1";
+        int port = 7001;
+        ServiceInfo serviceInfo = new ServiceInfo();
+        serviceInfo.setName(serviceName);
+        Instance instance = new Instance();
+        instance.setIp(ip);
+        instance.setPort(port);
+        instance.setClusterName(Constants.DEFAULT_CLUSTER_NAME);
+        List<Instance> instances = new ArrayList<>();
+        instances.add(instance);
+        serviceInfo.setHosts(instances);
+        
+        Service service = Service.newService(Constants.DEFAULT_NAMESPACE_ID, Constants.DEFAULT_GROUP, serviceName);
+        when(existingMetadataInfoMock.getService()).thenReturn(service);
+        when(existingMetadataInfoMock.getCreateTime()).thenReturn(0L);
+        when(existingMetadataInfoMock.getMetadataId()).
+                thenReturn(InstancePublishInfo.genMetadataId(ip, port, Constants.DEFAULT_CLUSTER_NAME));
+        
+        set.add(existingMetadataInfoMock);
+        
+        Assert.assertEquals(2, set.size());
+        
+        when(serviceStorage.getPushData(service)).thenReturn(serviceInfo);
+        
+        expiredMetadataCleaner.doClean();
+        
+        verify(metadataManagerMock).getExpiredMetadataInfos();
+        verify(metadataOperateServiceMock).deleteServiceMetadata(expiredMetadataInfoMock.getService());
+        verify(metadataOperateServiceMock, never()).deleteServiceMetadata(service);
+        verify(serviceStorage).getPushData(service);
+        
+        Assert.assertEquals(1, set.size());
     }
 }
