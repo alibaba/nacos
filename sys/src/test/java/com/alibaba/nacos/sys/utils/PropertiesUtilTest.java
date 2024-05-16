67b672251e540ee7c7e4b5c6f9b965abe6a04dbd
shalk(xiao kun) <xshalk@163.com>
2024-05-16 09:40:12 +0800

    upgrade module naocs-sys from junit4 to junit5 (#12107)

--- sys/src/test/java/com/alibaba/nacos/sys/utils/PropertiesUtilTest.java
+++ sys/src/test/java/com/alibaba/nacos/sys/utils/PropertiesUtilTest.java
@@ -16,7 +16,7 @@
 
 package com.alibaba.nacos.sys.utils;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,19 +27,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Map;
 import java.util.Properties;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("prefix")
 @SpringBootTest(classes = PropertiesUtilTest.class)
-public class PropertiesUtilTest {
+class PropertiesUtilTest {
     
     @Autowired
     private ConfigurableEnvironment environment;
     
     @Test
     @SuppressWarnings("unchecked")
-    public void testGetPropertiesWithPrefixForMap() {
+    void testGetPropertiesWithPrefixForMap() {
         Map<String, Object> actual = PropertiesUtil.getPropertiesWithPrefixForMap(environment, "nacos.prefix");
         assertEquals(3, actual.size());
         for (Map.Entry<String, Object> entry : actual.entrySet()) {
@@ -62,13 +62,13 @@ public class PropertiesUtilTest {
     }
     
     @Test
-    public void testGetPropertiesWithPrefix() {
+    void testGetPropertiesWithPrefix() {
         Properties actual = PropertiesUtil.getPropertiesWithPrefix(environment, "nacos.prefix");
         assertEquals(3, actual.size());
     }
     
     @Test
-    public void testHandleSpringBinder() {
+    void testHandleSpringBinder() {
         Map properties = PropertiesUtil.handleSpringBinder(environment, "nacos.prefix", Map.class);
         assertEquals(3, properties.size());
     }
