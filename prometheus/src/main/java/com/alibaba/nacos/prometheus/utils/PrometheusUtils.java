91b2b24a1271265a3317316b4f8305b26cf49c7e
Thomas Lee <taomaree@gmail.com>
2024-06-17 02:18:19 +0800

    prometheus http sd: fix invalid label names

--- prometheus/src/main/java/com/alibaba/nacos/prometheus/utils/PrometheusUtils.java
+++ prometheus/src/main/java/com/alibaba/nacos/prometheus/utils/PrometheusUtils.java
@@ -62,6 +62,9 @@ public class PrometheusUtils {
         labelNode.put("__meta_clusterName", clusterName);
         //export metadata
         Map<String, String> metadata = instance.getMetadata();
+        // auto convert label names contain with "." and "-" to "_"
+        metadata = metadata.entrySet().stream().collect(Collectors.toMap(e -> e.getKey().replace(".", "_").replace("-", "_"), e -> e.getValue()));
+        
         metadata.forEach(labelNode::put);
         ObjectNode jsonNode = JacksonUtils.createEmptyJsonNode();
         jsonNode.replace("targets", targetsNode);
