cc55afd6d56007e158b1103c915da6f0af14a1a3
DioxideCN <dioxide.cn@gmail.com>
2024-07-12 17:51:13 +0800

    Upgrade nacos-core to springboot 3

--- core/src/main/java/com/alibaba/nacos/core/control/http/HttpTpsCheckRequestParser.java
+++ core/src/main/java/com/alibaba/nacos/core/control/http/HttpTpsCheckRequestParser.java
@@ -20,7 +20,7 @@ package com.alibaba.nacos.core.control.http;
 
 import com.alibaba.nacos.plugin.control.tps.request.TpsCheckRequest;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * http tps check request parser.
