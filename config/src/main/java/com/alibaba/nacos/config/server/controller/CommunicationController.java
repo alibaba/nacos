243cc486d7fdad38a185bcc05d5104c3120ddb3f
DioxideCN <dioxide.cn@gmail.com>
2024-07-12 22:12:41 +0800

    Upgrade nacos-config module to springboot 3

--- config/src/main/java/com/alibaba/nacos/config/server/controller/CommunicationController.java
+++ config/src/main/java/com/alibaba/nacos/config/server/controller/CommunicationController.java
@@ -33,8 +33,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
