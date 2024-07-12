baeced76c0c823cf6050c23ed18de2cf55cad238
DioxideCN <dioxide.cn@gmail.com>
2024-07-12 12:07:23 +0800

    Upgrade nacos-auth of jakarta.servlet

--- auth/src/main/java/com/alibaba/nacos/auth/parser/http/ConfigHttpResourceParser.java
+++ auth/src/main/java/com/alibaba/nacos/auth/parser/http/ConfigHttpResourceParser.java
@@ -20,7 +20,7 @@ import com.alibaba.nacos.api.common.Constants;
 import com.alibaba.nacos.common.utils.NamespaceUtil;
 import com.alibaba.nacos.common.utils.StringUtils;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Properties;
 
 /**
