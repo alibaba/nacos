714398d7529b2c04e290d96d4b700282aa5fc410
DioxideCN <dioxide.cn@gmail.com>
2024-07-13 11:48:13 +0800

    Upgrade nacos-cmdb module to springboot 3

--- cmdb/src/main/java/com/alibaba/nacos/cmdb/controllers/OperationController.java
+++ cmdb/src/main/java/com/alibaba/nacos/cmdb/controllers/OperationController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * Operation controller.
