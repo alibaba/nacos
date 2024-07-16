38d22ff19633e96e116bf0579b7849dbbe0141e5
DioxideCN <dioxide.cn@gmail.com>
2024-07-16 13:34:12 +0800

    Upgrade nacos-common to support httpclient 5.2

--- common/src/main/java/com/alibaba/nacos/common/http/client/response/DefaultClientHttpResponse.java
+++ common/src/main/java/com/alibaba/nacos/common/http/client/response/DefaultClientHttpResponse.java
@@ -22,6 +22,7 @@ import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Iterator;
 
 /**
  * ApacheClientHttpResponse implementation {@link HttpClientResponse}.
