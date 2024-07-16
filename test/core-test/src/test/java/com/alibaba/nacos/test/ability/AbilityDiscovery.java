deae901894963def3a79383377f523ac0ae84f0a
DioxideCN <dioxide.cn@gmail.com>
2024-07-16 15:57:28 +0800

    Update test module to support springboot 3

--- test/core-test/src/test/java/com/alibaba/nacos/test/ability/AbilityDiscovery.java
+++ test/core-test/src/test/java/com/alibaba/nacos/test/ability/AbilityDiscovery.java
@@ -48,7 +48,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.annotation.Resource;
