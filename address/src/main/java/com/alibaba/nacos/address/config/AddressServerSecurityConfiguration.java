1d634239933c9af0471e738d389ed57782d7895b
DioxideCN <dioxide.cn@gmail.com>
2024-07-16 15:22:13 +0800

    Upgrade nacos-address to support springboot 3

--- address/src/main/java/com/alibaba/nacos/address/config/AddressServerSecurityConfiguration.java
+++ address/src/main/java/com/alibaba/nacos/address/config/AddressServerSecurityConfiguration.java
@@ -19,7 +19,9 @@ package com.alibaba.nacos.address.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -34,8 +36,10 @@ public class AddressServerSecurityConfiguration {
     @Order(99)
     public SecurityFilterChain addressServerSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
-                requestMatcherRegistry -> requestMatcherRegistry.mvcMatchers("/nacos/v1/as/**").authenticated()).csrf()
-                .disable().httpBasic();
+                requestMatcherRegistry -> requestMatcherRegistry
+                        .requestMatchers("/nacos/v1/as/**").authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
