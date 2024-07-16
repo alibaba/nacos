a46e1859abea0ac618ca507b36abb6411393c13e
DioxideCN <dioxide.cn@gmail.com>
2024-07-16 14:38:27 +0800

    Upgrade nacos-naming module to springboot 3

--- naming/src/main/java/com/alibaba/nacos/naming/web/ClientAttributesFilter.java
+++ naming/src/main/java/com/alibaba/nacos/naming/web/ClientAttributesFilter.java
@@ -29,12 +29,12 @@ import com.alibaba.nacos.naming.misc.Loggers;
 import com.alibaba.nacos.naming.misc.UtilsAndCommons;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.Optional;
 
--- naming/src/main/java/com/alibaba/nacos/naming/web/TrafficReviseFilter.java
+++ naming/src/main/java/com/alibaba/nacos/naming/web/TrafficReviseFilter.java
@@ -25,13 +25,13 @@ import com.alibaba.nacos.naming.misc.SwitchDomain;
 import com.alibaba.nacos.common.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
 
