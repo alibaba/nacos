2e47aa318b87fad23389eb200643a51aef16f13b
koushenhai <2413176044@qq.com>
2024-07-19 21:58:26 +0800

    删除console多余的@EnableScheduling

--- console/src/main/java/com/alibaba/nacos/Nacos.java
+++ console/src/main/java/com/alibaba/nacos/Nacos.java
@@ -25,7 +25,6 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Nacos starter.
