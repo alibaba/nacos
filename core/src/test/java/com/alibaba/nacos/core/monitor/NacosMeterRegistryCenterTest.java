51819d80c089553e84ba4e004be9327702a58105
DioxideCN <dioxide.cn@gmail.com>
2024-08-01 15:51:00 +0800

    Fallback to use micrometer 1.12.8

--- core/src/test/java/com/alibaba/nacos/core/monitor/NacosMeterRegistryCenterTest.java
+++ core/src/test/java/com/alibaba/nacos/core/monitor/NacosMeterRegistryCenterTest.java
@@ -17,7 +17,7 @@
 package com.alibaba.nacos.core.monitor;
 
 import com.alibaba.nacos.sys.utils.ApplicationUtils;
-import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
