6f524ec4b32c3d4231b2edd5515b9b0e920c422e
张煜坤 <zhangyukun@bupt.edu.cn>
2024-08-26 17:21:42 +0800

    [ISSUE #12017] Add the console backend API for config section
    * Add history handling module

---
+++ console/src/main/java/com/alibaba/nacos/console/controller/v3/config/ConsoleHistoryController.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 1999-2024 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.alibaba.nacos.console.controller.v3.config;
+
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.model.v2.Result;
+import com.alibaba.nacos.auth.annotation.Secured;
+import com.alibaba.nacos.common.utils.NamespaceUtil;
+import com.alibaba.nacos.common.utils.StringUtils;
+import com.alibaba.nacos.config.server.model.ConfigHistoryInfo;
+import com.alibaba.nacos.config.server.model.ConfigInfoWrapper;
+import com.alibaba.nacos.config.server.paramcheck.ConfigDefaultHttpParamExtractor;
+import com.alibaba.nacos.config.server.utils.ParamUtils;
+import com.alibaba.nacos.console.proxy.config.HistoryProxy;
+import com.alibaba.nacos.core.paramcheck.ExtractorManager;
+import com.alibaba.nacos.persistence.model.Page;
+import com.alibaba.nacos.plugin.auth.constant.ActionTypes;
+import com.alibaba.nacos.plugin.auth.constant.SignType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * .
+ *
+ * @author zhangyukun on:2024/8/16
+ */
+@RestController
+@RequestMapping("/v3/console/cs/history")
+@ExtractorManager.Extractor(httpExtractor = ConfigDefaultHttpParamExtractor.class)
+public class ConsoleHistoryController {
+    
+    private final HistoryProxy historyProxy;
+
+    @Autowired
+    public ConsoleHistoryController(HistoryProxy historyProxy) {
+        this.historyProxy = historyProxy;
+    }
+    
+    /**
+     * Query the detailed configuration history information. notes:
+     *
+     * @param nid         history_config_info nid
+     * @param dataId      dataId
+     * @param group       groupId
+     * @param namespaceId namespaceId
+     * @return history config info
+     */
+    @GetMapping
+    @Secured(action = ActionTypes.READ, signType = SignType.CONFIG)
+    public Result<ConfigHistoryInfo> getConfigHistoryInfo(@RequestParam("dataId") String dataId,
+            @RequestParam("group") String group,
+            @RequestParam(value = "namespaceId", required = false, defaultValue = StringUtils.EMPTY) String namespaceId,
+            @RequestParam("nid") Long nid) throws NacosException {
+        // check namespaceId
+        ParamUtils.checkTenantV2(namespaceId);
+        namespaceId = NamespaceUtil.processNamespaceParameter(namespaceId);
+        // check params
+        ParamUtils.checkParam(dataId, group, "datumId", "content");
+        
+        return Result.success(historyProxy.getConfigHistoryInfo(dataId, group, namespaceId, nid));
+    }
+
+    /**
+     * Query the list history config. notes:
+     *
+     * @param dataId      dataId string value [required].
+     * @param group       group string value [required].
+     * @param namespaceId namespaceId.
+     * @param pageNo      pageNo integer value.
+     * @param pageSize    pageSize integer value.
+     * @return the page of history config.
+     */
+    @GetMapping("/list")
+    @Secured(action = ActionTypes.READ, signType = SignType.CONFIG)
+    public Result<Page<ConfigHistoryInfo>> listConfigHistory(@RequestParam("dataId") String dataId,
+            @RequestParam("group") String group,
+            @RequestParam(value = "namespaceId", required = false, defaultValue = StringUtils.EMPTY) String namespaceId,
+            @RequestParam(value = "pageNo", required = false, defaultValue = "1") Integer pageNo,
+            @RequestParam(value = "pageSize", required = false, defaultValue = "100") Integer pageSize)
+            throws NacosException {
+        pageSize = Math.min(500, pageSize);
+        // check namespaceId
+        ParamUtils.checkTenantV2(namespaceId);
+        namespaceId = NamespaceUtil.processNamespaceParameter(namespaceId);
+        // check params
+        ParamUtils.checkParam(dataId, group, "datumId", "content");
+    
+        return Result.success(historyProxy.listConfigHistory(dataId, group, namespaceId, pageNo, pageSize));
+    }
+    
+    /**
+     * Query previous config history information. notes:
+     *
+     * @param id          config_info id
+     * @param dataId      dataId
+     * @param group       groupId
+     * @param namespaceId namespaceId
+     * @return history config info
+     */
+    @GetMapping(value = "/previous")
+    @Secured(action = ActionTypes.READ, signType = SignType.CONFIG)
+    public Result<ConfigHistoryInfo> getPreviousConfigHistoryInfo(@RequestParam("dataId") String dataId,
+            @RequestParam("group") String group,
+            @RequestParam(value = "namespaceId", required = false, defaultValue = StringUtils.EMPTY) String namespaceId,
+            @RequestParam("id") Long id) throws NacosException {
+        // check namespaceId
+        ParamUtils.checkTenantV2(namespaceId);
+        namespaceId = NamespaceUtil.processNamespaceParameter(namespaceId);
+        // check params
+        ParamUtils.checkParam(dataId, group, "datumId", "content");
+        
+        return Result.success(historyProxy.getPreviousConfigHistoryInfo(dataId, group, namespaceId, id));
+    }
+    
+    /**
+     * Query configs list by namespace.
+     *
+     * @param namespaceId config_info namespace
+     * @return list
+     */
+    @GetMapping(value = "/configs")
+    @Secured(action = ActionTypes.READ, signType = SignType.CONFIG)
+    public Result<List<ConfigInfoWrapper>> getConfigsByTenant(@RequestParam("namespaceId") String namespaceId)
+            throws NacosException {
+        // check namespaceId
+        ParamUtils.checkTenantV2(namespaceId);
+        namespaceId = NamespaceUtil.processNamespaceParameter(namespaceId);
+        
+        return Result.success(historyProxy.getConfigsByTenant(namespaceId));
+    }
+
+}
---
+++ console/src/main/java/com/alibaba/nacos/console/handler/inner/config/HistoryInnerHandler.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 1999-2024 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.alibaba.nacos.console.handler.inner.config;
+
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.exception.api.NacosApiException;
+import com.alibaba.nacos.api.model.v2.ErrorCode;
+import com.alibaba.nacos.config.server.model.ConfigHistoryInfo;
+import com.alibaba.nacos.config.server.model.ConfigInfoWrapper;
+import com.alibaba.nacos.config.server.service.HistoryService;
+import com.alibaba.nacos.console.handler.config.HistoryHandler;
+import com.alibaba.nacos.persistence.model.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * .
+ *
+ * @author zhangyukun on:2024/8/16
+ */
+@Service
+public class HistoryInnerHandler implements HistoryHandler {
+    
+    private final HistoryService historyService;
+    
+    @Autowired
+    public HistoryInnerHandler(HistoryService historyService) {
+        this.historyService = historyService;
+    }
+    
+    @Override
+    public ConfigHistoryInfo getConfigHistoryInfo(String dataId, String group, String namespaceId, Long nid)
+            throws NacosException {
+        ConfigHistoryInfo configHistoryInfo;
+        try {
+            configHistoryInfo = historyService.getConfigHistoryInfo(dataId, group, namespaceId, nid);
+        } catch (DataAccessException e) {
+            throw new NacosApiException(HttpStatus.NOT_FOUND.value(), ErrorCode.RESOURCE_NOT_FOUND,
+                    "certain config history for nid = " + nid + " not exist");
+        }
+        return configHistoryInfo;
+    }
+    
+    @Override
+    public Page<ConfigHistoryInfo> listConfigHistory(String dataId, String group, String namespaceId, Integer pageNo,
+            Integer pageSize) throws NacosException {
+        return historyService.listConfigHistory(dataId, group, namespaceId, pageNo, pageSize);
+    }
+    
+    @Override
+    public ConfigHistoryInfo getPreviousConfigHistoryInfo(String dataId, String group, String namespaceId, Long id)
+            throws NacosException {
+        ConfigHistoryInfo configHistoryInfo;
+        try {
+            configHistoryInfo = historyService.getPreviousConfigHistoryInfo(dataId, group, namespaceId, id);
+        } catch (DataAccessException e) {
+            throw new NacosApiException(HttpStatus.NOT_FOUND.value(), ErrorCode.RESOURCE_NOT_FOUND,
+                    "previous config history for id = " + id + " not exist");
+        }
+        return configHistoryInfo;
+    }
+    
+    @Override
+    public List<ConfigInfoWrapper> getConfigsByTenant(String namespaceId) throws NacosApiException {
+        return historyService.getConfigListByNamespace(namespaceId);
+    }
+}
