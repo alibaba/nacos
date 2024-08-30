bc9126449d49b10c09047e7e975837f44c6705bf
张煜坤 <zhangyukun@bupt.edu.cn>
2024-08-30 21:39:20 +0800

    [ISSUE #12017] Refactor the old version of the console's controller
    * Refactor the old version of the console's controller

---
+++ console/src/main/java/com/alibaba/nacos/console/controller/v3/ConsoleServerStateController.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 1999-2024 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.alibaba.nacos.console.controller.v3;
+
+import com.alibaba.nacos.api.model.v2.Result;
+import com.alibaba.nacos.console.paramcheck.ConsoleDefaultHttpParamExtractor;
+import com.alibaba.nacos.console.proxy.ServerStateProxy;
+import com.alibaba.nacos.core.paramcheck.ExtractorManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * Controller for managing server state-related operations.
+ *
+ * @author zhangyukun on:2024/8/27
+ */
+@RestController
+@RequestMapping("/v3/console/server")
+@ExtractorManager.Extractor(httpExtractor = ConsoleDefaultHttpParamExtractor.class)
+public class ConsoleServerStateController {
+    
+    private final ServerStateProxy serverStateProxy;
+    
+    public ConsoleServerStateController(ServerStateProxy serverStateProxy) {
+        this.serverStateProxy = serverStateProxy;
+    }
+    
+    /**
+     * Get server state of current server.
+     *
+     * @return state json.
+     */
+    @GetMapping("/state")
+    public Result<Map<String, String>> serverState() {
+        Map<String, String> serverState = serverStateProxy.getServerState();
+        return Result.success(serverState);
+    }
+    
+    /**
+     * Get the announcement content based on the specified language.
+     *
+     * @param language Language for the announcement (default: "zh-CN")
+     * @return Announcement content as a string wrapped in a Result object
+     */
+    @GetMapping("/announcement")
+    public Result<String> getAnnouncement(
+            @RequestParam(required = false, name = "language", defaultValue = "zh-CN") String language) {
+        String announcement = serverStateProxy.getAnnouncement(language);
+        return Result.success(announcement);
+    }
+    
+    /**
+     * Get the console UI guide information.
+     *
+     * @return Console UI guide information as a string wrapped in a Result object
+     */
+    @GetMapping("/guide")
+    public Result<String> getConsoleUiGuide() {
+        String guideInformation = serverStateProxy.getConsoleUiGuide();
+        return Result.success(guideInformation);
+    }
+}
