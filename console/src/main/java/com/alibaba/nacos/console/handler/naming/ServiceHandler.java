f1b00a3232188109ce6b8566ed3dcac8abd9bbfe
张煜坤 <zhangyukun@bupt.edu.cn>
2024-09-04 14:42:00 +0800

    [ISSUE #12017] Add the console backend API for new section (#12553)
    * [ISSUE #12017] Add the console backend API for config section
    
    * Add history handling module
    
    * [ISSUE #12017] Add the console backend API for naming section
    
    * Add service handling module
    
    * Add instance handling module
    
    * [ISSUE #12017] Add the console backend API for auth section
    
    * Add user handling module
    
    * Add role handling module
    
    * Add permission handling module
    
    * [ISSUE #12017] Add the console backend API for core section
    
    * Add cluster handling module
    
    * [ISSUE #12017] Fix the error by adding ApiType
    
    * Fix the error by adding ApiType
    
    * Revert "[ISSUE #12017] Add the console backend API for auth section"
    
    This reverts commit 052112a709734eaad3857d4a04831db9ae291b34.
    
    * [ISSUE #12017] Updated ControllerV3 for auth section
    
    * Updated ControllerV3 for auth section
    
    * [ISSUE #12017] Fix errors
    
    * Add ApiType annotation
    
    * Transfer the updateCluster method
    
    * Fix Compilation Errors

---
+++ console/src/main/java/com/alibaba/nacos/console/handler/naming/ServiceHandler.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 1999-2024 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.alibaba.nacos.console.handler.naming;
+
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.naming.core.v2.metadata.ClusterMetadata;
+import com.alibaba.nacos.naming.core.v2.metadata.ServiceMetadata;
+import com.alibaba.nacos.naming.core.v2.pojo.Service;
+import com.alibaba.nacos.naming.model.form.ServiceForm;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Interface for handling service-related operations.
+ *
+ * @author zhangyukun
+ */
+public interface ServiceHandler {
+    
+    /**
+     * Create a new service.
+     *
+     * @param serviceForm the service form containing the service details
+     * @param serviceMetadata the service metadata created from serviceForm
+     * @throws Exception if an error occurs during service creation
+     */
+    void createService(ServiceForm serviceForm, ServiceMetadata serviceMetadata) throws Exception;
+    
+    /**
+     * Delete an existing service.
+     *
+     * @param namespaceId the namespace ID
+     * @param serviceName the service name
+     * @param groupName   the group name
+     * @throws Exception if an error occurs during service deletion
+     */
+    void deleteService(String namespaceId, String serviceName, String groupName) throws Exception;
+    
+    /**
+     * Update an existing service.
+     *
+     * @param serviceForm     the service form containing the service details
+     * @param service         the service object created from serviceForm
+     * @param serviceMetadata the service metadata created from serviceForm
+     * @param metadata       the service metadata
+     * @throws Exception if an error occurs during service update
+     */
+    void updateService(ServiceForm serviceForm, Service service, ServiceMetadata serviceMetadata,
+            Map<String, String> metadata) throws Exception;
+    
+    /**
+     * Get all selector types.
+     *
+     * @return a list of selector types
+     */
+    List<String> getSelectorTypeList();
+    
+    /**
+     * Get the list of subscribers for a service.
+     *
+     * @param pageNo      the page number
+     * @param pageSize    the size of the page
+     * @param namespaceId the namespace ID
+     * @param serviceName the service name
+     * @param aggregation whether to aggregate the results
+     * @return a JSON node containing the list of subscribers
+     * @throws Exception if an error occurs during fetching subscribers
+     */
+    ObjectNode getSubscribers(int pageNo, int pageSize, String namespaceId, String serviceName, boolean aggregation)
+            throws Exception;
+    
+    /**
+     * List service detail information.
+     *
+     * @param withInstances     whether to include instances
+     * @param namespaceId       the namespace ID
+     * @param pageNo            the page number
+     * @param pageSize          the size of the page
+     * @param serviceName       the service name
+     * @param groupName         the group name
+     * @param containedInstance instance name pattern which will be contained in detail
+     * @param hasIpCount        whether to filter services with empty instances
+     * @return service detail information
+     * @throws NacosException if an error occurs during fetching service details
+     */
+    Object getServiceList(boolean withInstances, String namespaceId, int pageNo, int pageSize, String serviceName,
+            String groupName, String containedInstance, boolean hasIpCount) throws NacosException;
+    
+    /**
+     * Get the detail of a specific service.
+     *
+     * @param namespaceId             the namespace ID
+     * @param serviceNameWithoutGroup the service name without group
+     * @param groupName               the group name
+     * @return service detail information
+     * @throws NacosException if an error occurs during fetching service details
+     */
+    Object getServiceDetail(String namespaceId, String serviceNameWithoutGroup, String groupName) throws NacosException;
+    
+    /**
+     * Update the metadata of a cluster.
+     *
+     * @param namespaceId     the namespace ID
+     * @param serviceName     the service name
+     * @param clusterName     the cluster name
+     * @param clusterMetadata the metadata for the cluster
+     * @throws Exception if the update operation fails
+     */
+    void updateClusterMetadata(String namespaceId, String serviceName, String clusterName,
+            ClusterMetadata clusterMetadata) throws Exception;
+}
+
