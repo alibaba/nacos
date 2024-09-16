46124d562c81fc4075458830d483894c0e42eab0
DioxideCN <dioxide.cn@gmail.com>
2024-09-17 00:02:32 +0800

    debug for native nacos bean processor

--- console/src/main/java/com/alibaba/nacos/console/aot/BeanProcessorHints.java
+++ console/src/main/java/com/alibaba/nacos/console/aot/BeanProcessorHints.java
@@ -1,3 +1,21 @@
+/*
+ *
+ * Copyright 1999-2021 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.alibaba.nacos.console.aot;
 
 import org.springframework.beans.BeansException;
@@ -5,6 +23,8 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
 /**
+ * Just for test.
+ *
  * @author Dioxide.CN
  * @date 2024/9/16
  * @since 1.0
@@ -12,6 +32,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class BeanProcessorHints implements BeanPostProcessor {
     
+    /**
+     * Just for test.
+     */
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         // ClientConnectionEventListenerRegistry
