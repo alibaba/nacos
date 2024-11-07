ad318c06352592e7d04ee540a0cad3a7383d6e1e
shiyiyue1102 <liuzunfei@gmail.com>
2024-11-07 23:17:11 +0800

    testcase fix

--- config/src/test/java/com/alibaba/nacos/config/server/remote/ConfigPublishRequestHandlerTest.java
+++ config/src/test/java/com/alibaba/nacos/config/server/remote/ConfigPublishRequestHandlerTest.java
@@ -51,7 +51,6 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
