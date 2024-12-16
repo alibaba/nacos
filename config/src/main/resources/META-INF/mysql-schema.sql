edca274e39354a74672db421b4beafcd4b4cec08
shiyiyue1102 <liuzunfei@gmail.com>
2024-12-16 10:29:44 +0800

    add column gray name  into his_config_info

--- config/src/main/resources/META-INF/mysql-schema.sql
+++ config/src/main/resources/META-INF/mysql-schema.sql
@@ -154,6 +154,7 @@ CREATE TABLE `his_config_info` (
                                    `tenant_id` varchar(128) DEFAULT '' COMMENT '租户字段',
                                    `encrypted_data_key` varchar(1024) NOT NULL DEFAULT '' COMMENT '密钥',
                                    `publish_type` varchar(50)  DEFAULT 'formal' COMMENT 'publish type gray or formal',
+                                   gray_name varchar(128) DEFAULT NULL,
                                    `ext_info`  longtext DEFAULT NULL COMMENT 'ext info',
                                    PRIMARY KEY (`nid`),
                                    KEY `idx_gmt_create` (`gmt_create`),
