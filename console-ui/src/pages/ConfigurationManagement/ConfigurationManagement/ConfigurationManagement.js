183159257e44dc866a70e51875218e36497893a1
tuanzhang118 <161485764+tuanzhang118@users.noreply.github.com>
2025-07-28 16:27:07 +0800

    修复 配置列表 group模糊查询bug

--- console-ui/src/pages/ConfigurationManagement/ConfigurationManagement/ConfigurationManagement.js
+++ console-ui/src/pages/ConfigurationManagement/ConfigurationManagement/ConfigurationManagement.js
@@ -256,12 +256,13 @@ class ConfigurationManagement extends React.Component {
   }
 
   changeParamsBySearchType(params) {
+    debugger;
     if (this.state.defaultFuzzySearch) {
       if (params.dataId && params.dataId !== '') {
         params.dataId = '*' + params.dataId + '*';
       }
-      if (params.group && params.group !== '') {
-        params.group = '*' + params.group + '*';
+      if (params.groupName && params.groupName !== '') {
+        params.groupName = '*' + params.groupName + '*';
       }
     }
     if (this.state.defaultFuzzySearch) {
