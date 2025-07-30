93552e21ebcd0c938d7a019621090a6e63335a07
杨翊 SionYang <xiweng.yy@alibaba-inc.com>
2025-07-30 09:35:48 +0800

    Update console ui headert - add link for mcp marketplace  (#13657)
    * Update console ui header.
    
    * Update console ui header.

--- console-ui/src/locales/en-US.js
+++ console-ui/src/locales/en-US.js
@@ -21,6 +21,7 @@ const I18N_CONF = {
     blog: 'BLOG',
     community: 'COMMUNITY',
     enterprise: 'ENTERPRISE EDITION',
+    mcp: 'MCP Marketplace',
     languageSwitchButton: '中',
     logout: 'logout',
     changePassword: 'modify password',
