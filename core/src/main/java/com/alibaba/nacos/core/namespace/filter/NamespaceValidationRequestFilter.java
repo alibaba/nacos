6a7135a30543df42e80d98f75acc65bb9e92724d
FangYuan <374072213@qq.com>
2025-08-12 17:05:13 +0800

    [ISSUE #12191]: add filter

---
+++ core/src/main/java/com/alibaba/nacos/core/namespace/filter/NamespaceValidationRequestFilter.java
@@ -0,0 +1,98 @@
+/*
+ *
+ * Copyright 1999-2021 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.alibaba.nacos.core.namespace.filter;
+
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.exception.api.NacosApiException;
+import com.alibaba.nacos.api.naming.remote.request.AbstractNamingRequest;
+import com.alibaba.nacos.api.remote.request.Request;
+import com.alibaba.nacos.api.remote.request.RequestMeta;
+import com.alibaba.nacos.api.remote.response.Response;
+import com.alibaba.nacos.api.model.v2.ErrorCode;
+import com.alibaba.nacos.core.remote.AbstractRequestFilter;
+import com.alibaba.nacos.core.service.NamespaceOperationService;
+
+import com.alibaba.nacos.core.utils.Loggers;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+/**
+ * Namespace validation request filter for NamingRequest.
+ *
+ * @author FangYuan
+ * @since 2025-08-11 21:51:29
+ */
+@Component
+public class NamespaceValidationRequestFilter extends AbstractRequestFilter {
+
+    private final NamespaceOperationService namespaceOperationService;
+
+    public NamespaceValidationRequestFilter(NamespaceOperationService namespaceOperationService) {
+        this.namespaceOperationService = namespaceOperationService;
+    }
+
+    @Override
+    protected Response filter(Request request, RequestMeta meta, Class handlerClazz) throws NacosException {
+        try {
+            Method method = getHandleMethod(handlerClazz);
+
+            if (method.isAnnotationPresent(NamespaceValidation.class)) {
+                NamespaceValidation namespaceValidation = method.getAnnotation(NamespaceValidation.class);
+                if (!namespaceValidation.enable()) {
+                    return null;
+                }
+                if (!(request instanceof AbstractNamingRequest)) {
+                    return null;
+                }
+
+                AbstractNamingRequest namingRequest = (AbstractNamingRequest) request;
+                boolean exist = isNamespaceExist(namingRequest);
+                if (!exist) {
+                    Response response = super.getDefaultResponseInstance(handlerClazz);
+                    response.setErrorInfo(ErrorCode.NAMESPACE_NOT_EXIST.getCode(),
+                            String.format("Namespace '%s' does not exist. Please create the namespace first.",
+                                    namingRequest.getNamespace()));
+
+                    return response;
+                }
+            }
+        } catch (Exception e) {
+            Loggers.CORE.warn("Namespace validation error for request: {}, exception: {}", request, e);
+        }
+
+        return null;
+    }
+
+    private boolean isNamespaceExist(AbstractNamingRequest request) {
+        boolean namespaceExist;
+        try {
+            namespaceExist = namespaceOperationService.isNamespaceExist(request.getNamespace());
+        } catch (NacosApiException e) {
+            namespaceExist = true;
+        } catch (Exception e) {
+            Loggers.CORE.warn("Namespace validation query db error for request: {}, exception: {}", request, e);
+            // throw exception will make the request fail
+            namespaceExist = false;
+        }
+
+        return namespaceExist;
+    }
+
+}
---
+++ core/src/test/java/com/alibaba/nacos/core/namespace/filter/NamespaceValidationRequestFilterTest.java
@@ -0,0 +1,161 @@
+/*
+ *
+ * Copyright 1999-2021 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.alibaba.nacos.core.namespace.filter;
+
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.exception.api.NacosApiException;
+import com.alibaba.nacos.api.naming.remote.request.AbstractNamingRequest;
+import com.alibaba.nacos.api.naming.remote.request.InstanceRequest;
+import com.alibaba.nacos.api.naming.remote.response.InstanceResponse;
+import com.alibaba.nacos.api.remote.request.Request;
+import com.alibaba.nacos.api.remote.request.RequestMeta;
+import com.alibaba.nacos.api.remote.response.Response;
+import com.alibaba.nacos.api.model.v2.ErrorCode;
+import com.alibaba.nacos.core.remote.RequestHandler;
+import com.alibaba.nacos.core.service.NamespaceOperationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
+
+/**
+ * {@link NamespaceValidationRequestFilter} unit test.
+ *
+ * @author FangYuan
+ * @since 2025-08-12 16:24:00
+ */
+@ExtendWith(MockitoExtension.class)
+class NamespaceValidationRequestFilterTest {
+
+    private NamespaceValidationRequestFilter namespaceValidationFilter;
+
+    @Mock
+    private NamespaceOperationService namespaceOperationService;
+
+    @Mock
+    private Request request;
+
+    @Mock
+    private RequestMeta requestMeta;
+
+    @Mock
+    private AbstractNamingRequest namingRequest;
+
+    @BeforeEach
+    void setUp() {
+        namespaceValidationFilter = new NamespaceValidationRequestFilter(namespaceOperationService);
+    }
+
+    @Test
+    void testFilterWithoutNamespaceValidationAnnotation() throws NacosException {
+        Response actual = namespaceValidationFilter.filter(request, requestMeta, MockWithoutAnnotation.class);
+        assertNull(actual);
+    }
+
+    @Test
+    void testFilterWithNamespaceValidationDisabled() throws NacosException {
+        Response actual = namespaceValidationFilter.filter(request, requestMeta, MockWithDisabledValidation.class);
+        assertNull(actual);
+    }
+
+    @Test
+    void testFilterWithNonNamingRequest() throws NacosException {
+        Response actual = namespaceValidationFilter.filter(request, requestMeta, MockWithEnabledValidation.class);
+        assertNull(actual);
+    }
+
+    @Test
+    void testFilterWithNamingRequestAndNamespaceExists() throws NacosException {
+        when(namingRequest.getNamespace()).thenReturn("test-namespace");
+        when(namespaceOperationService.isNamespaceExist("test-namespace")).thenReturn(true);
+
+        Response actual = namespaceValidationFilter.filter(namingRequest, requestMeta, MockWithEnabledValidation.class);
+        assertNull(actual);
+    }
+
+    @Test
+    void testFilterWithNamingRequestAndNamespaceNotExists() throws NacosException {
+        when(namingRequest.getNamespace()).thenReturn("non-existent-namespace");
+        when(namespaceOperationService.isNamespaceExist("non-existent-namespace")).thenReturn(false);
+
+        Response actual = namespaceValidationFilter.filter(namingRequest, requestMeta, MockWithEnabledValidation.class);
+        assertNotNull(actual);
+        assertEquals(ErrorCode.NAMESPACE_NOT_EXIST.getCode(), actual.getErrorCode());
+        assertEquals("Namespace 'non-existent-namespace' does not exist. Please create the namespace first.",
+                actual.getMessage());
+    }
+
+    @Test
+    void testFilterWithNamingRequestAndNamespaceCheckThrowsNacosApiException() throws NacosException {
+        when(namingRequest.getNamespace()).thenReturn("public");
+        when(namespaceOperationService.isNamespaceExist("public"))
+                .thenThrow(new NacosApiException(HttpStatus.BAD_REQUEST.value(), ErrorCode.NAMESPACE_ALREADY_EXIST,
+                        "namespaceId [public] is default namespace id and already exist."));
+
+        Response actual = namespaceValidationFilter.filter(namingRequest, requestMeta, MockWithEnabledValidation.class);
+        assertNull(actual);
+    }
+
+    @Test
+    void testFilterWithNamingRequestAndNamespaceCheckThrowsException() throws NacosException {
+        when(namingRequest.getNamespace()).thenReturn("test-namespace");
+        when(namespaceOperationService.isNamespaceExist("test-namespace"))
+                .thenThrow(new RuntimeException("Database error"));
+
+        Response actual = namespaceValidationFilter.filter(namingRequest, requestMeta, MockWithEnabledValidation.class);
+        assertNotNull(actual);
+        assertEquals(ErrorCode.NAMESPACE_NOT_EXIST.getCode(), actual.getErrorCode());
+        assertEquals("Namespace 'test-namespace' does not exist. Please create the namespace first.",
+                actual.getMessage());
+    }
+
+    static class MockWithoutAnnotation extends RequestHandler<Request, Response> {
+        @Override
+        public Response handle(Request request, RequestMeta meta) throws NacosException {
+            return new Response() {
+            };
+        }
+    }
+
+    static class MockWithEnabledValidation extends RequestHandler<InstanceRequest, InstanceResponse> {
+
+        @NamespaceValidation
+        @Override
+        public InstanceResponse handle(InstanceRequest request, RequestMeta meta) throws NacosException {
+            return new InstanceResponse();
+        }
+    }
+
+    static class MockWithDisabledValidation extends RequestHandler<Request, Response> {
+        @NamespaceValidation(enable = false)
+        @Override
+        public Response handle(Request request, RequestMeta meta) throws NacosException {
+            return new Response() {
+            };
+        }
+    }
+
+}
