de9554eefb29f1fc67c8b6e1edf5d7b2b0cc49ad
melod_yi <melod.yi.xdb@gmail.com>
2025-09-02 14:08:30 +0800

    code check: remove unused import

--- plugin-default-impl/nacos-default-auth-plugin/src/test/java/com/alibaba/nacos/plugin/auth/impl/roles/NacosRoleServiceDirectImplTest.java
+++ plugin-default-impl/nacos-default-auth-plugin/src/test/java/com/alibaba/nacos/plugin/auth/impl/roles/NacosRoleServiceDirectImplTest.java
@@ -38,7 +38,6 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
