728e884956e9226a40237b47c04e83b86963a626
Sunrisea <1336317033@qq.com>
2025-09-16 18:00:15 +0800

    配置元数据变更接口 POST 改 PUT 方法

--- config/src/main/java/com/alibaba/nacos/config/server/controller/v3/ConfigControllerV3.java
+++ config/src/main/java/com/alibaba/nacos/config/server/controller/v3/ConfigControllerV3.java
@@ -83,6 +83,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -238,7 +239,7 @@ public class ConfigControllerV3 {
      * @return the result
      * @throws NacosException the nacos exception
      */
-    @PostMapping("/metadata")
+    @PutMapping("/metadata")
     @Secured(action = ActionTypes.WRITE, signType = SignType.CONFIG, apiType = ApiType.ADMIN_API)
     public Result<Boolean> publishConfigMetadata(HttpServletRequest request, ConfigFormV3 configForm)
             throws NacosException {
