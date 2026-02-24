1e0a1f396163a82c9ac1308abddeabbdeb95edd6
INotWant <tinggao@bupt.edu.cn>
2026-02-24 16:36:30 +0800

    Fix: 灰度配置更新时存在的多个问题 (#14482)

--- config/src/main/java/com/alibaba/nacos/config/server/service/ConfigMigrateService.java
+++ config/src/main/java/com/alibaba/nacos/config/server/service/ConfigMigrateService.java
@@ -966,6 +966,10 @@ public class ConfigMigrateService {
         
         ConfigInfo configInfo = new ConfigInfo(configForm.getDataId(), configForm.getGroup(),
                 configForm.getNamespaceId(), configForm.getAppName(), configForm.getContent());
+        // set old md5
+        if (StringUtils.isNotBlank(configRequestInfo.getCasMd5())) {
+            configInfo.setMd5(configRequestInfo.getCasMd5());
+        }
         configInfo.setType(configForm.getType());
         configInfo.setEncryptedDataKey(configForm.getEncryptedDataKey());
         
