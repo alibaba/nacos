20a26af42fde2ac56f7ffafdb8aa295591dcabbb
daguimu <daguimu.geek@gmail.com>
2026-03-26 11:14:44 +0800

    fix: fix HttpURLConnection leak on exception in JdkHttpClientRequest
    When an exception occurs during request setup (setting headers, writing
    body, or connecting), the HttpURLConnection is never disconnected,
    causing a connection resource leak. Wrap the request logic in try-catch
    to ensure conn.disconnect() is called on the exception path.
    
    Add unit tests for connection disconnect behavior on exception.

--- common/src/main/java/com/alibaba/nacos/common/http/client/request/JdkHttpClientRequest.java
+++ common/src/main/java/com/alibaba/nacos/common/http/client/request/JdkHttpClientRequest.java
@@ -91,38 +91,43 @@ public class JdkHttpClientRequest implements HttpClientRequest {
         replaceDefaultConfig(requestHttpEntity.getHttpClientConfig());
         
         HttpURLConnection conn = (HttpURLConnection) uri.toURL().openConnection();
-        Map<String, String> headerMap = headers.getHeader();
-        if (headerMap != null && headerMap.size() > 0) {
-            for (Map.Entry<String, String> entry : headerMap.entrySet()) {
-                conn.setRequestProperty(entry.getKey(), entry.getValue());
+        try {
+            Map<String, String> headerMap = headers.getHeader();
+            if (headerMap != null && headerMap.size() > 0) {
+                for (Map.Entry<String, String> entry : headerMap.entrySet()) {
+                    conn.setRequestProperty(entry.getKey(), entry.getValue());
+                }
             }
+
+            conn.setConnectTimeout(this.httpClientConfig.getConTimeOutMillis());
+            conn.setReadTimeout(this.httpClientConfig.getReadTimeOutMillis());
+            conn.setRequestMethod(httpMethod);
+            if (body != null && !"".equals(body)) {
+                if (body instanceof File) {
+                    handleFileUpload(conn, (File) body);
+                }
+                String contentType = headers.getValue(HttpHeaderConsts.CONTENT_TYPE);
+                String bodyStr = body instanceof String ? (String) body : JacksonUtils.toJson(body);
+                if (MediaType.APPLICATION_FORM_URLENCODED.equals(contentType)) {
+                    Map<String, String> map = JacksonUtils.toObj(bodyStr, HashMap.class);
+                    bodyStr = HttpUtils.encodingParams(map, headers.getCharset());
+                }
+                if (bodyStr != null) {
+                    conn.setDoOutput(true);
+                    byte[] b = bodyStr.getBytes(StandardCharsets.UTF_8);
+                    conn.setRequestProperty(CONTENT_LENGTH, String.valueOf(b.length));
+                    OutputStream outputStream = conn.getOutputStream();
+                    outputStream.write(b, 0, b.length);
+                    outputStream.flush();
+                    IoUtils.closeQuietly(outputStream);
+                }
+            }
+            conn.connect();
+            return new JdkHttpClientResponse(conn);
+        } catch (Exception e) {
+            conn.disconnect();
+            throw e;
         }
-        
-        conn.setConnectTimeout(this.httpClientConfig.getConTimeOutMillis());
-        conn.setReadTimeout(this.httpClientConfig.getReadTimeOutMillis());
-        conn.setRequestMethod(httpMethod);
-        if (body != null && !"".equals(body)) {
-            if (body instanceof File) {
-                handleFileUpload(conn, (File) body);
-            }
-            String contentType = headers.getValue(HttpHeaderConsts.CONTENT_TYPE);
-            String bodyStr = body instanceof String ? (String) body : JacksonUtils.toJson(body);
-            if (MediaType.APPLICATION_FORM_URLENCODED.equals(contentType)) {
-                Map<String, String> map = JacksonUtils.toObj(bodyStr, HashMap.class);
-                bodyStr = HttpUtils.encodingParams(map, headers.getCharset());
-            }
-            if (bodyStr != null) {
-                conn.setDoOutput(true);
-                byte[] b = bodyStr.getBytes(StandardCharsets.UTF_8);
-                conn.setRequestProperty(CONTENT_LENGTH, String.valueOf(b.length));
-                OutputStream outputStream = conn.getOutputStream();
-                outputStream.write(b, 0, b.length);
-                outputStream.flush();
-                IoUtils.closeQuietly(outputStream);
-            }
-        }
-        conn.connect();
-        return new JdkHttpClientResponse(conn);
     }
     
     private void handleFileUpload(HttpURLConnection conn, File file) throws IOException {
