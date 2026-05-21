c40bb72db5bc1409f8319fc97bcf986677649a2f
xiweng.yy <xiweng.yy@alibaba-inc.com>
2026-05-21 19:47:14 +0800

    [ISSUE #15183] Update Skill import UI assets

--- console/src/main/resources/static/next/js/index31.js
+++ console/src/main/resources/static/next/js/index31.js
@@ -1,1 +1,1 @@
-import{o as e}from"./vendor-react.js";function r(){return e.jsx("div",{children:"NewSkillPage - TODO"})}export{r as default};
+import{j as e}from"./vendor-react.js";function r(){return e.jsx("div",{children:"NewSkillPage - TODO"})}export{r as default};
