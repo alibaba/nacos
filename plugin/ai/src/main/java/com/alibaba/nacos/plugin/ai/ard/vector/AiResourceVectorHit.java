7005438481752cef225e4440715e4d63a50cc9af
sai <mosong.lp@alibaba-inc.com>
2026-07-03 15:51:19 +0800

    Add ARD vector index SPI

---
+++ plugin/ai/src/main/java/com/alibaba/nacos/plugin/ai/ard/vector/AiResourceVectorHit.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 1999-2026 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.plugin.ai.ard.vector;
+
+/**
+ * Chunk-level vector search hit.
+ *
+ * @author nacos
+ */
+public class AiResourceVectorHit {
+
+    private Long entryId;
+
+    private Long chunkId;
+
+    private String identifier;
+
+    private String resourceType;
+
+    private String resourceName;
+
+    private String resourceVersion;
+
+    private String chunkType;
+
+    private double score;
+
+    public Long getEntryId() {
+        return entryId;
+    }
+
+    public void setEntryId(Long entryId) {
+        this.entryId = entryId;
+    }
+
+    public Long getChunkId() {
+        return chunkId;
+    }
+
+    public void setChunkId(Long chunkId) {
+        this.chunkId = chunkId;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public void setResourceType(String resourceType) {
+        this.resourceType = resourceType;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    public String getResourceVersion() {
+        return resourceVersion;
+    }
+
+    public void setResourceVersion(String resourceVersion) {
+        this.resourceVersion = resourceVersion;
+    }
+
+    public String getChunkType() {
+        return chunkType;
+    }
+
+    public void setChunkType(String chunkType) {
+        this.chunkType = chunkType;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+}
