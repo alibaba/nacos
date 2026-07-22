170d3d723ba25838901aa86328b1671b7ef92936
MajorHe1 <53824889+MajorHe1@users.noreply.github.com>
2026-07-22 20:48:18 +0800

    [ISSUE #15549] use else-if in JdkHttpClientRequest.execute() when body is not file (#15550)
    * [ISSUE #15549] use else-if in JdkHttpClientRequest.execute() when body iis not file
    
    * [ISSUE #15549] add unit-test for JdkHttpClientRequest.execute() when body is file
    
    * [ISSUE #15549] fix unit-test check style for JdkHttpClientRequest.execute()
    
    * [ISSUE #15549] fix spotless problem
    
    ---------
    
    Co-authored-by: MajorHe1 <601023364@qq.com>

--- common/src/test/java/com/alibaba/nacos/common/http/client/request/JdkHttpClientRequestTest.java
+++ common/src/test/java/com/alibaba/nacos/common/http/client/request/JdkHttpClientRequestTest.java
@@ -52,7 +52,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.startsWith;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
@@ -201,6 +203,27 @@ class JdkHttpClientRequestTest {
         verify(connection, atLeast(1)).setDoOutput(true);
         verify(outputStream, atLeast(1)).flush();
         assertNotNull(response);
+    }
+    
+    @Test
+    @DisplayName("execute with File body should not fall into else-if branch")
+    void testExecuteFileBody() throws Exception {
+        File tempFile = File.createTempFile("test", ".txt");
+        tempFile.deleteOnExit();
+        Files.write(tempFile.toPath(), "test content".getBytes(StandardCharsets.UTF_8));
+        
+        Header header = Header.newInstance();
+        HttpClientConfig config = HttpClientConfig.builder().build();
+        RequestHttpEntity httpEntity = new RequestHttpEntity(config, header, Query.EMPTY, tempFile);
+        HttpClientResponse response = httpClientRequest.execute(uri, "POST", httpEntity);
+        
+        // handleFileUpload will set multipart Content-Type
+        verify(connection).setRequestProperty(eq("Content-Type"),
+            startsWith("multipart/form-data; boundary="));
+        // should not fall into else-if branch, so Content-Length will not be set
+        verify(connection, never()).setRequestProperty(eq("Content-Length"), anyString());
+        assertEquals(connection, getActualConnection(response));
+        tempFile.delete();
     }
     
     @Test
