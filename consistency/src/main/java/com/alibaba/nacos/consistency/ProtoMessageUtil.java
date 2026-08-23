3edae16bafdcc582b34ae08e6931fde325efdabe
Hyacinth-of-Security <144213008+lxcxjxhx@users.noreply.github.com>
2026-08-23 21:28:15 +0800

    fix: remove legacy GetRequest and Log parsing as per reviewer feedback
    Remove deprecated GetRequest and Log compatibility code since Nacos 3.x
    does not support upgrading from 1.x. This simplifies the parse method
    and removes unused conversion methods.

--- consistency/src/main/java/com/alibaba/nacos/consistency/ProtoMessageUtil.java
+++ consistency/src/main/java/com/alibaba/nacos/consistency/ProtoMessageUtil.java
@@ -16,8 +16,6 @@
 
 package com.alibaba.nacos.consistency;
 
-import com.alibaba.nacos.consistency.entity.GetRequest;
-import com.alibaba.nacos.consistency.entity.Log;
 import com.alibaba.nacos.consistency.entity.ReadRequest;
 import com.alibaba.nacos.consistency.entity.WriteRequest;
 import com.alibaba.nacos.consistency.exception.ConsistencyException;
@@ -45,71 +43,24 @@ public class ProtoMessageUtil {
     
     /**
      * Converts the byte array to a specific Protobuf object.
-     * Internally, the protobuf new and old objects are compatible.
      *
      * @param bytes An array of bytes
      * @return Message
      */
     public static Message parse(byte[] bytes) {
-        Message result;
         try {
             if (bytes[0] == REQUEST_TYPE_FIELD_TAG) {
                 if (bytes[1] == REQUEST_TYPE_READ) {
-                    result = ReadRequest.parseFrom(bytes);
+                    return ReadRequest.parseFrom(bytes);
                 } else {
-                    result = WriteRequest.parseFrom(bytes);
+                    return WriteRequest.parseFrom(bytes);
                 }
-                return result;
             }
         } catch (Throwable e) {
-            LOGGER.debug("Failed to parse new protocol request, will try legacy format", e);
-        }
-
-        // old consistency entity, will be @Deprecated in future
-        try {
-            GetRequest request = GetRequest.parseFrom(bytes);
-            return convertToReadRequest(request);
-        } catch (Throwable e) {
-            LOGGER.debug("Failed to parse legacy GetRequest, will try Log format", e);
-        }
-
-        try {
-            Log log = Log.parseFrom(bytes);
-            return convertToWriteRequest(log);
-        } catch (Throwable e) {
-            LOGGER.debug("Failed to parse legacy Log", e);
+            LOGGER.debug("Failed to parse protocol request", e);
         }
         
         throw new ConsistencyException(
             "The current array cannot be serialized to the corresponding object");
-    }
-    
-    /**
-     * convert Log to WriteRequest.
-     *
-     * @param log log
-     * @return {@link WriteRequest}
-     */
-    public static WriteRequest convertToWriteRequest(Log log) {
-        return WriteRequest.newBuilder().setKey(log.getKey()).setGroup(log.getGroup())
-            .setData(log.getData())
-            .setType(log.getType())
-            .setOperation(log.getOperation())
-            .putAllExtendInfo(log.getExtendInfoMap())
-            .build();
-    }
-    
-    /**
-     * convert Log to ReadRequest.
-     *
-     * @param request request
-     * @return {@link ReadRequest}
-     */
-    public static ReadRequest convertToReadRequest(GetRequest request) {
-        return ReadRequest.newBuilder()
-            .setGroup(request.getGroup())
-            .setData(request.getData())
-            .putAllExtendInfo(request.getExtendInfoMap())
-            .build();
     }
 }
