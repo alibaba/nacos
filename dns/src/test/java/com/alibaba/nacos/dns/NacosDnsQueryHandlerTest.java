4cb3698567cd7932d18f6082d85886eaa4578169
黄彬 <2423244747@qq.com>
2026-09-18 15:01:21 +0800

    style(dns): remove unused assertNotNull import (checkstyle)

--- dns/src/test/java/com/alibaba/nacos/dns/NacosDnsQueryHandlerTest.java
+++ dns/src/test/java/com/alibaba/nacos/dns/NacosDnsQueryHandlerTest.java
@@ -34,7 +34,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
